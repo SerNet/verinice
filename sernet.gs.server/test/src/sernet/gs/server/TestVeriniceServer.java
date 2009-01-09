122eba903a68d1e0f6a483d74b35a0b503e2d68e
Alexander Koderman <ak@sernet.de>
2009-01-09 00:25:18 +0000

    - added necessary libraries to server - junit test for commandservice - separation of layers, removing dependencies from jface, swt and other GUI packages
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@199 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

---
+++ sernet.gs.server/test/src/sernet/gs/server/TestVeriniceServer.java
@@ -0,0 +1,47 @@
+package sernet.gs.server;
+import java.io.File;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.access.BeanFactoryLocator;
+import org.springframework.beans.factory.access.BeanFactoryReference;
+import org.springframework.beans.factory.access.SingletonBeanFactoryLocator;
+
+import sernet.gs.ui.rcp.main.bsi.model.BSIModel;
+import sernet.gs.ui.rcp.main.connect.HibernateBaseDao;
+import sernet.gs.ui.rcp.main.service.ICommandService;
+import sernet.gs.ui.rcp.main.service.commands.CommandException;
+import sernet.gs.ui.rcp.main.service.crudcommands.LoadBSIModelComplete;
+
+
+public class TestVeriniceServer extends TestCase {
+	
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testConnect() throws CommandException {
+		BeanFactoryLocator beanFactoryLocator = SingletonBeanFactoryLocator
+		.getInstance();
+		BeanFactoryReference beanFactoryReference = beanFactoryLocator
+		.useBeanFactory("ctx");
+		 ICommandService service = (ICommandService) beanFactoryReference
+			.getFactory().getBean("commandServiceHttpInvoker");
+		 
+		 LoadBSIModelComplete command = new LoadBSIModelComplete(false);
+		 service.executeCommand(command);
+		 BSIModel model = command.getModel();
+		 System.out.println(model.getDbVersion());
+		 assertNotNull(model);
+		 assertNotNull(model.getDbVersion());
+	}
+	
+}
