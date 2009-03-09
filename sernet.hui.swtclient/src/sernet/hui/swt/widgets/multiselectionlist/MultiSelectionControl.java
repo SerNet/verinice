a4e61b9458f0349cadefbfb80e0a27e578a238fa
Alexander Koderman <ak@sernet.de>
2009-03-09 21:54:20 +0000

    - applied copyright / GPL notice to all source files
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@259 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.hui.swtclient/src/sernet/hui/swt/widgets/multiselectionlist/MultiSelectionControl.java
+++ sernet.hui.swtclient/src/sernet/hui/swt/widgets/multiselectionlist/MultiSelectionControl.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Alexander Koderman <ak@sernet.de>.
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 
+ * of the License, or (at your option) any later version.
+ *     This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU General Public License for more details.
+ *     You should have received a copy of the GNU General Public 
+ * License along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Alexander Koderman <ak@sernet.de> - initial API and implementation
+ ******************************************************************************/
 /*
  * This file is part of the SerNet Customer Database Application (SNKDB).
  * Copyright Alexander Prack, 2004.
@@ -112,7 +129,7 @@ public class MultiSelectionControl implements IHuiControl {
 		fgColor = text.getForeground();
 		
 		Button editBtn = new Button(container, SWT.PUSH);
-		editBtn.setText("Ändern...");
+		editBtn.setText("Ã„ndern...");
 		editBtn.setToolTipText(this.type.getTooltiptext());
 		editBtn.setEnabled(editable);
 		editBtn.addSelectionListener(new SelectionListener() {
--- sernet.hui.swtclient/src/sernet/hui/swt/widgets/multiselectionlist/MultiSelectionDialog.java
+++ sernet.hui.swtclient/src/sernet/hui/swt/widgets/multiselectionlist/MultiSelectionDialog.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Alexander Koderman <ak@sernet.de>.
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 
+ * of the License, or (at your option) any later version.
+ *     This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU General Public License for more details.
+ *     You should have received a copy of the GNU General Public 
+ * License along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Alexander Koderman <ak@sernet.de> - initial API and implementation
+ ******************************************************************************/
 package sernet.hui.swt.widgets.multiselectionlist;
 
 import java.util.ArrayList;
@@ -42,7 +59,7 @@ public class MultiSelectionDialog extends org.eclipse.swt.widgets.Dialog {
 
 			dialogShell.setLayout(new GridLayout(1, false));
 			dialogShell.setSize(400, 300);
-			dialogShell.setText("Optionen für Feld: " + propertyType.getName());
+			dialogShell.setText("Optionen fÃ¼r Feld: " + propertyType.getName());
 			
 			//Composite content = new Composite(dialogShell, SWT.NULL);
 			//content.setLayout(new FillLayout(SWT.VERTICAL));
