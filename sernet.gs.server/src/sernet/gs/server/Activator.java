ba4f37e911177656669906a07b79dbb555d7f771
Robert Schuster <r.schuster@tarent.de>
2009-08-03 12:12:14 +0000

    - reformatted
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@367 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.server/src/sernet/gs/server/Activator.java
+++ sernet.gs.server/src/sernet/gs/server/Activator.java
@@ -34,7 +34,7 @@ import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class Activator extends Plugin {
-	
+
 	Logger log = Logger.getLogger(Activator.class);
 
 	// The plug-in ID
@@ -42,11 +42,11 @@ public class Activator extends Plugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	private static final String OSGI_EXTENDER_SYMBOLIC_NAME = "org.springframework.osgi.extender";
-	
+
 	private static final String PAX_WEB_SYMBOLIC_NAME = "org.ops4j.pax.web.pax-web-bundle";
-	
+
 	/**
 	 * The constructor
 	 */
@@ -55,63 +55,63 @@ public class Activator extends Plugin {
 
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-		
+
 		plugin = this;
 
-		// Starts the Spring OSGi Extender which provides registering the Spring namespace handlers. If you get
-		// exception saying there is no schema for Spring security, then the OSGi extender is not running.
-		for (Bundle bundle : context.getBundles())
-		{
-			if (bundle.getSymbolicName().equals(OSGI_EXTENDER_SYMBOLIC_NAME))
-			{
+		// Starts the Spring OSGi Extender which provides registering the Spring
+		// namespace handlers. If you get exception saying there is no schema 
+		// for Spring security, then the OSGi extender is not running.
+		for (Bundle bundle : context.getBundles()) {
+			if (bundle.getSymbolicName().equals(OSGI_EXTENDER_SYMBOLIC_NAME)) {
 				if (bundle.getState() == Bundle.INSTALLED
-						|| bundle.getState() == Bundle.RESOLVED)
-				{
+						|| bundle.getState() == Bundle.RESOLVED) {
 					log.debug("Manually starting Spring's OSGi Extender");
 					bundle.start();
 				}
 			}
 		}
 
-		ServiceReference sr = context.getServiceReference(WebContainer.class.getName());
-		
+		ServiceReference sr = context.getServiceReference(WebContainer.class
+				.getName());
+
 		// Starts the Pax Web Bundle. This makes the HTTP service available.
-		if (sr == null)
-		{
-			for (Bundle bundle : context.getBundles())
-			{
-				if (bundle.getSymbolicName().equals(PAX_WEB_SYMBOLIC_NAME))
-				{
+		if (sr == null) {
+			for (Bundle bundle : context.getBundles()) {
+				if (bundle.getSymbolicName().equals(PAX_WEB_SYMBOLIC_NAME)) {
 					if (bundle.getState() == Bundle.INSTALLED
-							|| bundle.getState() == Bundle.RESOLVED)
-					{
+							|| bundle.getState() == Bundle.RESOLVED) {
 						log.debug("Manually starting Pax Web Http Service.");
 						bundle.start();
-					}
-					else
-						throw new IllegalStateException("pax-web bundle is not in a proper state to get started.");
+					} else
+						throw new IllegalStateException(
+								"pax-web bundle is not in a proper state to get started.");
 
-					sr = context.getServiceReference(WebContainer.class.getName());
+					sr = context.getServiceReference(WebContainer.class
+							.getName());
 					if (sr == null)
-						throw new IllegalStateException("pax-web bundle was started but there is still no http service available. Giving up.");
+						throw new IllegalStateException(
+								"pax-web bundle was started but there is still no http service available. Giving up.");
 				}
 			}
 		}
 		if (sr == null)
 			throw new IllegalStateException("No http service. Giving up.");
-		
+
 		WebContainer wc = (WebContainer) context.getService(sr);
-		
+
 		HttpContext ctx = wc.createDefaultHttpContext();
-		
+
 		Dictionary<String, String> dict = new Hashtable<String, String>();
-		dict.put("contextConfigLocation", "WebContent/WEB-INF/applicationContext-veriniceserver.xml");
-		dict.put(ContextLoader.CONTEXT_CLASS_PARAM, OsgiBundleXmlWebApplicationContext.class.getName());
+		dict.put("contextConfigLocation",
+				"WebContent/WEB-INF/applicationContext-veriniceserver.xml");
+		dict.put(ContextLoader.CONTEXT_CLASS_PARAM,
+				OsgiBundleXmlWebApplicationContext.class.getName());
 		wc.setContextParam(dict, ctx);
-		
+
 		dict = new Hashtable<String, String>();
 		dict.put("servlet-name", "GetHitroConfig");
-		wc.registerServlet(new GetHitroConfig(), new String[] { "/GetHitroConfig" }, dict, ctx);
+		wc.registerServlet(new GetHitroConfig(),
+				new String[] { "/GetHitroConfig" }, dict, ctx);
 
 		dict = new Hashtable<String, String>();
 		dict.put("servlet-name", "context");
@@ -119,30 +119,35 @@ public class Activator extends Plugin {
 
 		dict = new Hashtable<String, String>();
 		dict.put("servlet-name", "springDispatcher");
-		dict.put("contextConfigLocation", "WebContent/WEB-INF/springDispatcher-servlet.xml");
-		dict.put(ContextLoader.CONTEXT_CLASS_PARAM, OsgiBundleXmlWebApplicationContext.class.getName());
-		wc.registerServlet(new DispatcherServlet(), new String[] { "/service/*" }, dict, ctx); 
+		dict.put("contextConfigLocation",
+				"WebContent/WEB-INF/springDispatcher-servlet.xml");
+		dict.put(ContextLoader.CONTEXT_CLASS_PARAM,
+				OsgiBundleXmlWebApplicationContext.class.getName());
+		wc.registerServlet(new DispatcherServlet(),
+				new String[] { "/service/*" }, dict, ctx);
 
 		dict = new Hashtable<String, String>();
 		dict.put("servlet-name", "serverTest");
-		wc.registerServlet(new ServerTestServlet(), new String[] { "/servertest" }, dict, ctx); 
-		
+		wc.registerServlet(new ServerTestServlet(),
+				new String[] { "/servertest" }, dict, ctx);
+
 		dict = new Hashtable<String, String>();
 		dict.put("filter-name", "springSecurityFilterChain");
-		
-		wc.registerFilter(new DelegatingFilterProxy(), new String[] { "/service/*" }, null, dict, ctx);
-		
+
+		wc.registerFilter(new DelegatingFilterProxy(),
+				new String[] { "/service/*" }, null, dict, ctx);
+
 	}
 
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
-		
+
 	}
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
