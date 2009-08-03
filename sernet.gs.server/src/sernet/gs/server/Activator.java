23ea2538e4c7290570018e8c787ab235830a1633
Robert Schuster <r.schuster@tarent.de>
2009-08-03 12:11:29 +0000

    - added copyright header - start osgi extender and pax-web programmatically
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@366 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.server/src/sernet/gs/server/Activator.java
+++ sernet.gs.server/src/sernet/gs/server/Activator.java
@@ -1,10 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Robert Schuster <r.schuster@tarent.de>.
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 
+ * of the License, or (at your option) any later version.
+ *     This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU General Public License for more details.
+ *     You should have received a copy of the GNU General Public 
+ * License along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Robert Schuster <r.schuster@tarent.de> - initial API and implementation
+ ******************************************************************************/
 package sernet.gs.server;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
 
+import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.Plugin;
 import org.ops4j.pax.web.service.WebContainer;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.http.HttpContext;
@@ -15,12 +34,18 @@ import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class Activator extends Plugin {
+	
+	Logger log = Logger.getLogger(Activator.class);
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "veriniceServer";
 
 	// The shared instance
 	private static Activator plugin;
+	
+	private static final String OSGI_EXTENDER_SYMBOLIC_NAME = "org.springframework.osgi.extender";
+	
+	private static final String PAX_WEB_SYMBOLIC_NAME = "org.ops4j.pax.web.pax-web-bundle";
 	
 	/**
 	 * The constructor
@@ -32,13 +57,48 @@ public class Activator extends Plugin {
 		super.start(context);
 		
 		plugin = this;
-				
+
+		// Starts the Spring OSGi Extender which provides registering the Spring namespace handlers. If you get
+		// exception saying there is no schema for Spring security, then the OSGi extender is not running.
+		for (Bundle bundle : context.getBundles())
+		{
+			if (bundle.getSymbolicName().equals(OSGI_EXTENDER_SYMBOLIC_NAME))
+			{
+				if (bundle.getState() == Bundle.INSTALLED
+						|| bundle.getState() == Bundle.RESOLVED)
+				{
+					log.debug("Manually starting Spring's OSGi Extender");
+					bundle.start();
+				}
+			}
+		}
+
 		ServiceReference sr = context.getServiceReference(WebContainer.class.getName());
 		
+		// Starts the Pax Web Bundle. This makes the HTTP service available.
 		if (sr == null)
 		{
-			throw new IllegalStateException("Http Service is not available.");
+			for (Bundle bundle : context.getBundles())
+			{
+				if (bundle.getSymbolicName().equals(PAX_WEB_SYMBOLIC_NAME))
+				{
+					if (bundle.getState() == Bundle.INSTALLED
+							|| bundle.getState() == Bundle.RESOLVED)
+					{
+						log.debug("Manually starting Pax Web Http Service.");
+						bundle.start();
+					}
+					else
+						throw new IllegalStateException("pax-web bundle is not in a proper state to get started.");
+
+					sr = context.getServiceReference(WebContainer.class.getName());
+					if (sr == null)
+						throw new IllegalStateException("pax-web bundle was started but there is still no http service available. Giving up.");
+				}
+			}
 		}
+		if (sr == null)
+			throw new IllegalStateException("No http service. Giving up.");
 		
 		WebContainer wc = (WebContainer) context.getService(sr);
 		
