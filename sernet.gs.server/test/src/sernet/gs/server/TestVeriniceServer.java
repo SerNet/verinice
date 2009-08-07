1989b6ac15d111bdd7ac55fd8d90ab356ab5ad03
Robert Schuster <r.schuster@tarent.de>
2009-08-07 11:06:23 +0000

    - updated test to newer API
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@409 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.server/test/src/sernet/gs/server/TestVeriniceServer.java
+++ sernet.gs.server/test/src/sernet/gs/server/TestVeriniceServer.java
@@ -16,19 +16,13 @@
  *     Alexander Koderman <ak@sernet.de> - initial API and implementation
  ******************************************************************************/
 package sernet.gs.server;
-import java.io.File;
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import junit.framework.TestCase;
 
-import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpState;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.httpclient.auth.AuthScope;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.access.BeanFactoryLocator;
 import org.springframework.beans.factory.access.BeanFactoryReference;
 import org.springframework.beans.factory.access.SingletonBeanFactoryLocator;
@@ -38,29 +32,16 @@ import sernet.gs.ui.rcp.main.bsi.model.BSIModel;
 import sernet.gs.ui.rcp.main.bsi.model.MassnahmenUmsetzung;
 import sernet.gs.ui.rcp.main.bsi.model.NKKategorie;
 import sernet.gs.ui.rcp.main.bsi.model.NetzKomponente;
-import sernet.gs.ui.rcp.main.common.model.BuildInput;
-import sernet.gs.ui.rcp.main.common.model.CnAElementFactory;
-import sernet.gs.ui.rcp.main.common.model.CnAElementHome;
 import sernet.gs.ui.rcp.main.common.model.CnATreeElement;
 import sernet.gs.ui.rcp.main.common.model.HitroUtil;
-import sernet.gs.ui.rcp.main.common.model.NullMonitor;
-import sernet.gs.ui.rcp.main.common.model.configuration.Configuration;
-import sernet.gs.ui.rcp.main.connect.HibernateBaseDao;
 import sernet.gs.ui.rcp.main.service.IAuthService;
 import sernet.gs.ui.rcp.main.service.ICommandService;
-import sernet.gs.ui.rcp.main.service.ServiceFactory;
-import sernet.gs.ui.rcp.main.service.WhereAmIUtil;
 import sernet.gs.ui.rcp.main.service.commands.CommandException;
-import sernet.gs.ui.rcp.main.service.commands.ICommand;
-import sernet.gs.ui.rcp.main.service.crudcommands.CreateConfiguration;
 import sernet.gs.ui.rcp.main.service.crudcommands.LoadBSIModel;
-import sernet.gs.ui.rcp.main.service.crudcommands.LoadBSIModelForTreeView;
 import sernet.gs.ui.rcp.main.service.crudcommands.LoadCnAElementByType;
-import sernet.gs.ui.rcp.main.service.crudcommands.LoadConfiguration;
 import sernet.gs.ui.rcp.main.service.crudcommands.LoadMassnahmenTitles;
 import sernet.gs.ui.rcp.main.service.crudcommands.SaveElement;
 import sernet.gs.ui.rcp.main.service.grundschutzparser.LoadBausteine;
-import sernet.gs.ui.rcp.main.service.taskcommands.FindAllTags;
 
 
 public class TestVeriniceServer extends TestCase {
@@ -70,13 +51,6 @@ public class TestVeriniceServer extends TestCase {
 
 	public void setUp() throws Exception {
 		super.setUp();
-		
-		// basic Verinice client setup:
-		// use remote service to execute commands:
-		ServiceFactory.setService(ServiceFactory.REMOTE);
-		
-		// tell me where to find HitroUI configuration and other stuff:
-		WhereAmIUtil.setLocation(WhereAmIUtil.LOCATION_CLIENT);
 		
 		// initialize HitroUI type factory:
 		HitroUtil.getInstance().init("http://localhost:2010/veriniceserver");
