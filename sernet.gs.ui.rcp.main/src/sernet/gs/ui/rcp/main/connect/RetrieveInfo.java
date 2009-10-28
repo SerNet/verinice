f0c0044f9384ca6f3c99e08019ee995f0f8427c3
Daniel Murygin <dm@sernet.de>
2009-10-28 13:41:12 +0000

    Determined by RetrieveInfo retrieve will join references of CnATreeElement
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@719 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

---
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/connect/RetrieveInfo.java
@@ -0,0 +1,176 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Daniel Murygin <dm@sernet.de>.
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 
+ * of the License, or (at your option) any later version.
+ *     This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU General Public License for more details.
+ *     You should have received a copy of the GNU General Public 
+ * License along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Daniel Murygin <dm@sernet.de> - initial API and implementation
+ ******************************************************************************/
+package sernet.gs.ui.rcp.main.connect;
+
+import sernet.gs.ui.rcp.main.common.model.CnATreeElement;
+
+
+/**
+ * Parameter of Method retrieve in {@link IBaseDao}.
+ * Determined by RetrieveInfo retrieve will join references of {@link CnATreeElement}.
+ * 
+ * @author Daniel Murygin <dm@sernet.de>
+ */
+public class RetrieveInfo {
+
+	boolean properties = false;
+	
+	boolean linksUp = false;
+	
+	boolean linksUpProperties = false;
+	
+	boolean linksDown = false;
+	
+	boolean linksDownProperties = false;
+	
+	boolean children = false;
+	
+	boolean childrenProperties = false;
+	
+	boolean innerJoin = false;
+
+	public RetrieveInfo() {
+		super();
+	}
+
+	/**
+	 * @return true if properties are joined and retrieved
+	 */
+	public boolean isProperties() {
+		return properties;
+	}
+
+	public RetrieveInfo setProperties(boolean properties) {
+		this.properties = properties;
+		return this;
+	}
+
+	/**
+	 * @return true if links-up are joined and retrieved
+	 */
+	public boolean isLinksUp() {
+		return linksUp;
+	}
+
+	public RetrieveInfo setLinksUp(boolean linksUp) {
+		this.linksUp = linksUp;
+		return this;
+	}
+
+	/**
+	 * @return true if properties of links-up are joined and retrieved
+	 */
+	public boolean isLinksUpProperties() {
+		return linksUpProperties;
+	}
+
+	public RetrieveInfo setLinksUpProperties(boolean linksUpProperties) {
+		this.linksUpProperties = linksUpProperties;
+		return this;
+	}
+
+	/**
+	 * @return true if links-down are joined and retrieved
+	 */
+	public boolean isLinksDown() {
+		return linksDown;
+	}
+
+	public RetrieveInfo setLinksDown(boolean linksDown) {
+		this.linksDown = linksDown;
+		return this;
+	}
+
+	/**
+	 * @return true if properties of links-down are joined and retrieved
+	 */
+	public boolean isLinksDownProperties() {
+		return linksDownProperties;
+	}
+
+	public RetrieveInfo setLinksDownProperties(boolean linksDownProperties) {
+		this.linksDownProperties = linksDownProperties;
+		return this;
+	}
+	
+	/**
+	 * @return true if children are joined and retrieved
+	 */
+	public boolean isChildren() {
+		return children;
+	}
+
+	public RetrieveInfo setChildren(boolean children) {
+		this.children = children;
+		return this;
+	}
+	
+	/**
+	 * @return true if properties of children are joined and retrieved
+	 */
+	public boolean isChildrenProperties() {
+		return childrenProperties;
+	}
+
+	public RetrieveInfo setChildrenProperties(boolean childrenProperties) {
+		this.childrenProperties = childrenProperties;
+		return this;
+	}
+	
+	/**
+	 * @return true if inner joins are used
+	 */
+	public boolean isInnerJoin() {
+		return innerJoin;
+	}
+
+	public RetrieveInfo setInnerJoin(boolean innerJoin) {
+		this.innerJoin = innerJoin;
+		return this;
+	}
+
+	public String toString() {
+		StringBuffer sb = new StringBuffer("RetrieveInfo - ");
+		if(properties) {
+			sb.append( " properties");
+		}
+		if(linksUp) {
+			sb.append(" linksUp");
+		}
+		if(linksUpProperties) {
+			sb.append(" linksUpProperties").append(linksUpProperties);
+		}
+		if(linksDown) {
+			sb.append(" linksDown");
+		}
+		if(linksDownProperties) {
+			sb.append(" linksDownProperties");
+		}
+		if(children) {
+			sb.append(" children");
+		}
+		if(childrenProperties) {
+			sb.append(" childrenProperties");
+		}
+		if(childrenProperties) {
+			sb.append(" innerJoin");
+		}
+		return sb.toString();
+	}
+	
+}
