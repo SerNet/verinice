f745ff70ee96d748986f03cdb9ea35c790dc9f36
Daniel Murygin <dm@sernet.de>
2009-10-28 13:30:03 +0000

    loads the accessory links (linksDown) for a CnATreeElement
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@718 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

---
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/service/crudcommands/LoadLinksDown.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Daniel Murygin <dm@sernet.de>.
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 
+ * of the License, or (at your option) any later version.
+ *     This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU General Public License for more details.
+ *     You should have received a copy of the GNU General Public 
+ * License along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Daniel Murygin <dm@sernet.de> - initial API and implementation
+ ******************************************************************************/
+package sernet.gs.ui.rcp.main.service.crudcommands;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+import sernet.gs.ui.rcp.main.common.model.CnALink;
+import sernet.gs.ui.rcp.main.common.model.CnATreeElement;
+import sernet.gs.ui.rcp.main.connect.IBaseDao;
+import sernet.gs.ui.rcp.main.connect.RetrieveInfo;
+import sernet.gs.ui.rcp.main.service.commands.GenericCommand;
+
+/**
+ * This command loads the accessory links (linksDown) for a CnATreeElement.
+ * To excute 
+ * <ul>
+ * <li>create this command by passing a CnATreeElement</li>
+ * <li>call method <code>execute()</code></li>
+ * <li>get links by calling method <code>getLinksDown()</code></li>
+ * </ul>
+ * 
+ * @author Daniel Murygin <dm@sernet.de>
+ */
+public class LoadLinksDown extends GenericCommand {
+
+	private static final long serialVersionUID = 130016664627158967L;
+
+	private static final Logger LOG = Logger.getLogger(LoadLinksDown.class);
+	
+	private CnATreeElement parent;
+	
+	private Set<CnALink> linksDown;
+	
+	/**
+	 * Creates a new command to load the accessory links of <code>parent</code>.
+	 * 
+	 * @param parent A CnATreeElement, must not be null
+	 */
+	public LoadLinksDown(CnATreeElement parent) {
+		super();
+		this.parent = parent;
+	}
+
+	/**
+	 * Executes this command.
+	 * Loads accessory links of <code>CnATreeElement parent</code> by calling dao method retrieve.
+	 * 
+	 * @see sernet.gs.ui.rcp.main.service.commands.ICommand#execute()
+	 */
+	public void execute() {
+		if(this.parent==null) {
+			LOG.error("Can not execute command, parent is null");
+			throw new RuntimeException("Can not execute command, parent is null");
+		}
+		if(this.parent.getDbId()==null) {
+			LOG.error("Can not execute command, dbid of parent is null");
+			throw new RuntimeException("Can not execute command, dbif of parent is null");
+		}
+		
+		IBaseDao<? extends CnATreeElement, Serializable> dao = getDaoFactory().getDAO(this.parent.getClass());
+		
+		RetrieveInfo ri = new RetrieveInfo();
+		ri.setLinksDown(true).setLinksDownProperties(true);
+		CnATreeElement parentWithLinksDown = dao.retrieve(parent.getDbId(),ri);
+		linksDown = parentWithLinksDown.getLinksDown();
+	}
+
+	/**
+	 * Returns the accessory links of <code>CnATreeElement parent</code>.
+	 * Call exceute before this method. Otherwise null is returned.
+	 * 
+	 * @return the accessory links of <code>CnATreeElement parent</code>
+	 */
+	public Set<CnALink> getLinksDown() {
+		return linksDown;
+	}
+
+}
