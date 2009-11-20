042cac0207b64adc3293162f2afb286873eea9a0
Daniel Murygin <dm@sernet.de>
2009-11-20 10:50:37 +0000

    ISO 27001 stati
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@773 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.server/src/sernet/gs/web/UmsetzungConverter.java
+++ sernet.gs.server/src/sernet/gs/web/UmsetzungConverter.java
@@ -17,6 +17,17 @@ public class UmsetzungConverter implements Converter {
 	public static final String TEILWEISE = "Teilweise";
 	
 	public static final String UNBEARBEITET = "Unbearbeitet";
+
+	private static final String ESTABLISHED = "Level 3: Established";
+
+	private static final String MANAGED = "Level 2: Managed";
+
+	private static final String OPTIMIZING = "Level 5: Optimizing";
+
+	private static final String PERFORMED = "Level 1: Performed";
+
+	private static final String PREDICTABLE = "Level 4: Predictable";
+	
 	
 	@Override
 	public Object getAsObject(FacesContext arg0, UIComponent arg1, String text) {
@@ -31,6 +42,16 @@ public class UmsetzungConverter implements Converter {
 			value=MassnahmenUmsetzung.P_UMSETZUNG_TEILWEISE;
 		} else if(UNBEARBEITET.equals(text)) {
 			value=MassnahmenUmsetzung.P_UMSETZUNG_UNBEARBEITET;
+		} else if(ESTABLISHED.equals(text)) {
+			value=MassnahmenUmsetzung.P_UMSETZUNG_ESTABLISHED;
+		} else if(MANAGED.equals(text)) {
+			value=MassnahmenUmsetzung.P_UMSETZUNG_MANAGED;
+		} else if(OPTIMIZING.equals(text)) {
+			value=MassnahmenUmsetzung.P_UMSETZUNG_OPTIMIZING;
+		} else if(PERFORMED.equals(text)) {
+			value=MassnahmenUmsetzung.P_UMSETZUNG_PERFORMED;
+		} else if(PREDICTABLE.equals(text)) {
+			value=MassnahmenUmsetzung.P_UMSETZUNG_PREDICTABLE;
 		}
 		return value;
 	}
@@ -48,6 +69,16 @@ public class UmsetzungConverter implements Converter {
 			text=TEILWEISE;
 		} else if(MassnahmenUmsetzung.P_UMSETZUNG_UNBEARBEITET.equals(value)) {
 			text=UNBEARBEITET;
+		} else if(MassnahmenUmsetzung.P_UMSETZUNG_ESTABLISHED.equals(value)) {
+			text=ESTABLISHED;
+		} else if(MassnahmenUmsetzung.P_UMSETZUNG_MANAGED.equals(value)) {
+			text=MANAGED;
+		} else if(MassnahmenUmsetzung.P_UMSETZUNG_OPTIMIZING.equals(value)) {
+			text=OPTIMIZING;
+		} else if(MassnahmenUmsetzung.P_UMSETZUNG_PERFORMED.equals(value)) {
+			text=PERFORMED;
+		} else if(MassnahmenUmsetzung.P_UMSETZUNG_PREDICTABLE.equals(value)) {
+			text=PREDICTABLE;
 		}
 		return text;
 	}
