925d780b04b4905f697edecb9987768153123514
Daniel Murygin <dm@sernet.de>
2010-01-20 15:15:16 +0000

    ASSIGNED - bug 51: SM View http://localhost:10081/cgi-bin/bugzilla/show_bug.cgi?id=51
    ISO 27k elements added to elementbuilders
    getter for ISO27kModel added 
    
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@926 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/common/model/CnAElementFactory.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/common/model/CnAElementFactory.java
@@ -17,14 +17,17 @@
  ******************************************************************************/
 package sernet.gs.ui.rcp.main.common.model;
 
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
 
 import sernet.gs.model.Baustein;
 import sernet.gs.ui.rcp.main.Activator;
+import sernet.gs.ui.rcp.main.CnAWorkspace;
 import sernet.gs.ui.rcp.main.ExceptionUtil;
 import sernet.gs.ui.rcp.main.bsi.model.Anwendung;
 import sernet.gs.ui.rcp.main.bsi.model.BSIModel;
@@ -32,6 +35,7 @@ import sernet.gs.ui.rcp.main.bsi.model.BausteinUmsetzung;
 import sernet.gs.ui.rcp.main.bsi.model.BausteinVorschlag;
 import sernet.gs.ui.rcp.main.bsi.model.Client;
 import sernet.gs.ui.rcp.main.bsi.model.Gebaeude;
+import sernet.gs.ui.rcp.main.bsi.model.IBSIStrukturElement;
 import sernet.gs.ui.rcp.main.bsi.model.ITVerbund;
 import sernet.gs.ui.rcp.main.bsi.model.NetzKomponente;
 import sernet.gs.ui.rcp.main.bsi.model.Person;
@@ -40,12 +44,30 @@ import sernet.gs.ui.rcp.main.bsi.model.Server;
 import sernet.gs.ui.rcp.main.bsi.model.SonstIT;
 import sernet.gs.ui.rcp.main.bsi.model.SubtypenZielobjekte;
 import sernet.gs.ui.rcp.main.bsi.model.TelefonKomponente;
+import sernet.gs.ui.rcp.main.bsi.views.actions.BSIModelViewOpenDBAction.StatusResult;
+import sernet.gs.ui.rcp.main.service.ICommandService;
 import sernet.gs.ui.rcp.main.service.ServiceFactory;
 import sernet.gs.ui.rcp.main.service.commands.CommandException;
 import sernet.gs.ui.rcp.main.service.commands.RuntimeCommandException;
 import sernet.gs.ui.rcp.main.service.crudcommands.CreateAnwendung;
 import sernet.gs.ui.rcp.main.service.crudcommands.CreateITVerbund;
+import sernet.gs.ui.rcp.main.service.crudcommands.SaveElement;
 import sernet.gs.ui.rcp.main.service.crudcommands.UpdateMultipleElements;
+import sernet.verinice.iso27k.command.LoadModel;
+import sernet.verinice.iso27k.model.Asset;
+import sernet.verinice.iso27k.model.AssetGroup;
+import sernet.verinice.iso27k.model.Audit;
+import sernet.verinice.iso27k.model.AuditGroup;
+import sernet.verinice.iso27k.model.Control;
+import sernet.verinice.iso27k.model.ControlGroup;
+import sernet.verinice.iso27k.model.ExceptionGroup;
+import sernet.verinice.iso27k.model.IISO27kElement;
+import sernet.verinice.iso27k.model.IISO27kRoot;
+import sernet.verinice.iso27k.model.ISO27KModel;
+import sernet.verinice.iso27k.model.Organization;
+import sernet.verinice.iso27k.model.PersonGroup;
+import sernet.verinice.iso27k.model.Requirement;
+import sernet.verinice.iso27k.model.RequirementGroup;
 
 /**
  * Factory for all model elements. Contains typed factories for sub-elements.
@@ -69,16 +91,22 @@ import sernet.gs.ui.rcp.main.service.crudcommands.UpdateMultipleElements;
  */
 public class CnAElementFactory {
 
+	private final Logger log = Logger.getLogger(CnAElementFactory.class);
+	
 	private static List<IModelLoadListener> listeners = new ArrayList<IModelLoadListener>();
 
 	private static CnAElementFactory instance;
 
-	private HashMap<String, IElementBuilder> bsiElementbuilders = new HashMap<String, IElementBuilder>();
+	private HashMap<String, IElementBuilder> elementbuilders = new HashMap<String, IElementBuilder>();
 
 	private CnAElementHome dbHome;
 
 	private static BSIModel loadedModel;
 
+	private static ISO27KModel isoModel;
+
+	private ICommandService commandService;
+	
 	private interface IElementBuilder<T extends CnATreeElement, U> {
 		public T build(CnATreeElement container, BuildInput<U> input) throws Exception;
 	}
@@ -96,7 +124,7 @@ public class CnAElementFactory {
 	private CnAElementFactory() {
 		dbHome = CnAElementHome.getInstance();
 
-		bsiElementbuilders.put(Gebaeude.TYPE_ID, new IElementBuilder() {
+		elementbuilders.put(Gebaeude.TYPE_ID, new IElementBuilder() {
 			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
 				Gebaeude child = dbHome.save(container, Gebaeude.class);
 				container.addChild(child);
@@ -105,7 +133,7 @@ public class CnAElementFactory {
 			}
 		});
 
-		bsiElementbuilders.put(Client.TYPE_ID, new IElementBuilder() {
+		elementbuilders.put(Client.TYPE_ID, new IElementBuilder() {
 			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
 				Client child = dbHome.save(container, Client.class);
 				container.addChild(child);
@@ -114,7 +142,7 @@ public class CnAElementFactory {
 			}
 		});
 
-		bsiElementbuilders.put(SonstIT.TYPE_ID, new IElementBuilder() {
+		elementbuilders.put(SonstIT.TYPE_ID, new IElementBuilder() {
 			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
 				SonstIT child = dbHome.save(container, SonstIT.class);
 				container.addChild(child);
@@ -123,7 +151,7 @@ public class CnAElementFactory {
 			}
 		});
 
-		bsiElementbuilders.put(Server.TYPE_ID, new IElementBuilder() {
+		elementbuilders.put(Server.TYPE_ID, new IElementBuilder() {
 			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
 				Server child = dbHome.save(container, Server.class);
 				container.addChild(child);
@@ -132,7 +160,7 @@ public class CnAElementFactory {
 			}
 		});
 
-		bsiElementbuilders.put(TelefonKomponente.TYPE_ID, new IElementBuilder() {
+		elementbuilders.put(TelefonKomponente.TYPE_ID, new IElementBuilder() {
 			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
 				TelefonKomponente child = dbHome.save(container, TelefonKomponente.class);
 				container.addChild(child);
@@ -141,7 +169,7 @@ public class CnAElementFactory {
 			}
 		});
 
-		bsiElementbuilders.put(Raum.TYPE_ID, new IElementBuilder() {
+		elementbuilders.put(Raum.TYPE_ID, new IElementBuilder() {
 			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
 				Raum child = dbHome.save(container, Raum.class);
 				container.addChild(child);
@@ -150,7 +178,7 @@ public class CnAElementFactory {
 			}
 		});
 
-		bsiElementbuilders.put(NetzKomponente.TYPE_ID, new IElementBuilder() {
+		elementbuilders.put(NetzKomponente.TYPE_ID, new IElementBuilder() {
 			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
 				NetzKomponente child = dbHome.save(container, NetzKomponente.class);
 				container.addChild(child);
@@ -159,7 +187,7 @@ public class CnAElementFactory {
 			}
 		});
 
-		bsiElementbuilders.put(Person.TYPE_ID, new IElementBuilder() {
+		elementbuilders.put(Person.TYPE_ID, new IElementBuilder() {
 			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
 				Person child = dbHome.save(container, Person.class);
 				container.addChild(child);
@@ -168,7 +196,7 @@ public class CnAElementFactory {
 			}
 		});
 
-		bsiElementbuilders.put(Anwendung.TYPE_ID, new IElementBuilder() {
+		elementbuilders.put(Anwendung.TYPE_ID, new IElementBuilder() {
 			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
 
 				Logger.getLogger(this.getClass()).debug("Creating new Anwendung in " + container);
@@ -183,7 +211,7 @@ public class CnAElementFactory {
 			}
 		});
 
-		bsiElementbuilders.put(BausteinUmsetzung.TYPE_ID, new IElementBuilder<BausteinUmsetzung, Baustein>() {
+		elementbuilders.put(BausteinUmsetzung.TYPE_ID, new IElementBuilder<BausteinUmsetzung, Baustein>() {
 			public BausteinUmsetzung build(CnATreeElement container, BuildInput<Baustein> input) throws Exception {
 
 				BausteinUmsetzung bu = dbHome.save(container, input.getInput());
@@ -196,7 +224,7 @@ public class CnAElementFactory {
 			}
 		});
 
-		bsiElementbuilders.put(ITVerbund.TYPE_ID, new IElementBuilder() {
+		elementbuilders.put(ITVerbund.TYPE_ID, new IElementBuilder() {
 			public ITVerbund build(CnATreeElement container, BuildInput input) throws Exception {
 
 				Logger.getLogger(this.getClass()).debug("Creating new ITVerbund in " + container);
@@ -209,6 +237,118 @@ public class CnAElementFactory {
 			}
 		});
 
+		// ISO 27000 builders
+		elementbuilders.put(Organization.TYPE_ID, new IElementBuilder() {
+			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
+				Organization child = dbHome.save(container, Organization.class);
+				container.addChild(child);
+				child.setParent(container);
+				return child;
+			}
+		});
+		
+		elementbuilders.put(AssetGroup.TYPE_ID, new IElementBuilder() {
+			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
+				AssetGroup child = dbHome.save(container, AssetGroup.class);
+				container.addChild(child);
+				child.setParent(container);
+				return child;
+			}
+		});
+		elementbuilders.put(Asset.TYPE_ID, new IElementBuilder() {
+			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
+				Asset child = dbHome.save(container, Asset.class);
+				container.addChild(child);
+				child.setParent(container);
+				return child;
+			}
+		});
+		
+		elementbuilders.put(PersonGroup.TYPE_ID, new IElementBuilder() {
+			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
+				PersonGroup child = dbHome.save(container, PersonGroup.class);
+				container.addChild(child);
+				child.setParent(container);
+				return child;
+			}
+		});
+		elementbuilders.put(sernet.verinice.iso27k.model.PersonIso.TYPE_ID, new IElementBuilder() {
+			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
+				sernet.verinice.iso27k.model.PersonIso child = dbHome.save(container, sernet.verinice.iso27k.model.PersonIso.class);
+				container.addChild(child);
+				child.setParent(container);
+				return child;
+			}
+		});
+		
+		elementbuilders.put(AuditGroup.TYPE_ID, new IElementBuilder() {
+			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
+				AuditGroup child = dbHome.save(container, AuditGroup.class);
+				container.addChild(child);
+				child.setParent(container);
+				return child;
+			}
+		});
+		elementbuilders.put(Audit.TYPE_ID, new IElementBuilder() {
+			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
+				Audit child = dbHome.save(container, Audit.class);
+				container.addChild(child);
+				child.setParent(container);
+				return child;
+			}
+		});
+			
+		elementbuilders.put(ControlGroup.TYPE_ID, new IElementBuilder() {
+			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
+				ControlGroup child = dbHome.save(container, ControlGroup.class);
+				container.addChild(child);
+				child.setParent(container);
+				return child;
+			}
+		});
+		elementbuilders.put(Control.TYPE_ID, new IElementBuilder() {
+			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
+				Control child = dbHome.save(container, Control.class);
+				container.addChild(child);
+				child.setParent(container);
+				return child;
+			}
+		});
+		
+		elementbuilders.put(ExceptionGroup.TYPE_ID, new IElementBuilder() {
+			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
+				ExceptionGroup child = dbHome.save(container, ExceptionGroup.class);
+				container.addChild(child);
+				child.setParent(container);
+				return child;
+			}
+		});
+		elementbuilders.put(sernet.verinice.iso27k.model.Exception.TYPE_ID, new IElementBuilder() {
+			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
+				sernet.verinice.iso27k.model.Exception child = dbHome.save(container, sernet.verinice.iso27k.model.Exception.class);
+				container.addChild(child);
+				child.setParent(container);
+				return child;
+			}
+		});
+		
+		elementbuilders.put(RequirementGroup.TYPE_ID, new IElementBuilder() {
+			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
+				RequirementGroup child = dbHome.save(container, RequirementGroup.class);
+				container.addChild(child);
+				child.setParent(container);
+				return child;
+			}
+		});
+		elementbuilders.put(Requirement.TYPE_ID, new IElementBuilder() {
+			public CnATreeElement build(CnATreeElement container, BuildInput input) throws Exception {
+				Requirement child = dbHome.save(container, Requirement.class);
+				container.addChild(child);
+				child.setParent(container);
+				return child;
+			}
+		});
+		
 	}
 
 	public static CnAElementFactory getInstance() {
@@ -227,15 +367,16 @@ public class CnAElementFactory {
 	 */
 	@SuppressWarnings("unchecked")
 	public CnATreeElement saveNew(CnATreeElement container, String buildableTypeId, BuildInput input, boolean fireUpdates) throws Exception {
-		IElementBuilder builder = bsiElementbuilders.get(buildableTypeId);
-		if (builder == null)
+		IElementBuilder builder = elementbuilders.get(buildableTypeId);
+		if (builder == null) {
 			throw new Exception("Konnte Element nicht erzeugen.");
+		}
 		CnATreeElement child = builder.build(container, input);
 
 		// notify all listeners:
 		if (fireUpdates) {
-			getLoadedModel().childAdded(container, child);
-			CnAElementFactory.getLoadedModel().databaseChildAdded(child);
+			CnAElementFactory.getModel(child).childAdded(container, child);
+			CnAElementFactory.getModel(child).databaseChildAdded(child);
 		}
 		return child;
 	}
@@ -277,6 +418,70 @@ public class CnAElementFactory {
 	 */
 	public boolean isDbOpen() {
 		return dbHome != null && dbHome.isOpen();
+	}
+	
+	/**
+	 * Returns the model for a {@link CnATreeElement}
+	 * 
+	 * @param element returned model belongs to this element
+	 * @return the model for an element
+	 */
+	public static CnATreeElement getModel(CnATreeElement element) {
+		CnATreeElement model = null;
+		if(element instanceof IBSIStrukturElement) {
+			model = CnAElementFactory.getLoadedModel();
+		}
+		if(element instanceof IISO27kElement) {
+			model = CnAElementFactory.getInstance().getISO27kModel();
+		}
+		return model;
+	}
+	
+	public ISO27KModel getISO27kModel() {
+		if(isoModel==null) {
+			isoModel = loadIsoModel();
+			if(isoModel==null) {
+				isoModel = createIsoModel();
+			}
+		}
+		return isoModel;
+	}
+
+	/**
+	 * @return
+	 */
+	private ISO27KModel loadIsoModel() {
+		try {
+			CnAWorkspace.getInstance().createDatabaseConfig();
+			
+			Activator.inheritVeriniceContextState();
+			Activator.checkDbVersion();
+			
+			LoadModel loadModel = new LoadModel();
+			loadModel = getCommandService().executeCommand(loadModel);
+			isoModel = loadModel.getModel();			
+		} catch(Exception e) {
+			log.error("Error while loading ISO27KModel", e);
+		}
+		return isoModel;
+	}
+	
+	/**
+	 * @return
+	 */
+	private ISO27KModel createIsoModel() {
+		try {
+			isoModel=new ISO27KModel();
+			SaveElement<ISO27KModel> saveCommand = new SaveElement<ISO27KModel>(isoModel);
+			saveCommand = getCommandService().executeCommand(saveCommand);
+			isoModel = saveCommand.getElement();
+			if (log.isInfoEnabled()) {
+				log.info("ISO27KModel created");
+			}
+		} catch(Exception e) {
+			log.error("Error while creating ISO27KModel", e);
+		}
+		return isoModel;
 	}
 
 	public BSIModel loadOrCreateModel(IProgress monitor) throws Exception {
@@ -333,6 +538,24 @@ public class CnAElementFactory {
 		} catch (Exception e) {
 			ExceptionUtil.log(e, "Konnte Modell nicht aus der Datenbank erneuern..");
 		}
+	}
+	
+	private ICommandService getCommandService() {
+		if(commandService==null) {
+			commandService = createCommandService();
+		}
+		return commandService;
+	}
+	
+	private ICommandService createCommandService() {
+		try {
+			ServiceFactory.openCommandService();
+		} catch (MalformedURLException e) {
+			log.error("Error while opening command service", e);
+			throw new RuntimeException("Error while opening command service", e);
+		}
+		commandService = ServiceFactory.lookupCommandService();
+		return commandService;
 	}
 
 }
