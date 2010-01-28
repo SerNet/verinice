8bf5019df8d91b2254c0fa16fdb7c21d7ffed928
Daniel Murygin <dm@sernet.de>
2010-01-28 15:13:16 +0000

    ASSIGNED - bug 15: Import and work with catalogs from CSV files http://localhost:10081/cgi-bin/bugzilla/show_bug.cgi?id=15
    chapters from cataloges are added as ControlGroups
    
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@983 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.ui.rcp.main/src/sernet/verinice/iso27k/rcp/ControlTransformOperation.java
+++ sernet.gs.ui.rcp.main/src/sernet/verinice/iso27k/rcp/ControlTransformOperation.java
@@ -24,24 +24,20 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.MessageBox;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.internal.UIPlugin;
 
 import sernet.gs.ui.rcp.main.Activator;
 import sernet.gs.ui.rcp.main.bsi.dnd.DNDItems;
 import sernet.gs.ui.rcp.main.common.model.CnAElementFactory;
+import sernet.gs.ui.rcp.main.common.model.CnATreeElement;
 import sernet.gs.ui.rcp.main.service.ICommandService;
 import sernet.gs.ui.rcp.main.service.ServiceFactory;
+import sernet.gs.ui.rcp.main.service.commands.CommandException;
 import sernet.gs.ui.rcp.main.service.crudcommands.SaveElement;
 import sernet.verinice.iso27k.model.Control;
 import sernet.verinice.iso27k.model.ControlGroup;
 import sernet.verinice.iso27k.model.Group;
 import sernet.verinice.iso27k.service.IItem;
-import sernet.verinice.iso27k.service.ITransformer;
 import sernet.verinice.iso27k.service.ItemControlTransformer;
 
 /**
@@ -80,29 +76,14 @@ public class ControlTransformOperation implements IRunnableWithProgress {
 	public void run(IProgressMonitor monitor)  {
 		try {	
 			Activator.inheritVeriniceContextState();
-			List<IItem> itemList = extractChildren(DNDItems.getItems());
-			numberOfControls = itemList.size();
+			this.numberOfControls = 0;
+			List<IItem> itemList = createInsertList(DNDItems.getItems());
 			StringBuilder sb = new StringBuilder();
 			sb.append("Transforming ").append(numberOfControls).append(" catalog items to controls.");
 			monitor.beginTask(sb.toString(), numberOfControls);
-			ITransformer<IItem,Control> transformer = new ItemControlTransformer(itemList);
-			List<Control> controlList = transformer.getResultList();
 			int i = 0;
-			for (Control control : controlList) {
-				if(monitor.isCanceled()) {
-					LOG.warn("Transforming canceled. " + i + " of " + numberOfControls + " items transformed.");
-					break;
-				}
-				monitor.setTaskName(getText(numberOfControls,i,control.getTitle()));
-				selectedGroup.addChild(control);
-				control.setParent(selectedGroup);
-				SaveElement<Control> saveCommand = new SaveElement<Control>(control);
-				saveCommand = getCommandService().executeCommand(saveCommand);
-				control = saveCommand.getElement();
-				control.setParent(selectedGroup);
-				CnAElementFactory.getModel(control).childAdded(selectedGroup, control);
-				CnAElementFactory.getModel(control).databaseChildAdded(control);
-				monitor.worked(i++);
+			for (IItem item : itemList) {				
+				insertItem(monitor, selectedGroup, item, i);
 			}		
 		} catch (Exception e) {
 			LOG.error("Error while transforming to control", e);
@@ -112,37 +93,86 @@ public class ControlTransformOperation implements IRunnableWithProgress {
 
 	}
 	
-	/**
-	 * Extracts all children from dragged items.
-	 * Returns a list with all sub-items of dragged items. 
-	 * Items with children are not included in the returned list.
-	 * Only "leaves" will be returned.
-	 * 
-	 * @param items A list with dragged items
-	 * @return A list with all sub-items of dragged items
-	 */
-	private List<IItem> extractChildren(List<IItem> itemDragList) {
-		List<IItem> itemList = new ArrayList<IItem>();
-		for (IItem item : itemDragList) {
-			extractChildren(item,itemList);
-		}
-		return itemList;
-	}
+	
 
 	/**
-	 * Recursive method to implement extractChildren(List<IItem> itemDragList)
-	 * 
+	 * @param monitor
+	 * @param group 
 	 * @param item
-	 * @param itemList
 	 */
-	private void extractChildren(final IItem item, List<IItem> itemList) {
-		if(item.getItems()==null || item.getItems().size()==0) {
-			itemList.add(item);
+	private void insertItem(IProgressMonitor monitor, Group group, IItem item, int i) {
+		if(monitor.isCanceled()) {
+			LOG.warn("Transforming canceled. " + i + " of " + numberOfControls + " items transformed.");
+			return;
+		}
+		SaveElement command = null;
+		CnATreeElement element = null;
+		if(item.getItems()!=null && item.getItems().size()>0) {
+			// create a group
+			element = ItemControlTransformer.transformToGroup(item);
+			monitor.setTaskName(getText(numberOfControls,i,element.getTitle()));
+			group.addChild(element);
+			element.setParent(group);
+			command = new SaveElement<ControlGroup>((ControlGroup) element);
+			
 		} else {
+			// create a control
+			element = ItemControlTransformer.transform(item);
+			monitor.setTaskName(getText(numberOfControls,i,element.getTitle()));
+			group.addChild(element);
+			element.setParent(group);
+			command = new SaveElement<Control>((Control) element);
+		}
+
+		try {
+			command = getCommandService().executeCommand(command);
+		} catch (CommandException e) {
+			LOG.error("Error while inserting control", e);
+			throw new RuntimeException("Error while inserting control", e);
+		}
+		
+		
+		element = (CnATreeElement) command.getElement();
+		element.setParent(group);
+		CnAElementFactory.getModel(element).childAdded(group, element);
+		CnAElementFactory.getModel(element).databaseChildAdded(element);
+		monitor.worked(i++);
+		i++;
+		if(item.getItems()!=null) {
 			for (IItem child : item.getItems()) {
-				extractChildren(child,itemList);
+				insertItem(monitor,(Group) element,child,i);
 			}
-		}	
+		}
+	}
+
+	private List<IItem> createInsertList(List<IItem> itemDragList) {
+		List<IItem> tempList = new ArrayList<IItem>();
+		List<IItem> insertList = new ArrayList<IItem>();
+		int depth = 0;
+		int removed = 0;
+		for (IItem item : itemDragList) {
+			createInsertList(item,tempList,insertList, depth, removed);
+		}
+		this.numberOfControls = tempList.size() - removed;
+		return insertList;
+	}
+
+	private void createInsertList(final IItem item, List<IItem> tempList, List<IItem> insertList, int depth, int removed) {
+		if(!tempList.contains(item)) {
+			tempList.add(item);
+			if(depth==0) {
+				insertList.add(item);
+			}
+			if(item.getItems()!=null) {
+				depth++;
+				for (IItem child : item.getItems()) {
+					createInsertList(child,tempList,insertList,depth,removed);
+				}
+			}
+		} else {
+			insertList.remove(item);
+			removed++;
+		}
 	}
 
 	/**
