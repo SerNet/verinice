0cd4fe603ff8f4bd19cbcd4c86b3202dfc05fc4f
Daniel Murygin <dm@sernet.de>
2010-01-28 15:14:42 +0000

    ASSIGNED - bug 15: Import and work with catalogs from CSV files http://localhost:10081/cgi-bin/bugzilla/show_bug.cgi?id=15
    imports
    
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@987 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.ui.rcp.main/src/sernet/verinice/iso27k/rcp/action/ControlDropPerformer.java
+++ sernet.gs.ui.rcp.main/src/sernet/verinice/iso27k/rcp/action/ControlDropPerformer.java
@@ -19,12 +19,9 @@
  ******************************************************************************/
 package sernet.verinice.iso27k.rcp.action;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
-import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.MessageDialogWithToggle;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.viewers.Viewer;
@@ -34,19 +31,12 @@ import org.eclipse.ui.part.ViewPart;
 import org.eclipse.ui.progress.IProgressService;
 
 import sernet.gs.ui.rcp.main.Activator;
-import sernet.gs.ui.rcp.main.bsi.dnd.DNDItems;
-import sernet.gs.ui.rcp.main.common.model.CnAElementFactory;
 import sernet.gs.ui.rcp.main.preferences.PreferenceConstants;
 import sernet.gs.ui.rcp.main.service.ICommandService;
 import sernet.gs.ui.rcp.main.service.ServiceFactory;
-import sernet.gs.ui.rcp.main.service.crudcommands.SaveElement;
 import sernet.verinice.iso27k.model.Control;
 import sernet.verinice.iso27k.model.Group;
 import sernet.verinice.iso27k.rcp.ControlTransformOperation;
-import sernet.verinice.iso27k.rcp.ISMView;
-import sernet.verinice.iso27k.service.IItem;
-import sernet.verinice.iso27k.service.ITransformer;
-import sernet.verinice.iso27k.service.ItemControlTransformer;
 
 /**
  * @author Daniel Murygin <dm@sernet.de>
