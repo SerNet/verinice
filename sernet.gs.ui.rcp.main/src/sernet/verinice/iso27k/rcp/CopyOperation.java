2796c3375ede7ce7fb55e29974c9c82194deda22
Daniel Murygin <dm@sernet.de>
2010-02-04 16:07:50 +0000

    ASSIGNED - bug 14: No Copy / Move possible in tree view http://localhost:10081/cgi-bin/bugzilla/show_bug.cgi?id=14
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@1013 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

---
+++ sernet.gs.ui.rcp.main/src/sernet/verinice/iso27k/rcp/CopyOperation.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Daniel Murygin <dm@sernet.de>.
+ *
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public 
+ * License along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Daniel Murygin <dm@sernet.de> - initial API and implementation
+ ******************************************************************************/
+package sernet.verinice.iso27k.rcp;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+
+import sernet.gs.ui.rcp.main.common.model.CnATreeElement;
+import sernet.verinice.iso27k.model.Control;
+import sernet.verinice.iso27k.model.ControlGroup;
+import sernet.verinice.iso27k.model.Group;
+import sernet.verinice.iso27k.service.ControlTransformService;
+import sernet.verinice.iso27k.service.CopyService;
+import sernet.verinice.iso27k.service.IProgressObserver;
+
+/**
+ * Operation with copies elements and adds them to a group.
+ * 
+ * Operation is executed as task in a {@link IProgressMonitor}
+ * 
+ * @author Daniel Murygin <dm@sernet.de>
+ */
+public class CopyOperation implements IRunnableWithProgress {
+
+	private static final Logger LOG = Logger.getLogger(CopyOperation.class);
+
+	private IProgressObserver progressObserver;
+	
+	private CopyService service;
+	
+	@SuppressWarnings("unchecked")
+	private Group selectedGroup;
+
+	private List<CnATreeElement> elements;
+	
+
+	@SuppressWarnings("unchecked")
+	public CopyOperation(Group selectedGroup, List<CnATreeElement> elements) {
+		this.selectedGroup = selectedGroup;
+		this.elements = elements;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.actions.WorkspaceModifyOperation#execute(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public void run(IProgressMonitor monitor)  {	
+		progressObserver = new RcpProgressObserver(monitor);
+		service = new CopyService(progressObserver,this.selectedGroup, elements);
+		service.run();
+	}
+
+	/**
+	 * @return
+	 */
+	public int getNumberOfElements() {
+		int n = 0;
+		if(service!=null) {
+			n = service.getNumberOfElements();
+		}
+		return n;
+	}
+
+}
