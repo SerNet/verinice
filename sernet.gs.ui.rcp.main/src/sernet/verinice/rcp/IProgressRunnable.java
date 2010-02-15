cf611defa80b4bcf72bde6279cfe15c84c251f4c
Daniel Murygin <dm@sernet.de>
2010-02-15 13:23:49 +0000

    ASSIGNED - bug 71: CopynPaste: Drop for "Bausteine" no longer working http://localhost:10081/cgi-bin/bugzilla/show_bug.cgi?id=71
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@1098 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

---
+++ sernet.gs.ui.rcp.main/src/sernet/verinice/rcp/IProgressRunnable.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Daniel Murygin <dm@sernet.de>.
+ *
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public 
+ * License along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Daniel Murygin <dm@sernet.de> - initial API and implementation
+ ******************************************************************************/
+package sernet.verinice.rcp;
+
+import org.eclipse.jface.operation.IRunnableWithProgress;
+
+/**
+ * @author Daniel Murygin <dm@sernet.de>
+ */
+public interface IProgressRunnable extends IRunnableWithProgress {
+
+	/**
+	 * Returns the number of processed elements
+	 * 
+	 * @return number of processed elements
+	 */
+	int getNumberOfElements();
+}
