bb6252edcd0cd7d93e05ed7e088755d4125e74fc
Daniel Murygin <dm@sernet.de>
2010-02-16 20:06:49 +0000

    cleanup
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@1112 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/service/crudcommands/SaveConfiguration.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/service/crudcommands/SaveConfiguration.java
@@ -27,52 +27,53 @@ import sernet.gs.ui.rcp.main.service.commands.IAuthAwareCommand;
 import sernet.hui.common.connect.Property;
 
 /**
- * Save element of type T to the database using its class to lookup
- * the DAO from the factory.
+ * Save element of type T to the database using its class to lookup the DAO from
+ * the factory.
  * 
  * @author koderman@sernet.de
- * @version $Rev$ $LastChangedDate$ 
- * $LastChangedBy$
- *
+ * @version $Rev$ $LastChangedDate$ $LastChangedBy$
+ * 
  * @param <T>
  */
-public class SaveConfiguration<T extends Configuration> extends GenericCommand 
-	implements IAuthAwareCommand {
+public class SaveConfiguration<T extends Configuration> extends GenericCommand implements IAuthAwareCommand {
 
 	private T element;
 	private boolean updatePassword;
-	
+
 	private transient IAuthService authService;
 
 	/**
 	 * Save a configuration item
 	 * 
-	 * @param element item to save / update
-	 * @param updatePassword was the password newly entered and needs to be hashed? 
+	 * @param element
+	 *            item to save / update
+	 * @param updatePassword
+	 *            was the password newly entered and needs to be hashed?
 	 */
 	public SaveConfiguration(T element, boolean updatePassword) {
 		this.element = element;
 		this.updatePassword = updatePassword;
 	}
-	
+
 	public void execute() {
-		IBaseDao<T, Serializable> dao 
-			= (IBaseDao<T, Serializable>) getDaoFactory().getDAO(element.getClass());
-			if (updatePassword)
-				hashPassword();
-			element = dao.merge(element);
-			
-			// The roles may have been modified. As such the server needs to throw away its
-			// cached role data.
-			getCommandService().discardRoleMap();
+		IBaseDao<T, Serializable> dao = (IBaseDao<T, Serializable>) getDaoFactory().getDAO(element.getClass());
+		if (updatePassword) {
+			hashPassword();
+		}
+		element = dao.merge(element);
+
+		// The roles may have been modified. As such the server needs to throw
+		// away its
+		// cached role data.
+		getCommandService().discardRoleMap();
 	}
 
 	private void hashPassword() {
 		Property passProperty = element.getEntity().getProperties(Configuration.PROP_PASSWORD).getProperty(0);
 		Property userProperty = element.getEntity().getProperties(Configuration.PROP_USERNAME).getProperty(0);
-		
+
 		String hash = getAuthService().hashPassword(userProperty.getPropertyValue(), passProperty.getPropertyValue());
-	    passProperty.setPropertyValue(hash, false);
+		passProperty.setPropertyValue(hash, false);
 	}
 
 	public T getElement() {
@@ -86,7 +87,5 @@ public class SaveConfiguration<T extends Configuration> extends GenericCommand
 	public void setAuthService(IAuthService service) {
 		this.authService = service;
 	}
-	
-	
 
 }
