644b92498993fe555ae82ff42878f744a027b800
Daniel Murygin <dm@sernet.de>
2010-02-17 10:49:46 +0000

    NEW - bug 22: User can be created twice with same name http://localhost:10081/cgi-bin/bugzilla/show_bug.cgi?id=22
    cleanup
    
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@1114 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/ExceptionUtil.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/ExceptionUtil.java
@@ -34,102 +34,70 @@ import sernet.gs.ui.rcp.main.service.commands.CommandException;
  * Helper class to handle and display exceptions.
  * 
  * @author koderman@sernet.de
- * @version $Rev: 39 $ $LastChangedDate: 2007-11-27 12:26:19 +0100 (Di, 27 Nov 2007) $ 
- * $LastChangedBy: koderman $
- *
+ * @version $Rev: 39 $ $LastChangedDate: 2007-11-27 12:26:19 +0100 (Di, 27 Nov
+ *          2007) $ $LastChangedBy: koderman $
+ * 
  */
 public class ExceptionUtil {
-	
+
 	public static void log(Throwable e, final String msg) {
 		if (e instanceof StaleObjectStateException) {
 			Display.getDefault().syncExec(new Runnable() {
 				public void run() {
-					MessageDialog.openError(Display.getDefault().getActiveShell(),
-							"Synchronisation",
-							"Die Daten wurden zwischenzeitlich durch einen anderen Benutzer verändert und soeben " +
-							"aktualisiert. Ihre Aktion wurde abgebrochen um zu verhindern, dass unbeabsichtigt " +
-							"Daten überschrieben werden. Bitte überprüfen Sie die Änderungen und führen Sie ihre" +
-							"Eingabe ggf. erneut aus.");
+					MessageDialog.openError(Display.getDefault().getActiveShell(), "Synchronisation", "Die Daten wurden zwischenzeitlich durch einen anderen Benutzer verändert und soeben " + "aktualisiert. Ihre Aktion wurde abgebrochen um zu verhindern, dass unbeabsichtigt " + "Daten überschrieben werden. Bitte überprüfen Sie die Änderungen und führen Sie ihre" + "Eingabe ggf. erneut aus.");
 				}
 			});
 			return;
 		}
-		
+
 		if (e instanceof SecurityException) {
 			Display.getDefault().syncExec(new Runnable() {
 				public void run() {
-					MessageDialog.openError(Display.getDefault().getActiveShell(),
-							"Aktion nicht erlaubt",
-							"Sie haben nicht die Berechtigung, um diese Aktion auszuführen.");
+					MessageDialog.openError(Display.getDefault().getActiveShell(), "Aktion nicht erlaubt", "Sie haben nicht die Berechtigung, um diese Aktion auszuführen.");
 				}
 			});
 			return;
 		}
-		
-		if (e instanceof CommandException
-				&& e.getCause() != null) {
+
+		if (e instanceof CommandException && e.getCause() != null) {
 			try {
-				e = (Exception) e.getCause();
+				e = e.getCause();
 			} catch (Exception castException) {
 				// keep original exception
 			}
 		}
-		
-		
+
 		String text = e.getLocalizedMessage() != null ? e.getLocalizedMessage() : "siehe Details";
-		
+
 		if (Activator.getDefault() == null) {
 			// RCP not initialized, skip dialog and just print to stdout:
 			System.err.println(msg);
 			e.printStackTrace();
 			return;
 		}
-		
-		final MultiStatus errorStatus = new MultiStatus(Activator.getDefault().getBundle().getSymbolicName(),
-				IStatus.ERROR,
-				text,
-				e);
-		
-		
-		
+
+		final MultiStatus errorStatus = new MultiStatus(Activator.getDefault().getBundle().getSymbolicName(), IStatus.ERROR, text, e);
+
 		Status status;
 		if (e instanceof GenericJDBCException) {
 			GenericJDBCException jdbcEx = (GenericJDBCException) e;
-			status = new Status(IStatus.ERROR,
-					Activator.getDefault().getBundle().getSymbolicName(),
-					IStatus.ERROR,
-					jdbcEx.getSQLException().getMessage(),
-					e);
-			
+			status = new Status(IStatus.ERROR, Activator.getDefault().getBundle().getSymbolicName(), IStatus.ERROR, jdbcEx.getSQLException().getMessage(), e);
+
+		} else if (e.getStackTrace() != null) {
+			status = new Status(IStatus.ERROR, Activator.getDefault().getBundle().getSymbolicName(), IStatus.ERROR, stackTraceToString(e), e);
+
+		} else {
+			status = new Status(IStatus.ERROR, Activator.getDefault().getBundle().getSymbolicName(), IStatus.ERROR, e.getMessage(), e);
+
 		}
-		else if (e.getStackTrace()!= null) {
-			status = new Status(IStatus.ERROR,
-					Activator.getDefault().getBundle().getSymbolicName(),
-					IStatus.ERROR,
-					stackTraceToString(e),
-					e);
-			
-		}
-		else {
-			status = new Status(IStatus.ERROR,
-					Activator.getDefault().getBundle().getSymbolicName(),
-					IStatus.ERROR,
-					e.getMessage(),
-					e);
-			
-		}
-		
-		errorStatus.add(status );
+
+		errorStatus.add(status);
 		Activator.getDefault().getLog().log(status);
-		
-		if (Activator.getDefault().getPluginPreferences()
-				.getBoolean(PreferenceConstants.ERRORPOPUPS)) {
+
+		if (Activator.getDefault().getPluginPreferences().getBoolean(PreferenceConstants.ERRORPOPUPS)) {
 			Display.getDefault().syncExec(new Runnable() {
 				public void run() {
-					ErrorDialog.openError(Display.getDefault().getActiveShell(),
-							"Fehler",
-							msg,
-							errorStatus);
+					ErrorDialog.openError(Display.getDefault().getActiveShell(), "Fehler", msg, errorStatus);
 				}
 			});
 		}
