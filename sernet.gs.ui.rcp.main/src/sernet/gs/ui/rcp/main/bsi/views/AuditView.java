2907850b592ff95ddd675a0cedf0bcef3300b434
Daniel Murygin <dm@sernet.de>
2010-02-19 14:07:38 +0000

    RESOLVED - bug 77: Table in ToDoView should be sortable http://localhost:10081/cgi-bin/bugzilla/show_bug.cgi?id=77
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@1157 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/views/AuditView.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/views/AuditView.java
@@ -28,6 +28,8 @@ import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
@@ -37,6 +39,7 @@ import sernet.gs.ui.rcp.main.bsi.filter.MassnahmenSiegelFilter;
 import sernet.gs.ui.rcp.main.bsi.filter.MassnahmenUmsetzungFilter;
 import sernet.gs.ui.rcp.main.bsi.model.MassnahmenUmsetzung;
 import sernet.gs.ui.rcp.main.bsi.model.TodoViewItem;
+import sernet.gs.ui.rcp.main.bsi.views.GenericMassnahmenView.SortSelectionAdapter;
 import sernet.gs.ui.rcp.main.bsi.views.actions.AuditViewFilterAction;
 import sernet.gs.ui.rcp.main.common.model.PlaceHolder;
 
@@ -50,6 +53,100 @@ import sernet.gs.ui.rcp.main.common.model.PlaceHolder;
 public class AuditView extends GenericMassnahmenView {
 	public static final String ID = "sernet.gs.ui.rcp.main.bsi.views.auditview"; //$NON-NLS-1$
 
+	TableSorter tableSorter = new TableSorter();
+	
+	@Override
+	protected void createPartControlImpl(Composite parent) {
+		Table table = viewer.getTable();
+		
+		iconColumn = new TableColumn(table, SWT.LEFT);
+		iconColumn.setText(" "); //$NON-NLS-1$
+		iconColumn.setWidth(25);
+		iconColumn.addSelectionListener(new SortSelectionAdapter(this,iconColumn,0));
+		
+		dateColumn = new TableColumn(table, SWT.LEFT);
+		dateColumn.setText(Messages.AuditView_8);
+		dateColumn.setWidth(200);
+		dateColumn.addSelectionListener(new SortSelectionAdapter(this,dateColumn,1));
+		
+		bearbeiterColumn = new TableColumn(table, SWT.LEFT);
+		bearbeiterColumn.setText(Messages.AuditView_9);
+		bearbeiterColumn.setWidth(100);
+		bearbeiterColumn.addSelectionListener(new SortSelectionAdapter(this,bearbeiterColumn,2));
+		
+		siegelColumn = new TableColumn(table, SWT.LEFT);
+		siegelColumn.setText(Messages.AuditView_10);
+		siegelColumn.setWidth(20);
+		siegelColumn.addSelectionListener(new SortSelectionAdapter(this,siegelColumn,3));
+		
+		zielColumn = new TableColumn(table, SWT.LEFT);
+		zielColumn.setText(Messages.AuditView_11);
+		zielColumn.setWidth(150);
+		zielColumn.addSelectionListener(new SortSelectionAdapter(this,zielColumn,4));
+		
+		titleColumn = new TableColumn(table, SWT.LEFT);
+		titleColumn.setText(Messages.AuditView_12);
+		titleColumn.setWidth(250);
+		titleColumn.addSelectionListener(new SortSelectionAdapter(this,titleColumn,5));
+		
+		viewer.setColumnProperties(new String[] {
+				"_icon", //$NON-NLS-1$
+				"_date", //$NON-NLS-1$
+				"_bearbeiter", //$NON-NLS-1$
+				"_siegel", //$NON-NLS-1$
+				"_ziel", //$NON-NLS-1$
+				"_title" //$NON-NLS-1$
+		});
+		
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+	}
+	
+	protected String[] getUmsetzungPattern() {
+		return new String[] {
+				MassnahmenUmsetzung.P_UMSETZUNG_JA,
+				MassnahmenUmsetzung.P_UMSETZUNG_ENTBEHRLICH
+		};
+	}
+
+	@Override
+	protected Action createFilterAction(
+			MassnahmenUmsetzungFilter umsetzungFilter,
+			MassnahmenSiegelFilter siegelFilter) {
+		return new AuditViewFilterAction(viewer, Messages.AuditView_19,
+				umsetzungFilter, siegelFilter);
+	}
+
+	@Override
+	protected ILabelProvider createLabelProvider() {
+		return new AuditLabelProvider();
+	}
+
+	@Override
+	protected TableSorter createSorter() {
+		return tableSorter;
+	}
+
+	@Override
+	protected String getMeasureLoadJobLabel() {
+		return Messages.AuditView_4;
+	}
+
+	@Override
+	protected String getMeasureLoadPlaceholderLabel() {
+		return Messages.AuditView_2;
+	}
+
+	@Override
+	protected String getTaskErrorLabel() {
+		return Messages.AuditView_5;
+	}
+
+	@Override
+	protected String getMeasureLoadTaskLabel() {
+		return "Lade Massnahmen";
+	}
+	
 	private static class AuditLabelProvider extends LabelProvider implements ITableLabelProvider {
 
 		private SimpleDateFormat dateFormat =  new SimpleDateFormat("dd.MM.yy, EE"); //$NON-NLS-1$
@@ -98,120 +195,33 @@ public class AuditView extends GenericMassnahmenView {
 		}
 	}
 	
-	private static class AuditSorter extends ViewerSorter {
-		public boolean isSorterProperty(Object arg0, String arg1) {
-			return arg1.equals("_date"); //$NON-NLS-1$
-		}
+	private static class SortSelectionAdapter extends SelectionAdapter {
+		AuditView view;
+		TableColumn column;
+		int index;
 		
-		public int compare(Viewer viewer, Object o1, Object o2) {
-			if (o1 == null || o2 == null)
-				return 0;
-			TodoViewItem mn1 = (TodoViewItem) o1;
-			TodoViewItem mn2 = (TodoViewItem) o2;
-			return sortByDate(mn1.getNaechsteRevision(), mn2.getNaechsteRevision());
+		public SortSelectionAdapter(AuditView view, TableColumn column, int index) {
+			this.view = view;
+			this.column = column;
+			this.index = index;
 		}
+	
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			view.tableSorter.setColumn(index);
+			int dir = view.viewer.getTable().getSortDirection();
+			if (view.viewer.getTable().getSortColumn() == column) {
+				dir = dir == SWT.UP ? SWT.DOWN : SWT.UP;
+			} else {
 
-		private int sortByDate(Date date1, Date date2) {
-	        if (date1 == null)
-	            return 1;
-	        
-	        if (date2 == null)
-	            return -1;
-	        
-			int comp = date1.compareTo(date2);
-			// reverse order:
-			//comp = (comp < 0) ? 1 : (comp > 0) ? -1 : 0;
-			return comp;
-	        
+				dir = SWT.DOWN;
+			}
+			view.viewer.getTable().setSortDirection(dir);
+			view.viewer.getTable().setSortColumn(column);
+			view.viewer.refresh();
 		}
-		
 
 	}
 	
-	@Override
-	protected void createPartControlImpl(Composite parent) {
-		Table table = viewer.getTable();
-		
-		iconColumn = new TableColumn(table, SWT.LEFT);
-		iconColumn.setText(" "); //$NON-NLS-1$
-		iconColumn.setWidth(25);
-		
-		dateColumn = new TableColumn(table, SWT.LEFT);
-		dateColumn.setText(Messages.AuditView_8);
-		dateColumn.setWidth(200);
-		
-		bearbeiterColumn = new TableColumn(table, SWT.LEFT);
-		bearbeiterColumn.setText(Messages.AuditView_9);
-		bearbeiterColumn.setWidth(100);
-		
-		siegelColumn = new TableColumn(table, SWT.LEFT);
-		siegelColumn.setText(Messages.AuditView_10);
-		siegelColumn.setWidth(20);
-		
-		zielColumn = new TableColumn(table, SWT.LEFT);
-		zielColumn.setText(Messages.AuditView_11);
-		zielColumn.setWidth(150);
-		
-		titleColumn = new TableColumn(table, SWT.LEFT);
-		titleColumn.setText(Messages.AuditView_12);
-		titleColumn.setWidth(250);
-		
-		viewer.setColumnProperties(new String[] {
-				"_icon", //$NON-NLS-1$
-				"_date", //$NON-NLS-1$
-				"_bearbeiter", //$NON-NLS-1$
-				"_siegel", //$NON-NLS-1$
-				"_ziel", //$NON-NLS-1$
-				"_title" //$NON-NLS-1$
-		});
-		
-		table.setHeaderVisible(true);
-		table.setLinesVisible(true);
-	}
-	
-	protected String[] getUmsetzungPattern() {
-		return new String[] {
-				MassnahmenUmsetzung.P_UMSETZUNG_JA,
-				MassnahmenUmsetzung.P_UMSETZUNG_ENTBEHRLICH
-		};
-	}
-
-	@Override
-	protected Action createFilterAction(
-			MassnahmenUmsetzungFilter umsetzungFilter,
-			MassnahmenSiegelFilter siegelFilter) {
-		return new AuditViewFilterAction(viewer, Messages.AuditView_19,
-				umsetzungFilter, siegelFilter);
-	}
-
-	@Override
-	protected ILabelProvider createLabelProvider() {
-		return new AuditLabelProvider();
-	}
-
-	@Override
-	protected ViewerSorter createSorter() {
-		return new AuditSorter();
-	}
-
-	@Override
-	protected String getMeasureLoadJobLabel() {
-		return Messages.AuditView_4;
-	}
-
-	@Override
-	protected String getMeasureLoadPlaceholderLabel() {
-		return Messages.AuditView_2;
-	}
-
-	@Override
-	protected String getTaskErrorLabel() {
-		return Messages.AuditView_5;
-	}
-
-	@Override
-	protected String getMeasureLoadTaskLabel() {
-		return "Lade Massnahmen";
-	}
 
 }
