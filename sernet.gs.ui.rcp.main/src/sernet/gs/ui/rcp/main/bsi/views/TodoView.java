9172010e72d6847c2684842e2dda3ab1db8358b8
Daniel Murygin <dm@sernet.de>
2010-02-19 07:45:12 +0000

    format
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@1152 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/views/TodoView.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/views/TodoView.java
@@ -50,81 +50,6 @@ public class TodoView extends GenericMassnahmenView {
 	
 	public static final String ID = "sernet.gs.ui.rcp.main.bsi.views." +
 			"todoview"; //$NON-NLS-1$
-	
-	private static class TodoLabelProvider extends LabelProvider implements ITableLabelProvider {
-
-		private SimpleDateFormat dateFormat =  new SimpleDateFormat("dd.MM.yy, EE"); //$NON-NLS-1$
-		
-		public Image getColumnImage(Object element, int columnIndex) {
-			if (element instanceof PlaceHolder) {
-				return null;
-			}
-			
-			TodoViewItem mn = (TodoViewItem) element;
-			if (columnIndex == 0) {
-				return CnAImageProvider.getImage(mn);
-			}
-			return null;
-		}
-
-		public String getColumnText(Object element, int columnIndex) {
-			if (element instanceof PlaceHolder) {
-				if (columnIndex == 1) {
-					PlaceHolder ph = (PlaceHolder) element;
-					return ph.getTitle();
-				}
-				return "";
-			}
-			
-			TodoViewItem mn = (TodoViewItem) element;
-			switch(columnIndex) {
-			case 0: // icon
-				return ""; //$NON-NLS-1$
-			case 1: // date
-				Date date = mn.getUmsetzungBis();
-				if (date == null)
-					return Messages.TodoView_3;
-				return dateFormat.format(date);
-			case 2: // bearbeiter
-				return mn.getUmsetzungDurch();
-			case 3: // siegelstufe
-				return "" + mn.getStufe(); //$NON-NLS-1$
-			case 4: // zielobjekt
-				return mn.getParentTitle();
-			case 5: // title
-				return mn.getTitle();
-			}
-			return ""; //$NON-NLS-1$
-		}
-	}
-	
-	private static class TodoSorter extends ViewerSorter {
-		public boolean isSorterProperty(Object arg0, String arg1) {
-			return arg1.equals("_date"); //$NON-NLS-1$
-		}
-		
-		public int compare(Viewer viewer, Object o1, Object o2) {
-			if (o1 == null || o2 == null)
-				return 0;
-			TodoViewItem mn1 = (TodoViewItem) o1;
-			TodoViewItem mn2 = (TodoViewItem) o2;
-			return sortByDate(mn1.getUmsetzungBis(), mn2.getUmsetzungBis());
-		}
-
-		private int sortByDate(Date date1, Date date2) {
-	        if (date1 == null)
-	            return 1;
-	        
-	        if (date2 == null)
-	            return -1;
-	        
-			int comp = date1.compareTo(date2);
-			return comp;
-	        
-		}
-		
-
-	}
 
 	@Override
 	protected void createPartControlImpl(Composite parent) {
@@ -211,6 +136,81 @@ public class TodoView extends GenericMassnahmenView {
 	@Override
 	protected String getMeasureLoadTaskLabel() {
 		return "Lade Massnahmen";
+	}
+	
+	private static class TodoLabelProvider extends LabelProvider implements ITableLabelProvider {
+
+		private SimpleDateFormat dateFormat =  new SimpleDateFormat("dd.MM.yy, EE"); //$NON-NLS-1$
+		
+		public Image getColumnImage(Object element, int columnIndex) {
+			if (element instanceof PlaceHolder) {
+				return null;
+			}
+			
+			TodoViewItem mn = (TodoViewItem) element;
+			if (columnIndex == 0) {
+				return CnAImageProvider.getImage(mn);
+			}
+			return null;
+		}
+
+		public String getColumnText(Object element, int columnIndex) {
+			if (element instanceof PlaceHolder) {
+				if (columnIndex == 1) {
+					PlaceHolder ph = (PlaceHolder) element;
+					return ph.getTitle();
+				}
+				return "";
+			}
+			
+			TodoViewItem mn = (TodoViewItem) element;
+			switch(columnIndex) {
+			case 0: // icon
+				return ""; //$NON-NLS-1$
+			case 1: // date
+				Date date = mn.getUmsetzungBis();
+				if (date == null)
+					return Messages.TodoView_3;
+				return dateFormat.format(date);
+			case 2: // bearbeiter
+				return mn.getUmsetzungDurch();
+			case 3: // siegelstufe
+				return "" + mn.getStufe(); //$NON-NLS-1$
+			case 4: // zielobjekt
+				return mn.getParentTitle();
+			case 5: // title
+				return mn.getTitle();
+			}
+			return ""; //$NON-NLS-1$
+		}
+	}
+	
+	private static class TodoSorter extends ViewerSorter {
+		public boolean isSorterProperty(Object arg0, String arg1) {
+			return arg1.equals("_date"); //$NON-NLS-1$
+		}
+		
+		public int compare(Viewer viewer, Object o1, Object o2) {
+			if (o1 == null || o2 == null)
+				return 0;
+			TodoViewItem mn1 = (TodoViewItem) o1;
+			TodoViewItem mn2 = (TodoViewItem) o2;
+			return sortByDate(mn1.getUmsetzungBis(), mn2.getUmsetzungBis());
+		}
+
+		private int sortByDate(Date date1, Date date2) {
+	        if (date1 == null)
+	            return 1;
+	        
+	        if (date2 == null)
+	            return -1;
+	        
+			int comp = date1.compareTo(date2);
+			return comp;
+	        
+		}
+		
+
 	}
 
 }
