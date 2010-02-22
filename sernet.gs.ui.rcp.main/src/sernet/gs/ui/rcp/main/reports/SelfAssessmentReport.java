a46a0f8a8c04fe310ebaea09659378f5c5cca8fc
Alexander Koderman <ak@sernet.de>
2010-02-22 09:55:04 +0000

    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@1162 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

---
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/reports/SelfAssessmentReport.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Alexander Koderman <ak@sernet.de>.
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 
+ * of the License, or (at your option) any later version.
+ *     This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU General Public License for more details.
+ *     You should have received a copy of the GNU General Public 
+ * License along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Alexander Koderman <ak@sernet.de> - initial API and implementation
+ ******************************************************************************/
+package sernet.gs.ui.rcp.main.reports;
+
+import java.util.ArrayList;
+import java.util.Properties;
+
+import sernet.gs.ui.rcp.main.common.model.CnATreeElement;
+import sernet.gs.ui.rcp.office.IOOTableRow;
+
+/**
+ * @author koderman@sernet.de
+ * @version $Rev$ $LastChangedDate$ 
+ * $LastChangedBy$
+ *
+ */
+public class SelfAssessmentReport extends BsiReport  {
+
+	/**
+	 * @param reportProperties
+	 */
+	public SelfAssessmentReport() {
+		super(null);
+	}
+
+	/* (non-Javadoc)
+	 * @see sernet.gs.ui.rcp.main.reports.IBSIReport#getItems()
+	 */
+	public ArrayList<CnATreeElement> getItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see sernet.gs.ui.rcp.main.reports.IBSIReport#getReport(sernet.gs.ui.rcp.main.reports.PropertySelection)
+	 */
+	public ArrayList<IOOTableRow> getReport(PropertySelection shownPropertyTypes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see sernet.gs.ui.rcp.main.reports.IBSIReport#getTitle()
+	 */
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
