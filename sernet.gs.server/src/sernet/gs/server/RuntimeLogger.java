e9c8dbb1daf3d73a23f429d29a47ed0ccc63ad2d
Daniel Murygin <dm@sernet.de>
2010-03-03 09:54:47 +0000

    ASSIGNED - bug 74: Change License to LGPL http://localhost:10081/cgi-bin/bugzilla/show_bug.cgi?id=74
    spam proof emails
    
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@1222 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.server/src/sernet/gs/server/RuntimeLogger.java
+++ sernet.gs.server/src/sernet/gs/server/RuntimeLogger.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 Daniel Murygin <dm@sernet.de>.
+ * Copyright (c) 2009 Daniel Murygin <dm[at]sernet[dot]de>.
  * This program is free software: you can redistribute it and/or 
  * modify it under the terms of the GNU Lesser General Public License 
  * as published by the Free Software Foundation, either version 3 
@@ -8,12 +8,12 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty 
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
  * See the GNU Lesser General Public License for more details.
- *     You should have received a copy of the GNU General Public 
+ *     You should have received a copy of the GNU Lesser General Public 
  * License along with this program. 
  * If not, see <http://www.gnu.org/licenses/>.
  * 
  * Contributors:
- *     Daniel Murygin <dm@sernet.de> - initial API and implementation
+ *     Daniel Murygin <dm[at]sernet[dot]de> - initial API and implementation
  ******************************************************************************/
 package sernet.gs.server;
 
@@ -33,7 +33,7 @@ import org.springframework.util.StopWatch;
  * See: http://murygin.wordpress.com/2007/10/25/logging-mit-spring-aop/
  * for a german documentation.
  * 
- * @author Daniel Murygin <dm@sernet.de>
+ * @author Daniel Murygin <dm[at]sernet[dot]de>
  */
 public class RuntimeLogger { 
 	final static Logger LOG = Logger.getLogger(RuntimeLogger.class);
