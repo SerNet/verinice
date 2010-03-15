7382c19d0cbada25c4f5f196449bfb02585baa8c
Daniel Murygin <dm@sernet.de>
2010-03-15 16:12:52 +0000

    bug 96: Path to Derby DB cannot be edit in client
    clean-up
    
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@1237 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/preferences/DatenbankPreferencePage.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/preferences/DatenbankPreferencePage.java
@@ -35,11 +35,9 @@ import sernet.gs.ui.rcp.main.common.model.CnAElementFactory;
  * Main preference page for CnA Tool Settings.
  * 
  * @author akoderman[at]sernet[dot]de
- *
+ * 
  */
-public class DatenbankPreferencePage
-	extends FieldEditorPreferencePage
-	implements IWorkbenchPreferencePage {
+public class DatenbankPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
 	private static final Logger log = Logger.getLogger(DatenbankPreferencePage.class);
 
@@ -48,7 +46,7 @@ public class DatenbankPreferencePage
 	private StringFieldEditor url;
 	private StringFieldEditor user;
 	private StringFieldEditor pass;
-	
+
 	private boolean modified = false;
 
 	public DatenbankPreferencePage() {
@@ -62,48 +60,40 @@ public class DatenbankPreferencePage
 				Messages.getString("DatenbankPreferencePage.5") + //$NON-NLS-1$
 				Messages.getString("DatenbankPreferencePage.6")); //$NON-NLS-1$
 	}
-	
+
 	/**
-	 * Creates the field editors. Field editors are abstractions of
-	 * the common GUI blocks needed to manipulate various types
-	 * of preferences. Each field editor knows how to save and
-	 * restore itself.
+	 * Creates the field editors. Field editors are abstractions of the common
+	 * GUI blocks needed to manipulate various types of preferences. Each field
+	 * editor knows how to save and restore itself.
 	 */
+	@Override
 	public void createFieldEditors() {
-		
+
 		createRadioGroup();
-		dialect = new StringFieldEditor(PreferenceConstants.DB_DIALECT,
-				Messages.getString("DatenbankPreferencePage.7"), //$NON-NLS-1$
+		dialect = new StringFieldEditor(PreferenceConstants.DB_DIALECT, Messages.getString("DatenbankPreferencePage.7"), //$NON-NLS-1$
 				getFieldEditorParent());
 		addField(dialect);
-		
-		url = new StringFieldEditor(PreferenceConstants.DB_URL,
-				Messages.getString("DatenbankPreferencePage.8"), //$NON-NLS-1$
+
+		url = new StringFieldEditor(PreferenceConstants.DB_URL, Messages.getString("DatenbankPreferencePage.8"), //$NON-NLS-1$
 				getFieldEditorParent());
 		addField(url);
-		
-		
-		user = new StringFieldEditor(PreferenceConstants.DB_USER,
-				Messages.getString("DatenbankPreferencePage.9"), //$NON-NLS-1$
+
+		user = new StringFieldEditor(PreferenceConstants.DB_USER, Messages.getString("DatenbankPreferencePage.9"), //$NON-NLS-1$
 				getFieldEditorParent());
 		addField(user);
-		
-		pass = new StringFieldEditor(PreferenceConstants.DB_PASS,
-				Messages.getString("DatenbankPreferencePage.10"), //$NON-NLS-1$
+
+		pass = new StringFieldEditor(PreferenceConstants.DB_PASS, Messages.getString("DatenbankPreferencePage.10"), //$NON-NLS-1$
 				getFieldEditorParent());
 		addField(pass);
-		
 
 	}
-	
+
 	@Override
 	public void setVisible(boolean visible) {
 		super.setVisible(visible);
-		
+
 		if (visible) {
-			boolean standalone = getPreferenceStore()
-				.getString(PreferenceConstants.OPERATION_MODE)
-				.equals(PreferenceConstants.OPERATION_MODE_INTERNAL_SERVER);
+			boolean standalone = getPreferenceStore().getString(PreferenceConstants.OPERATION_MODE).equals(PreferenceConstants.OPERATION_MODE_INTERNAL_SERVER);
 			boolean dbOpen = CnAElementFactory.getInstance().isDbOpen();
 
 			// Do not show the fields when the remote server is in use
@@ -111,7 +101,7 @@ public class DatenbankPreferencePage
 			setEnabledFields(!dbOpen && standalone);
 		}
 	}
-	
+
 	private void setEnabledFields(boolean enable) {
 		Composite parent = getFieldEditorParent();
 		dbDriver.setEnabled(enable, parent);
@@ -119,29 +109,22 @@ public class DatenbankPreferencePage
 		url.setEnabled(enable, parent);
 		user.setEnabled(enable, parent);
 		pass.setEnabled(enable, parent);
-		
+
 		if (enable) {
 			setMessage(null);
-		}
-		else {
+		} else {
 			setMessage("DB-Verbindung derzeit geöffnet oder verinice-Server konfiguriert.");
 		}
 	}
 
 	private void createRadioGroup() {
-		dbDriver = new RadioGroupFieldEditor(PreferenceConstants.DB_DRIVER,
-				Messages.getString("DatenbankPreferencePage.11"), //$NON-NLS-1$
-				1,
-				new String[][] {
-					{Messages.getString("DatenbankPreferencePage.12"), PreferenceConstants.DB_DRIVER_DERBY},  //$NON-NLS-1$
-					{"Postgres", PreferenceConstants.DB_DRIVER_POSTGRES}, //$NON-NLS-1$
-					{"MySQL", PreferenceConstants.DB_DRIVER_MYSQL} //$NON-NLS-1$
-				},
-				getFieldEditorParent());
+		dbDriver = new RadioGroupFieldEditor(PreferenceConstants.DB_DRIVER, Messages.getString("DatenbankPreferencePage.11"), //$NON-NLS-1$
+				1, new String[][] { { Messages.getString("DatenbankPreferencePage.12"), PreferenceConstants.DB_DRIVER_DERBY }, //$NON-NLS-1$
+						{ "Postgres", PreferenceConstants.DB_DRIVER_POSTGRES }, //$NON-NLS-1$
+						{ "MySQL", PreferenceConstants.DB_DRIVER_MYSQL } //$NON-NLS-1$
+				}, getFieldEditorParent());
 		addField(dbDriver);
-		
-		
-		
+
 	}
 
 	@Override
@@ -149,65 +132,66 @@ public class DatenbankPreferencePage
 		super.propertyChange(event);
 		if (event.getProperty().equals(FieldEditor.VALUE)) {
 			if (event.getSource() == dbDriver) {
-				setDefaults((String)event.getNewValue());
+				setDefaults((String) event.getNewValue());
 			}
 			checkState();
 		}
-		
+
 		modified = true;
 	}
 
 	private void setDefaults(String newValue) {
 		if (newValue.equals(PreferenceConstants.DB_DRIVER_DERBY)) {
 			dialect.setStringValue(PreferenceConstants.DB_DIALECT_derby);
-			
+
 			// replace backslashes
 			// derby db url looks like this on windows: c:/Programme/Verinice...
-			String derbyUrl = PreferenceConstants.DB_URL_DERBY.replace("%s",CnAWorkspace //$NON-NLS-1$
-					.getInstance().getWorkdir().replaceAll("\\\\", "/") );
+			String derbyUrl = PreferenceConstants.DB_URL_DERBY.replace("%s", CnAWorkspace //$NON-NLS-1$
+					.getInstance().getWorkdir().replaceAll("\\\\", "/"));
 			Logger.getLogger(this.getClass()).debug("Derby url is " + derbyUrl);
 			url.setStringValue(derbyUrl);
 			user.setStringValue(""); //$NON-NLS-1$
 			pass.setStringValue(""); //$NON-NLS-1$
-		}
-		else if (newValue.equals(PreferenceConstants.DB_DRIVER_POSTGRES)) {
+		} else if (newValue.equals(PreferenceConstants.DB_DRIVER_POSTGRES)) {
 			dialect.setStringValue(PreferenceConstants.DB_DIALECT_postgres);
 			url.setStringValue(PreferenceConstants.DB_URL_POSTGRES);
 			user.setStringValue(""); //$NON-NLS-1$
 			pass.setStringValue(""); //$NON-NLS-1$
-		}
-		else if (newValue.equals(PreferenceConstants.DB_DRIVER_MYSQL)) {
+		} else if (newValue.equals(PreferenceConstants.DB_DRIVER_MYSQL)) {
 			dialect.setStringValue(PreferenceConstants.DB_DIALECT_mysql);
 			url.setStringValue(PreferenceConstants.DB_URL_MYSQL);
 			user.setStringValue(""); //$NON-NLS-1$
 			pass.setStringValue(""); //$NON-NLS-1$
 		}
-		
+
 	}
 
 	@Override
 	protected void checkState() {
 		super.checkState();
-		
-		if (!isValid())
+
+		if (!isValid()) {
 			return;
+		}
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
 	 */
 	public void init(IWorkbench workbench) {
-		
+
 	}
-	
-	public boolean performOk()
-	{
-		if (modified)
-		{
+
+	@Override
+	public boolean performOk() {
+		if (modified) {
 			log.debug("stopping internal server after change of db values");
 			Activator.getDefault().getInternalServer().stop();
 		}
-		
+
 		return super.performOk();
 	}
 }
