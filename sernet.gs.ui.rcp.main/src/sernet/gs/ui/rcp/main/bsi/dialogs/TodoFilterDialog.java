fa443e5fdaaa767361e40f7a6ec4b3d8c604921a
Daniel Murygin <dm@sernet.de>
2010-04-20 10:59:52 +0000

    clean up
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@1520 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/dialogs/TodoFilterDialog.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/dialogs/TodoFilterDialog.java
@@ -17,7 +17,6 @@
  ******************************************************************************/
 package sernet.gs.ui.rcp.main.bsi.dialogs;
 
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
@@ -38,11 +37,7 @@ public class TodoFilterDialog extends FilterDialog {
 	private String umsetzungDurch;
 	protected String zielobjekt;
 
-	public TodoFilterDialog(Shell parent,
-			String[] umsetzung, 
-			String[] siegel,
-			String umsetzungDurch,
-			String zielobjekt) {
+	public TodoFilterDialog(Shell parent, String[] umsetzung, String[] siegel, String umsetzungDurch, String zielobjekt) {
 		super(parent, umsetzung, siegel, null);
 		this.umsetzungDurch = umsetzungDurch;
 		this.zielobjekt = zielobjekt;
@@ -54,62 +49,59 @@ public class TodoFilterDialog extends FilterDialog {
 		GridLayout layout = new GridLayout();
 		layout.numColumns = 2;
 		container.setLayout(layout);
-		
+
 		Label intro = new Label(container, SWT.NONE);
-		intro.setLayoutData(new GridData(GridData.BEGINNING, GridData.CENTER,
-				false, false, 2, 1));
-		intro.setText("Wählen Sie aus, nach welchen Kriterien die Liste der Maßnahmen" +
-				" gefiltert wird.");
-		
+		intro.setLayoutData(new GridData(GridData.BEGINNING, GridData.CENTER, false, false, 2, 1));
+		intro.setText("Wählen Sie aus, nach welchen Kriterien die Liste der Maßnahmen gefiltert wird.");
+
 		Label label1 = new Label(container, SWT.NONE);
 		label1.setText("Verantwortlich ");
-		
+
 		text1 = new Text(container, SWT.BORDER);
-		text1.setLayoutData(new GridData(GridData.FILL, GridData.CENTER,
-				true, false, 1, 1));
+		text1.setLayoutData(new GridData(GridData.FILL, GridData.CENTER, true, false, 1, 1));
 		text1.addModifyListener(new ModifyListener() {
 			public void modifyText(ModifyEvent arg0) {
 				umsetzungDurch = text1.getText();
 			}
 		});
-		
+
 		Label label2 = new Label(container, SWT.NONE);
 		label2.setText("Zielobjekt");
-		
+
 		text2 = new Text(container, SWT.BORDER);
-		text2.setLayoutData(new GridData(GridData.FILL, GridData.CENTER,
-				true, false, 1, 1));
+		text2.setLayoutData(new GridData(GridData.FILL, GridData.CENTER, true, false, 1, 1));
 		text2.addModifyListener(new ModifyListener() {
 			public void modifyText(ModifyEvent arg0) {
 				zielobjekt = text2.getText();
 			}
 		});
-		
+
 		Group boxesComposite = createUmsetzungGroup(parent);
 		Group boxesComposite2 = createSiegelGroup(parent);
 		createUmsetzungCheckboxes(boxesComposite);
 		createSiegelCheckboxes(boxesComposite2);
 		initContent();
-		
+
 		return container;
 	}
 
+	@Override
 	protected void initContent() {
 		super.initContent();
 		text1.setText(umsetzungDurch != null ? umsetzungDurch : "");
 		text2.setText(zielobjekt != null ? zielobjekt : "");
 	}
-	
+
 	@Override
 	protected void configureShell(Shell newShell) {
 		super.configureShell(newShell);
 		newShell.setText("Maßnahmen Filter Einstellungen");
 	}
-	
+
 	public String getUmsetzungDurch() {
 		return umsetzungDurch;
 	}
-	
+
 	public String getZielobjekt() {
 		return zielobjekt;
 	}
