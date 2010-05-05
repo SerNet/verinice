49162f129c99d72aa2d06d602c9e3b63f432d7c7
Alexander Koderman <ak@sernet.de>
2010-05-05 16:19:27 +0000

    - MERGE from BRANCH/1.1
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@1608 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

---
+++ sernet.gs.server/update-jars.bat
@@ -0,0 +1,11 @@
+ set webinflib=WebContent\WEB-INF\lib
+ SET PATH=%PATH%;c:\programme\java\jdk1.6.0_18\bin
+  
+ jar cf %webinflib%\sernet.hui.common.jar -C ..\sernet.hui.common\bin .
+ jar cf %webinflib%\sernet.hui.swtclient.jar -C ..\sernet.hui.swtclient\bin . 
+ jar cf %webinflib%\sernet.hui.server.jar -C ..\sernet.hui.server\bin . 
+ jar cf %webinflib%\sernet.gs.ui.rcp.main.jar -C ..\sernet.gs.ui.rcp.main\bin . 
+ jar cf %webinflib%\sernet.gs.service.jar -C ..\sernet.gs.service\bin .
+ jar cf %webinflib%\sernet.gs.reveng.jar -C ..\sernet.gs.reveng\bin .
+
+
