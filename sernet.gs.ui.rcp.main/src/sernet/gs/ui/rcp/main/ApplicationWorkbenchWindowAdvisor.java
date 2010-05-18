7234c92ebba48a17b3de45b58887a7ed118b0bf7
Daniel Murygin <dm@sernet.de>
2010-05-18 13:00:54 +0000

    clean up
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@1649 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/ApplicationWorkbenchWindowAdvisor.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/ApplicationWorkbenchWindowAdvisor.java
@@ -44,100 +44,93 @@ import sernet.gs.ui.rcp.main.preferences.PreferenceConstants;
  * Workbench Window advisor.
  * 
  * @author koderman[at]sernet[dot]de
- * @version $Rev: 39 $ $LastChangedDate: 2007-11-27 12:26:19 +0100 (Di, 27 Nov 2007) $ 
- * $LastChangedBy: koderman $
- *
+ * @version $Rev: 39 $ $LastChangedDate: 2007-11-27 12:26:19 +0100 (Di, 27 Nov
+ *          2007) $ $LastChangedBy: koderman $
+ * 
  */
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
-	public ApplicationWorkbenchWindowAdvisor(
-			IWorkbenchWindowConfigurer configurer) {
-		super(configurer);
-	}
+    public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        super(configurer);
+    }
 
-	public ActionBarAdvisor createActionBarAdvisor(
-			IActionBarConfigurer configurer) {
-		return new ApplicationActionBarAdvisor(configurer);
-	}
+    @Override
+    public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
+        return new ApplicationActionBarAdvisor(configurer);
+    }
 
-	public void preWindowOpen() {
-		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-		configurer.setInitialSize(new Point(1000, 700));
-		configurer.setShowCoolBar(true);
-		configurer.setShowStatusLine(true);
-		configurer.setShowProgressIndicator(true);
-	}
+    @Override
+    public void preWindowOpen() {
+        IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+        configurer.setInitialSize(new Point(1000, 700));
+        configurer.setShowCoolBar(true);
+        configurer.setShowStatusLine(true);
+        configurer.setShowProgressIndicator(true);
+    }
 
-	@Override
-	public void postWindowOpen() {
-		if (Activator.getDefault().getPluginPreferences()
-				.getBoolean(PreferenceConstants.FIRSTSTART)) {
-			Activator.getDefault().getPluginPreferences().setValue(PreferenceConstants.FIRSTSTART, false);
+    @Override
+    public void postWindowOpen() {
+        if (Activator.getDefault().getPluginPreferences().getBoolean(PreferenceConstants.FIRSTSTART)) {
+            Activator.getDefault().getPluginPreferences().setValue(PreferenceConstants.FIRSTSTART, false);
 
-			MessageDialog.openInformation(Display.getCurrent().getActiveShell(), Messages.ApplicationWorkbenchWindowAdvisor_0, 
-			Messages.ApplicationWorkbenchWindowAdvisor_1);
-		}
-		
-		
-		
-		showFirstSteps();
-		preloadDBMapper();
-	}
+            MessageDialog.openInformation(Display.getCurrent().getActiveShell(), Messages.ApplicationWorkbenchWindowAdvisor_0, Messages.ApplicationWorkbenchWindowAdvisor_1);
+        }
 
-	private void preloadDBMapper() {
-		WorkspaceJob job = new WorkspaceJob(Messages.ApplicationWorkbenchWindowAdvisor_2) {
-			public IStatus runInWorkspace(IProgressMonitor monitor) {
-				Activator.inheritVeriniceContextState();
-				CnAElementHome.getInstance().preload(CnAWorkspace.getInstance().getConfDir());
-				return Status.OK_STATUS;
-			}
-		};
-		job.schedule();
-		
-	}
+        showFirstSteps();
+        preloadDBMapper();
+    }
 
-	
-	private void showFirstSteps() {
-		PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
-				.addPartListener(new IPartListener() {
+    private void preloadDBMapper() {
+        WorkspaceJob job = new WorkspaceJob(Messages.ApplicationWorkbenchWindowAdvisor_2) {
+            @Override
+            public IStatus runInWorkspace(IProgressMonitor monitor) {
+                Activator.inheritVeriniceContextState();
+                CnAElementHome.getInstance().preload(CnAWorkspace.getInstance().getConfDir());
+                return Status.OK_STATUS;
+            }
+        };
+        job.schedule();
 
-					public void partActivated(IWorkbenchPart part) {
-					}
+    }
 
-					public void partBroughtToTop(IWorkbenchPart part) {
-					}
+    private void showFirstSteps() {
+        PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().addPartListener(new IPartListener() {
 
-					public void partClosed(IWorkbenchPart part) {
-						if (part instanceof ViewIntroAdapterPart)
-							if (Activator.getDefault().getPluginPreferences()
-									.getBoolean(PreferenceConstants.FIRSTSTART)) {
-								Preferences prefs = Activator.getDefault().getPluginPreferences();
-								prefs.setValue(PreferenceConstants.FIRSTSTART, false);
+            public void partActivated(IWorkbenchPart part) {
+            }
 
-								ShowCheatSheetAction action = new ShowCheatSheetAction(true, Messages.ApplicationWorkbenchWindowAdvisor_3);
-								action.run();
-							}
-					}
+            public void partBroughtToTop(IWorkbenchPart part) {
+            }
 
-					public void partDeactivated(IWorkbenchPart part) {
-					}
+            public void partClosed(IWorkbenchPart part) {
+                if (part instanceof ViewIntroAdapterPart) {
+                    if (Activator.getDefault().getPluginPreferences().getBoolean(PreferenceConstants.FIRSTSTART)) {
+                        Preferences prefs = Activator.getDefault().getPluginPreferences();
+                        prefs.setValue(PreferenceConstants.FIRSTSTART, false);
 
-					public void partOpened(IWorkbenchPart part) {
-					}
-				});
+                        ShowCheatSheetAction action = new ShowCheatSheetAction(true, Messages.ApplicationWorkbenchWindowAdvisor_3);
+                        action.run();
+                    }
+                }
+            }
 
-	}
+            public void partDeactivated(IWorkbenchPart part) {
+            }
 
-	public void loadBsiCatalogues() {
-		try {
-			WorkspaceJob job = new OpenCataloguesJob(
-					Messages.ApplicationWorkbenchWindowAdvisor_20);
-			job.setUser(false);
-			job.schedule();
-		} catch (Exception e) {
-			Logger.getLogger(this.getClass()).error(
-					Messages.ApplicationWorkbenchWindowAdvisor_22, e);
-		}
-	}
+            public void partOpened(IWorkbenchPart part) {
+            }
+        });
+
+    }
+
+    public void loadBsiCatalogues() {
+        try {
+            WorkspaceJob job = new OpenCataloguesJob(Messages.ApplicationWorkbenchWindowAdvisor_20);
+            job.setUser(false);
+            job.schedule();
+        } catch (Exception e) {
+            Logger.getLogger(this.getClass()).error(Messages.ApplicationWorkbenchWindowAdvisor_22, e);
+        }
+    }
 
 }
