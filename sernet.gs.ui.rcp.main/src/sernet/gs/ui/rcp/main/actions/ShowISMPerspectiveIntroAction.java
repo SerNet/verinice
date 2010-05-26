a1fbb7a6a2b17e5af74feed8d918c5b5c3300012
Alexander Koderman <ak@sernet.de>
2010-05-26 15:20:39 +0000

    - NEW added introp actions to switch to any perspective with matching tutorial / cheatsheet
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@1700 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

---
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/actions/ShowISMPerspectiveIntroAction.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Alexander Koderman <ak@sernet.de>.
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 
+ * of the License, or (at your option) any later version.
+ *     This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU General Public License for more details.
+ *     You should have received a copy of the GNU General Public 
+ * License along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Alexander Koderman <ak@sernet.de> - initial API and implementation
+ ******************************************************************************/
+package sernet.gs.ui.rcp.main.actions;
+
+import sernet.gs.ui.rcp.main.Perspective;
+import sernet.verinice.iso27k.rcp.Iso27kPerspective;
+import sun.nio.cs.ext.ISO2022_CN;
+
+/**
+ * @author koderman@sernet.de
+ * @version $Rev$ $LastChangedDate$ 
+ * $LastChangedBy$
+ *
+ */
+public class ShowISMPerspectiveIntroAction extends ShowPerspectiveIntroAction {
+
+    /* (non-Javadoc)
+     * @see sernet.gs.ui.rcp.main.actions.ShowPerspectiveIntroAction#getCheatSheetId()
+     */
+    @Override
+    public String getCheatSheetId() {
+        return "sernet.gs.ui.rcp.main.cheatsheet1";
+    }
+
+    /* (non-Javadoc)
+     * @see sernet.gs.ui.rcp.main.actions.ShowPerspectiveIntroAction#getPerspectiveId()
+     */
+    @Override
+    public String getPerspectiveId() {
+        return Iso27kPerspective.ID;
+    }
+
+}
+
+
