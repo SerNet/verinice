937a4fd47dece4e25ff8e4fc0901e5162f21ade7
Sebastian Engel <s.engel@tarent.de>
2010-06-04 15:39:08 +0000

    - added methods for encrypting OutputStreams (e.g. for saving encrypted files) and decrypting InputStreams (e.g. for loading encrypted files)
    - extracted methods for Password Based Encryption in own abstract class
    
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@1739 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.verinice.encryption/src/sernet/verinice/encryption/IEncryptionService.java
+++ sernet.verinice.encryption/src/sernet/verinice/encryption/IEncryptionService.java
@@ -1,14 +1,16 @@
-/**
- * 
- */
 package sernet.verinice.encryption;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import sernet.verinice.encryption.impl.EncryptionException;
 
 /**
+ * Interface declaring the contract of the EncryptionService.
  * 
  * @author sengel <s.engel.@tarent.de>
- *
+ * 
  */
 public interface IEncryptionService {
 
@@ -24,7 +26,7 @@ public interface IEncryptionService {
 	 *             when a problem occured during the encryption process
 	 */
 	byte[] encrypt(byte[] unencryptedByteData, char[] password) throws EncryptionException;
-	
+
 	/**
 	 * Decrypts the given byte data with the given password using the AES algorithm.
 	 * 
@@ -37,4 +39,37 @@ public interface IEncryptionService {
 	 *             when a problem occured during the decryption process
 	 */
 	byte[] decrypt(byte[] encryptedByteData, char[] password) throws EncryptionException;
+
+	/**
+	 * Encrypts data received from the given OutputStream using the AES algorithm.
+	 * 
+	 * @param dataStream
+	 *            the OutputStream providing the unencrypted data to encrypt
+	 * @param password
+	 *            the password used for encryption
+	 * @return an OutputStream providing the encrypted data
+	 * @throws EncryptionException
+	 *             when a problem occured during the en- or decryption process
+	 * @throws IOException
+	 *             when there was a problem reading from the InputStream
+	 */
+	OutputStream encrypt(OutputStream unencryptedDataStream, char[] password)
+			throws EncryptionException, IOException;
+	
+	/**
+	 * Decrypts data received from the given InputStream using the AES algorithm.
+	 * 
+	 * @param encryptedInputStream
+	 *            the InputStream providing the encrypted data to decrypt
+	 * @param password
+	 *            the password used for decryption
+	 * @return an InputStream providing the decrypted data
+	 * @throws EncryptionException
+	 *             when a problem occured during the en- or decryption process
+	 * @throws IOException
+	 *             when there was a problem reading from the InputStream
+	 */
+	InputStream decrypt(InputStream encryptedInputStream, char[] password)
+		throws EncryptionException, IOException;
+
 }
--- sernet.verinice.encryption/src/sernet/verinice/encryption/impl/EncryptionService.java
+++ sernet.verinice.encryption/src/sernet/verinice/encryption/impl/EncryptionService.java
@@ -1,13 +1,9 @@
 package sernet.verinice.encryption.impl;
 
-import java.security.GeneralSecurityException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.security.Security;
-
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.PBEParameterSpec;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
@@ -25,33 +21,10 @@ import sernet.verinice.encryption.IEncryptionService;
  * More information on BouncyCastle can be found on <a
  * href="http://www.bouncycastle.org">http://www.bouncycastle.org</a>.
  * 
- * <p>
- * Information on Password Based Encryption can be found in <a
- * href="http://www.ietf.org/rfc/rfc2898.txt">RFC2898</a>.
- * </p>
- * 
  * @author sengel <s.engel.@tarent.de>
  * 
  */
 public class EncryptionService implements IEncryptionService {
-
-	/**
-	 * The salt used for Password Based En- and decryption. This should be at least 64 bit long ...
-	 */
-	private static final byte[] SALT = { (byte) 0xa3, (byte) 0x51, (byte) 0x56, (byte) 0x7b,
-			(byte) 0x9d, (byte) 0xf5, (byte) 0xf3, (byte) 0xff };
-
-	/**
-	 * The iteration count used for Password Based Encryption. A length of at least 1000 is
-	 * recommended.
-	 */
-	private static final int ITERATION_COUNT = 1200;
-
-	/**
-	 * The algorithm used for en- and decryption. ASE in is only available through the BouncyCastle
-	 * library.
-	 */
-	private static final String ENCRYPTION_ALGORITHM = "PBEWITHSHA256AND256BITAES-CBC-BC";
 
 	/**
 	 * Creates a new instance of EncryptionService. Adds BouncyCastle as a SecurityProvider.
@@ -64,84 +37,26 @@ public class EncryptionService implements IEncryptionService {
 		}
 	}
 
-	/**
-	 * Encrypts the given byte data with the given password using AES.
-	 * 
-	 * @param unencryptedByteData
-	 *            the data to encrypt
-	 * @param password
-	 *            the password used for encryption
-	 * @return the encrypted data as array of bytes
-	 * @throws EncryptionException
-	 *             when a problem occured during the encryption process
-	 */
+	@Override
 	public byte[] encrypt(byte[] unencryptedByteData, char[] password) throws EncryptionException {
-		return performPasswordEncryption(unencryptedByteData, password, false);
+		return PasswordBasedEncryption.encrypt(unencryptedByteData, password);
 	}
 
-	/**
-	 * Decrypts the given byte data with the given password using AES.
-	 * 
-	 * @param encryptedByteData
-	 *            the data to decrypt
-	 * @param password
-	 *            the password used for decryption
-	 * @return the decrypted data as array of bytes
-	 * @throws EncryptionException
-	 *             when a problem occured during the decryption process
-	 */
+	@Override
 	public byte[] decrypt(byte[] encryptedByteData, char[] password) throws EncryptionException {
-		return performPasswordEncryption(encryptedByteData, password, true);
+		return PasswordBasedEncryption.decrypt(encryptedByteData, password);
 	}
 
-	// ##################################################################
-	// ##### Static methods that actually do the en- and decryption #####
-	// ##################################################################
+	@Override
+	public OutputStream encrypt(OutputStream unencryptedDataStream, char[] password)
+			throws EncryptionException, IOException {
+		return PasswordBasedEncryption.encrypt(unencryptedDataStream, password);
+	}
 
-	/**
-	 * En- or decrypts the given byte data with the given password using AES.
-	 * 
-	 * @param byteData
-	 *            the byte data to en- or decrypt
-	 * @param password
-	 *            the password used for en- or decryption
-	 * @param decryptMode
-	 *            if true, this method performs a decryption instead of encryption on the given data
-	 * @return the en- or decrypted data as array of bytes
-	 * @throws EncryptionException
-	 *             when a problem occured during the encryption process
-	 */
-	private static byte[] performPasswordEncryption(byte[] byteData, char[] password,
-			boolean decryptMode) throws EncryptionException {
-
-		byte[] decryptedData = new byte[] {};
-
-		PBEKeySpec pbeKeySpec = new PBEKeySpec(password);
-		PBEParameterSpec pbeParameterSpec = new PBEParameterSpec(SALT, ITERATION_COUNT);
-
-		try {
-			SecretKeyFactory secretKeyFactory = 
-				SecretKeyFactory.getInstance(ENCRYPTION_ALGORITHM, "BC");
-			SecretKey pbeKey = secretKeyFactory.generateSecret(pbeKeySpec);
-
-			// Generate and initialize a PBE cipher
-			Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM, "BC");
-
-			if (decryptMode) {
-				cipher.init(Cipher.DECRYPT_MODE, pbeKey, pbeParameterSpec);
-			} else {
-				cipher.init(Cipher.ENCRYPT_MODE, pbeKey, pbeParameterSpec);
-			}
-
-			// encrypt
-			decryptedData = cipher.doFinal(byteData);
-
-		} catch (GeneralSecurityException e) {
-			throw new EncryptionException(
-					"There was a problem during the en- or decryption process. "
-							+ "See the stacktrace for details.", e);
-		}
-		return decryptedData;
+	@Override
+	public InputStream decrypt(InputStream encryptedInputStream, char[] password)
+			throws EncryptionException, IOException {
+		return PasswordBasedEncryption.decrypt(encryptedInputStream, password);
 	}
 
 }
