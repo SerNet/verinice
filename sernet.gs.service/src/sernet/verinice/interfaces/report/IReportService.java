87a953187dfa390d0f8ab745088d2714c0e054d2
Robert Schuster <r.schuster@tarent.de>
2010-07-28 09:16:11 +0000

    - simplified IReportService interface
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@2063 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.service/src/sernet/verinice/interfaces/report/IReportService.java
+++ sernet.gs.service/src/sernet/verinice/interfaces/report/IReportService.java
@@ -2,9 +2,5 @@ package sernet.verinice.interfaces.report;
 
 public interface IReportService {
 
-	void runTestReportGeneration();
-
-	void runSamtReportGeneration(IReportOptions reportOptions);
-	
 	IReportType[] getReportTypes();
 }
