46a7108a498abd014eb20e37228d8353f2170195
Daniel Murygin <dm@sernet.de>
2010-07-30 14:53:53 +0000

    - avoid NPEs in GSTOOL-Import, tt#64639
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@2080 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/model/CnAElementBuilder.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/model/CnAElementBuilder.java
@@ -55,48 +55,48 @@ public class CnAElementBuilder {
 	public CnATreeElement buildAndSave(ITVerbund itverbund, String typeId) throws Exception {
 		CnAElementFactory factory = CnAElementFactory.getInstance();
 	
-		if (typeId.equals(Anwendung.TYPE_ID)) {
+		if (Anwendung.TYPE_ID.equals(typeId)) {
 			CnATreeElement category = itverbund.getCategory(AnwendungenKategorie.TYPE_ID);
 			return CnAElementFactory.getInstance().saveNew(category, Anwendung.TYPE_ID, null);
 		}
 		
-		else if (typeId.equals(Client.TYPE_ID)) {
+		else if (Client.TYPE_ID.equals(typeId)) {
 			CnATreeElement category = itverbund.getCategory(ClientsKategorie.TYPE_ID);
 			return CnAElementFactory.getInstance().saveNew(category, Client.TYPE_ID, null);
 		} 
 	
-		else if (typeId.equals(Server.TYPE_ID)) {
+		else if (Server.TYPE_ID.equals(typeId)) {
 			CnATreeElement category = itverbund.getCategory(ServerKategorie.TYPE_ID);
 			return CnAElementFactory.getInstance().saveNew(category, Server.TYPE_ID, null);
 		}
 	
-		else if (typeId.equals(Person.TYPE_ID)) {
+		else if (Person.TYPE_ID.equals(typeId)) {
 			CnATreeElement category = itverbund.getCategory(PersonenKategorie.TYPE_ID);
 			return CnAElementFactory.getInstance().saveNew(category, Person.TYPE_ID, null);
 		
 		}
 	
-		else if (typeId.equals(TelefonKomponente.TYPE_ID)) {
+		else if (TelefonKomponente.TYPE_ID.equals(typeId)) {
 			CnATreeElement category = itverbund.getCategory(TKKategorie.TYPE_ID);
 			return CnAElementFactory.getInstance().saveNew(category, TelefonKomponente.TYPE_ID, null);
 		}
 		
-		else if (typeId.equals(SonstIT.TYPE_ID)) {
+		else if (SonstIT.TYPE_ID.equals(typeId)) {
 			CnATreeElement category = itverbund.getCategory(SonstigeITKategorie.TYPE_ID);
 			return CnAElementFactory.getInstance().saveNew(category, SonstIT.TYPE_ID, null);
 		} 
 		
-		else if (typeId.equals(NetzKomponente.TYPE_ID)) {
+		else if (NetzKomponente.TYPE_ID.equals(typeId)) {
 			CnATreeElement category = itverbund.getCategory(NKKategorie.TYPE_ID);
 			return CnAElementFactory.getInstance().saveNew(category, NetzKomponente.TYPE_ID, null);
 		}
 	
-		else if (typeId.equals(Gebaeude.TYPE_ID)) {
+		else if (Gebaeude.TYPE_ID.equals(typeId)) {
 			CnATreeElement category = itverbund.getCategory(GebaeudeKategorie.TYPE_ID);
 			return CnAElementFactory.getInstance().saveNew(category, Gebaeude.TYPE_ID, null);
 		} 
 	
-		else if (typeId.equals(Raum.TYPE_ID)) {
+		else if (Raum.TYPE_ID.equals(typeId)) {
 			CnATreeElement category = itverbund.getCategory(RaeumeKategorie.TYPE_ID);
 			return CnAElementFactory.getInstance().saveNew(category, Raum.TYPE_ID, null);
 		}
