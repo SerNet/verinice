d83ac5bc3afee85f2d1160118b8bed48eaf2065d
Robert Schuster <r.schuster@tarent.de>
2010-08-06 09:39:50 +0000

    - added documentation - renamed methods for better understanding of the concepts - added note about making the toc generation better
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@2104 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.verinice.report.service/src/sernet/verinice/report/service/impl/TocHelper.java
+++ sernet.verinice.report.service/src/sernet/verinice/report/service/impl/TocHelper.java
@@ -15,20 +15,56 @@ import java.util.ArrayList;
  * 
  */
 public class TocHelper {
+	/* TODO: Things to make this work better
+	 * - BIRT has built-in support for objects persisting report generation runs. Use that and make
+	 * this class not a bunch of static methods
+	 * - intensely use the chapter retrieval commands in the report to find all the TOC entries in
+	 * the first place (this reduces the amount of needed runs by one)
+	 * - find a way to modify the TOC table's page entries after the TOC table has been generated and
+	 * filled (If this is possible then the amount of neccessary runs is decreased by one again) 
+	 */
 
+	/** A page number counter that is being updated while the report is being generated.
+	 * 
+	 * The current value is stored along with a TOC entry.
+	 */
 	private static int pageNumber = 1;
 
+	/** A counter for the amount of pages the TOC itself requires. This is the only possibility
+	 * to correctly calculate the actual page for a TOC entry.
+	 */
 	private static int tocPageCount = 0;
 
+	/**
+	 * The TocHelpers state is preserved during runs of the report generation. It works
+	 * slightly different depending on which run it is. This is e.g. the behavior at the end
+	 * of each generation process:
+	 * <ul>
+	 * <li>First run: Collect TOC entries</li>
+	 * <li>Second run: Find out TOC page count</li>
+	 * <li>Third run: Reset everything</li>
+	 * </ul>
+	 */
 	private static int resetCount = 0;
 
+	/** When the TOC page count has been found out it is copied into this variable for usage
+	 * in the final run.
+	 */
 	private static int finalTocPageCount = 0;
 
+	/** When the TOC items have been found out they are copied into this list to be used in
+	 * the final run.
+	 */
 	private static ArrayList<String> finalItems = new ArrayList<String>();
 
+	/** When the TOC items' page numbers have been found out they are copied into this
+	 * list to be used in the final run.
+	 */
 	private static ArrayList<Integer> finalPages = new ArrayList<Integer>();
 
 	static {
+		// Initialize the TOC item list with a helpful message (just in case someone
+		// sees a report that was made out of the first run).
 		finalItems.add("<em>TOC generation needs 2 more runs!</em>");
 		finalPages.add(-1);
 	}
@@ -37,10 +73,25 @@ public class TocHelper {
 
 	private static ArrayList<Integer> pages = new ArrayList<Integer>();
 
+	/**
+	 * Returns the amount of TOC items in the report.
+	 * 
+	 * Supposed to be correct only in the 2nd and 3rd run.
+	 * 
+	 * @return
+	 */
 	public static int getTocCount() {
 		return finalItems.size();
 	}
 
+	/**
+	 * Adds a TOC item to the dataset.
+	 * 
+	 * <p>The <code>depth</code> value can be used for indentation.</p>
+	 * 
+	 * @param item
+	 * @param depth
+	 */
 	public static void addTocItem(String item, int depth) {
 		StringBuilder sb = new StringBuilder(depth + item.length());
 		for (int i = 0; i < depth; i++)
@@ -51,23 +102,56 @@ public class TocHelper {
 		pages.add(pageNumber);
 	}
 
+	/**
+	 * Retrieves a TOC item with the specified index.
+	 * 
+	 * <p>Note: Proper values (= those that reflect the actual report content) is
+	 * only supposed to be available in the 2nd and 3rd run.</p>
+	 * 
+	 * @param i
+	 * @return
+	 */
 	public static String getTocItem(int i) {
 		return finalItems.get(i);
 	}
 
-	public static Integer getTocPage(int i) {
+	/**
+	 * Retrieves a TOC item's page number.
+	 * 
+	 * <p>Warning: The value is supposed to be correct <em>only</em> in the 3rd run.</p>
+	 * 
+	 * @param i
+	 * @return
+	 */
+	public static Integer getTocItemPage(int i) {
 		return (resetCount == 2) ? finalPages.get(i) + finalTocPageCount : -1;
 	}
 
-	public static void resetPages() {
+	/**
+	 * Method to be called when one report generation run starts.
+	 * 
+	 * <p>It resets various internal counters.</p>
+	 */
+	public static void startRun() {
 		pageNumber = 1;
 		tocPageCount = 0;
 	}
 
-	public static void resetToc() {
+	/**
+	 * Method to be called when one report generation run ends.
+	 * 
+	 * <p>Changes the {@link TocHelper}'s internal state.</p>
+	 */
+	public static void endRun() {
 		++resetCount;
 		switch (resetCount) {
 		case 1:
+			// End of first run:
+			// Clear the example TOC items and use the collected ones instead.
+			//
+			// It is neccessary to provide the correct TOC entries because otherwise
+			// the 2nd step could not determine the amount of pages the TOC would require
+			// itself.
 			finalItems.clear();
 			finalPages.clear();
 			
@@ -79,6 +163,11 @@ public class TocHelper {
 
 			break;
 		case 2:
+			// End of 2nd run:
+			// Refresh the whole dataset
+			// Note: Actually it would only be neccessary to refresh the page numbers. However
+			// with random data being used for the report the actual amount of TOC entries could
+			// differ between the 1st and 2nd run. 
 			finalItems.clear();
 			finalPages.clear();
 			
@@ -89,6 +178,8 @@ public class TocHelper {
 
 			break;
 		case 3:
+			// End of 3rd run:
+			// Clear the dataset (and put example data into it).
 			finalItems.clear();
 			finalPages.clear();
 			
@@ -98,16 +189,25 @@ public class TocHelper {
 			items.clear();
 			pages.clear();
 
+			// Makes the process start over.
 			resetCount = 0;
 			break;
 		}
 
 	}
 
+	/**
+	 * Increases the page count. This method should be called whenever a general page break
+	 * happens. (E.g. override the document's "onPageBreak()" method.)
+	 */
 	public static void increasePageNumber() {
 		pageNumber++;
 	}
 
+	/**
+	 * Increases the TOC page count. This method should be called whenever a page break of the table
+	 * that resembles the TOC happens. (E.g. override the table's "onPageBreak()" method.)
+	 */
 	public static void increaseTocPageCount() {
 		tocPageCount++;
 	}
