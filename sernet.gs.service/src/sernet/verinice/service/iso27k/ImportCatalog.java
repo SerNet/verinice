4797aaacf351c64357d050f5783356c296eccf19
Daniel Murygin <dm@sernet.de>
2010-09-16 13:41:17 +0000

    clean up
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@2189 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.service/src/sernet/verinice/service/iso27k/ImportCatalog.java
+++ sernet.gs.service/src/sernet/verinice/service/iso27k/ImportCatalog.java
@@ -31,191 +31,191 @@ import sernet.gs.service.RuntimeCommandException;
 import sernet.gs.service.VeriniceCharset;
 import sernet.verinice.interfaces.GenericCommand;
 import sernet.verinice.interfaces.iso27k.ICatalog;
-import sernet.verinice.service.iso27k.Catalog;
 import sernet.verinice.interfaces.iso27k.ICatalogImporter;
-import sernet.verinice.service.iso27k.Item;
-import sernet.verinice.service.iso27k.ImportConfiguration;
 import au.com.bytecode.opencsv.CSVReader;
 
 /**
  * Command to import a CSV file.
  * 
- * ImportCatalog creates a {@link Catalog} from the file content.
- * To import an file create a new {@link ImportCatalog} by passing
- * the full file path and execute the command
+ * ImportCatalog creates a {@link Catalog} from the file content. To import an
+ * file create a new {@link ImportCatalog} by passing the full file path and
+ * execute the command
  * 
  * File content is read from the file and stored in an byte array by contructor.
  * 
  * @author Daniel Murygin <dm[at]sernet[dot]de>
  * 
- * Contributions:
- * koderman[at]sernet[dot]de added support for maturity levels
+ *         Contributions: koderman[at]sernet[dot]de added support for maturity
+ *         levels
  */
 @SuppressWarnings("serial")
 public class ImportCatalog extends GenericCommand implements ICatalogImporter {
 
-	private transient Logger log = Logger.getLogger(ImportCatalog.class);
-	
-	public Logger getLog() {
-		if(log==null) {
-			log = Logger.getLogger(ImportCatalog.class);
-		}
-		return log;
-	}
-	
-	ImportConfiguration config;
+    private transient Logger log = Logger.getLogger(ImportCatalog.class);
 
-	CsvFile csvFile;
-	
-	Catalog catalog = new Catalog();
-	
-	public ImportCatalog(String filePath) throws IOException {
-		this(filePath, VeriniceCharset.CHARSET_DEFAULT);
-	}
-	public ImportCatalog(String filePath,Charset charset) throws IOException {
-        super();
-        csvFile = new CsvFile(filePath,charset);
+    public Logger getLog() {
+        if (log == null) {
+            log = Logger.getLogger(ImportCatalog.class);
+        }
+        return log;
     }
-	
-	public ImportCatalog(InputStream is) throws IOException {
+
+    ImportConfiguration config;
+
+    CsvFile csvFile;
+
+    Catalog catalog = new Catalog();
+
+    public ImportCatalog(String filePath) throws IOException {
+        this(filePath, VeriniceCharset.CHARSET_DEFAULT);
+    }
+
+    public ImportCatalog(String filePath, Charset charset) throws IOException {
+        super();
+        csvFile = new CsvFile(filePath, charset);
+    }
+
+    public ImportCatalog(InputStream is) throws IOException {
         super();
         csvFile = new CsvFile(is);
     }
-	
-	public ImportCatalog(byte[] fileContent) throws IOException {
-		super();
-		csvFile = new CsvFile(fileContent);
-	}
 
-	/* (non-Javadoc)
-	 * @see sernet.gs.ui.rcp.main.service.commands.ICommand#execute()
-	 */
-	public void execute() {
-		try {
-			config = new ImportConfiguration(csvFile);			
-			importCatalog();
-		} catch(Exception e) {
-			getLog().error("Error while executing", e);
-			throw new RuntimeCommandException("Fehler beim Importieren des Katalogs.", e);
-		}	
-	}
+    public ImportCatalog(byte[] fileContent) throws IOException {
+        super();
+        csvFile = new CsvFile(fileContent);
+    }
 
-	/* (non-Javadoc)
-	 * @see sernet.verinice.iso27k.service.ICatalogImporter#importCatalog()
-	 */
-	public void importCatalog() { 
-		try {
-			CSVReader reader = new CSVReader(
-					new BufferedReader(new InputStreamReader(new ByteArrayInputStream(csvFile.getFileContent()), Charset.forName("UTF-8"))),
-					config.getSeperator(),
-					'"',
-					false
-			);
-			String[] nextLine;
-			Item item = null;
-		    while ((nextLine = reader.readNext()) != null) {
-		        // nextLine[] is an array of values from the line	
-		    	if(nextLine.length>=3) {
-    		        if(isNewTopic(nextLine)) {
-		        if (getLog().isDebugEnabled()) {
-		        	getLog().debug("#: " + nextLine[0]);
-		        	getLog().debug("heading: " + nextLine[1]);
-		        	getLog().debug("type: " + nextLine[2]);
-		        	getLog().debug("text: " + nextLine[3]);
-		        	
-		        	// line can have optional weight and threshold levels:
-		        	if (hasMaturityLevels(nextLine)) {
-		        		getLog().debug("maturity: " + nextLine[4]);
-		        		getLog().debug("weight 1: " + nextLine[5]);
-		        		getLog().debug("weight 2: " + nextLine[6]);
-		        		getLog().debug("threshold 1: " + nextLine[7]);
-		        		getLog().debug("threshold 2: " + nextLine[8]);
-		        		
-		        	}
-				}	     
-		       
-		        	if(item!=null) {
-		        		// buffer the old item
-		        		catalog.bufferItem(item);
-		        	}
-		        	// create a new one
-		        	item = new Item(nextLine[1],nextLine[2]);
-		        	item.setNumberString(nextLine[0].trim());
-		        	
-		        	item.setDescription(nextLine[3]);
-	
-		        	if (hasMaturityLevels(nextLine)) {
-		        		fillMaturityLevels(item, nextLine);
-		        	}
-		        	
-		        } else {
-		        	// add a new paragraph to the existing item
-		        	StringBuilder sb = new StringBuilder(item.getDescription());
-		        	sb.append("<p>").append(nextLine[3]).append("</p>");
-		        	item.setDescription(sb.toString());
-		        }
-		    	} else {
-		    	    log.warn("Invalid line in CSV file.");
-		    	}
-		    }
-		    // buffer the last item
-		    catalog.bufferItem(item);
-		    
-		    // create the tree
-		    catalog.processItemBuffer();
-		    
-		    if (getLog().isDebugEnabled()) {
-		    	getLog().debug(catalog);
-			}
-		    
-		} catch(IOException e) {
-			getLog().error("Error while importing", e);
-			throw new RuntimeException("Error while importing", e);
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see sernet.gs.ui.rcp.main.service.commands.ICommand#execute()
+     */
+    public void execute() {
+        try {
+            config = new ImportConfiguration(csvFile);
+            importCatalog();
+        } catch (Exception e) {
+            getLog().error("Error while executing", e);
+            throw new RuntimeCommandException("Fehler beim Importieren des Katalogs.", e);
+        }
+    }
 
-	/**
+    /*
+     * (non-Javadoc)
+     * 
+     * @see sernet.verinice.iso27k.service.ICatalogImporter#importCatalog()
+     */
+    public void importCatalog() {
+        try {
+            CSVReader reader = new CSVReader(new BufferedReader(new InputStreamReader(new ByteArrayInputStream(csvFile.getFileContent()), Charset.forName("UTF-8"))), config.getSeperator(), '"', false);
+            String[] nextLine;
+            Item item = null;
+            while ((nextLine = reader.readNext()) != null) {
+                // nextLine[] is an array of values from the line
+                if (nextLine.length >= 3) {
+                    if (isNewTopic(nextLine)) {
+                        if (getLog().isDebugEnabled()) {
+                            getLog().debug("#: " + nextLine[0]);
+                            getLog().debug("heading: " + nextLine[1]);
+                            getLog().debug("type: " + nextLine[2]);
+                            getLog().debug("text: " + nextLine[3]);
+
+                            // line can have optional weight and threshold
+                            // levels:
+                            if (hasMaturityLevels(nextLine)) {
+                                getLog().debug("maturity: " + nextLine[4]);
+                                getLog().debug("weight 1: " + nextLine[5]);
+                                getLog().debug("weight 2: " + nextLine[6]);
+                                getLog().debug("threshold 1: " + nextLine[7]);
+                                getLog().debug("threshold 2: " + nextLine[8]);
+
+                            }
+                        }
+
+                        if (item != null) {
+                            // buffer the old item
+                            catalog.bufferItem(item);
+                        }
+                        // create a new one
+                        item = new Item(nextLine[1], nextLine[2]);
+                        item.setNumberString(nextLine[0].trim());
+
+                        item.setDescription(nextLine[3]);
+
+                        if (hasMaturityLevels(nextLine)) {
+                            fillMaturityLevels(item, nextLine);
+                        }
+
+                    } else {
+                        // add a new paragraph to the existing item
+                        StringBuilder sb = new StringBuilder(item.getDescription());
+                        sb.append("<p>").append(nextLine[3]).append("</p>");
+                        item.setDescription(sb.toString());
+                    }
+                } else {
+                    log.warn("Invalid line in CSV file.");
+                }
+            }
+            // buffer the last item
+            catalog.bufferItem(item);
+
+            // create the tree
+            catalog.processItemBuffer();
+
+            if (getLog().isDebugEnabled()) {
+                getLog().debug(catalog);
+            }
+
+        } catch (IOException e) {
+            getLog().error("Error while importing", e);
+            throw new RuntimeException("Error while importing", e);
+        }
+    }
+
+    /**
      * @param nextLine
      * @return
      */
     private boolean isNewTopic(String[] nextLine) {
-        return nextLine[0]!=null && nextLine[0].length()>0;
+        return nextLine[0] != null && nextLine[0].length() > 0;
     }
 
     /**
-	 * @param item
-	 * @param nextLine
-	 */
-	private void fillMaturityLevels(Item item, String[] nextLine) {
-	    item.setWeight1(nextLine[4]);
-	    item.setWeight2(nextLine[5]);
-		item.setMaturity(nextLine[6]);
-		item.setThreshold1(nextLine[7]);
-		item.setThreshold2(nextLine[8]);
-		item.setMaturityLevelSupport(true);
-	}
+     * @param item
+     * @param nextLine
+     */
+    private void fillMaturityLevels(Item item, String[] nextLine) {
+        item.setWeight1(nextLine[4]);
+        item.setWeight2(nextLine[5]);
+        item.setMaturity(nextLine[6]);
+        item.setThreshold1(nextLine[7]);
+        item.setThreshold2(nextLine[8]);
+        item.setMaturityLevelSupport(true);
+    }
 
-	/**
-	 * @param nextLine
-	 * @return
-	 */
-	private boolean hasMaturityLevels(String[] nextLine) {
-		return nextLine.length == 9;
-	}
+    /**
+     * @param nextLine
+     * @return
+     */
+    private boolean hasMaturityLevels(String[] nextLine) {
+        return nextLine.length == 9;
+    }
 
-	public void setCsvFile(CsvFile csvFile) {
-		this.csvFile = csvFile;
-	}
+    public void setCsvFile(CsvFile csvFile) {
+        this.csvFile = csvFile;
+    }
 
-	public CsvFile getCsvFile() {
-		return csvFile;
-	}
+    public CsvFile getCsvFile() {
+        return csvFile;
+    }
 
-	/* (non-Javadoc)
-	 * @see sernet.verinice.iso27k.service.ICatalogImporter#getTree()
-	 */
-	public ICatalog getCatalog() {
-		return catalog;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see sernet.verinice.iso27k.service.ICatalogImporter#getTree()
+     */
+    public ICatalog getCatalog() {
+        return catalog;
+    }
 
 }
