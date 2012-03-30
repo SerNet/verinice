28819bea97eacc826903d8ba02abe668f7009c5b
Daniel Murygin <dm@sernet.de>
2012-03-30 09:34:52 +0000

    -No more crashes of Browser View on Windows (Bug 221)
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@3029 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/views/BrowserView.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/views/BrowserView.java
@@ -17,11 +17,6 @@
  ******************************************************************************/
 package sernet.gs.ui.rcp.main.bsi.views;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 import org.apache.log4j.Logger;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
@@ -38,33 +33,18 @@ import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.part.ViewPart;
 
-import sernet.gs.model.Baustein;
-import sernet.gs.model.Gefaehrdung;
-import sernet.gs.model.Massnahme;
 import sernet.gs.service.GSServiceException;
-import sernet.gs.service.VeriniceCharset;
 import sernet.gs.ui.rcp.main.Activator;
 import sernet.gs.ui.rcp.main.CnAWorkspace;
 import sernet.gs.ui.rcp.main.ExceptionUtil;
 import sernet.gs.ui.rcp.main.ImageCache;
 import sernet.gs.ui.rcp.main.StatusLine;
 import sernet.gs.ui.rcp.main.bsi.editors.BSIElementEditorInput;
-import sernet.gs.ui.rcp.main.bsi.model.GSScraperUtil;
-import sernet.gs.ui.rcp.main.bsi.model.TodoViewItem;
-import sernet.gs.ui.rcp.main.bsi.risikoanalyse.model.RisikoMassnahmeHome;
 import sernet.gs.ui.rcp.main.preferences.PreferenceConstants;
 import sernet.verinice.interfaces.ActionRightIDs;
-import sernet.verinice.interfaces.iso27k.IItem;
 import sernet.verinice.iso27k.rcp.ILinkedWithEditorView;
 import sernet.verinice.iso27k.rcp.LinkWithEditorPartListener;
-import sernet.verinice.model.bsi.BausteinUmsetzung;
-import sernet.verinice.model.bsi.MassnahmenUmsetzung;
-import sernet.verinice.model.bsi.risikoanalyse.GefaehrdungsUmsetzung;
-import sernet.verinice.model.bsi.risikoanalyse.RisikoMassnahmenUmsetzung;
 import sernet.verinice.model.common.CnATreeElement;
-import sernet.verinice.model.iso27k.IControl;
-import sernet.verinice.model.iso27k.Threat;
-import sernet.verinice.model.iso27k.Vulnerability;
 
 @SuppressWarnings("restriction")
 public class BrowserView extends ViewPart implements ILinkedWithEditorView {
@@ -82,6 +62,8 @@ public class BrowserView extends ViewPart implements ILinkedWithEditorView {
     private Action linkWithEditorAction;
 
     private boolean linkingActive = true;
+    
+    SerialiseBrowserLoadingListener serialiseListener;
 
 	public void createPartControl(Composite parent) {
 		GridLayout gl = new GridLayout(1, false);
@@ -91,9 +73,12 @@ public class BrowserView extends ViewPart implements ILinkedWithEditorView {
 			browser = new Browser(parent, SWT.NONE);
 			browser.setLayoutData(new GridData(GridData.FILL_BOTH
 					| GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL));
-
-			browser.setUrl(defaultImage());
 			
+			serialiseListener = new SerialiseBrowserLoadingListener(browser);
+			browser.addProgressListener(serialiseListener);
+			
+			browser.setUrl(defaultImage());
+
 			linkWithEditorAction = new Action(Messages.BrowserView_0, IAction.AS_CHECK_BOX) {
 	            @Override
 	            public void run() {
@@ -156,183 +141,17 @@ public class BrowserView extends ViewPart implements ILinkedWithEditorView {
 	protected void elementSelected(Object element) {
 	    try {
             StatusLine.setErrorMessage(""); //$NON-NLS-1$
-            if (element instanceof Massnahme) {
-                Massnahme mn = (Massnahme) element;
-                setText(GSScraperUtil.getInstance().getModel().getMassnahmeHtml(mn.getUrl(), mn.getStand()));
-            }
-
-            if (element instanceof Baustein) {
-                Baustein bst = (Baustein) element;
-                setUrl(GSScraperUtil.getInstance().getModel().getBaustein(bst.getUrl(), bst.getStand()), bst.getEncoding());
-            }
-
-            if (element instanceof Gefaehrdung) {
-                Gefaehrdung gef = (Gefaehrdung) element;
-                setUrl(GSScraperUtil.getInstance().getModel().getGefaehrdung(gef.getUrl(), gef.getStand()), gef.getEncoding());
-            }
-
-            if (element instanceof GefaehrdungsUmsetzung) {
-                GefaehrdungsUmsetzung gefUms = (GefaehrdungsUmsetzung) element;
-                if (gefUms.getUrl() == null || gefUms.getUrl().equals("null")) { //$NON-NLS-1$
-                    // try OwnGefaehrdung:
-                    browser.stop();
-                    browser.setText(toHtml(gefUms));
-                    return;
-                }
-                
-                setUrl(GSScraperUtil.getInstance().getModel().getGefaehrdung(gefUms.getUrl(),gefUms.getStand()), "iso-8859-1"); //$NON-NLS-1$
-            }
-
-            if (element instanceof RisikoMassnahmenUmsetzung) {
-                RisikoMassnahmenUmsetzung ums = (RisikoMassnahmenUmsetzung) element;
-                RisikoMassnahmeHome.getInstance().initRisikoMassnahmeUmsetzung(ums);
-                if (ums.getRisikoMassnahme() != null) {
-                    browser.stop();
-                    browser.setText(toHtml(ums));
-                    return;
-                }
-            }
-
-            if (element instanceof MassnahmenUmsetzung) {
-                MassnahmenUmsetzung mnu = (MassnahmenUmsetzung) element;
-                setText(GSScraperUtil.getInstance().getModel().getMassnahmeHtml(mnu.getUrl(), mnu.getStand()));
-            }
-            
-            if (element instanceof TodoViewItem) {
-                TodoViewItem item = (TodoViewItem) element;
-                setText(GSScraperUtil.getInstance().getModel().getMassnahmeHtml(item.getUrl(), item.getStand()));
-            }
-
-            if (element instanceof BausteinUmsetzung) {
-                BausteinUmsetzung bst = (BausteinUmsetzung) element;
-                setUrl(GSScraperUtil.getInstance().getModel().getBaustein(bst.getUrl(), bst.getStand()), bst.getEncoding());
-            }
-            
-            if (element instanceof IItem) {
-                IItem item = (IItem) element;
-                StringBuilder sb = new StringBuilder();
-                writeHtml(sb, item.getName(), item.getDescription(), VeriniceCharset.CHARSET_UTF_8.name());
-                setText(sb.toString()); 
-            }
-            
-            if (element instanceof IControl) {
-                IControl control = (IControl) element;
-                StringBuilder sb = new StringBuilder();
-                writeHtml(sb, control.getTitle(), control.getDescription(), VeriniceCharset.CHARSET_UTF_8.name());
-                setText(sb.toString());         
-            }
-            
-            if (element instanceof Threat) {
-                Threat item = (Threat) element;
-                StringBuilder sb = new StringBuilder();
-                writeHtml(sb, item.getTitle(), item.getDescription(), VeriniceCharset.CHARSET_UTF_8.name());
-                setText(sb.toString());         
-            }
-
-            if (element instanceof Vulnerability) {
-                Vulnerability item = (Vulnerability) element;
-                StringBuilder sb = new StringBuilder();
-                writeHtml(sb, item.getTitle(), item.getDescription(), VeriniceCharset.CHARSET_UTF_8.name());
-                setText(sb.toString());         
-            }
-            
+            setText(HtmlWriter.getHtml(element));
 
         } catch (GSServiceException e) {
-            StatusLine.setErrorMessage(e.getMessage());
-            Logger.getLogger(this.getClass()).error(
-                    Messages.BrowserView_4 + Messages.BrowserView_5);
+            StatusLine.setErrorMessage(Messages.BrowserView_4 + Messages.BrowserView_5);
+            LOG.error(Messages.BrowserView_4 + Messages.BrowserView_5, e);
             browser.setUrl(defaultImage());
         }
 	}
 
-	private String toHtml(GefaehrdungsUmsetzung ums) {
-		StringBuilder buf = new StringBuilder();
-		writeHtml(buf, ums.getId() + " " + ums.getTitle(), ums.getDescription(), "iso-8859-1"); //$NON-NLS-1$ //$NON-NLS-2$
-		return buf.toString();
-	}
-	
-	private void writeHtml(StringBuilder buf, String headline, String bodytext, String encoding) {
-		String cssDir = CnAWorkspace.getInstance().getWorkdir()+ File.separator + "html" + File.separator + "screen.css"; //$NON-NLS-1$ //$NON-NLS-2$
-		buf.append("<html><head>"); //$NON-NLS-1$
-		buf.append("<META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=").append(encoding).append("\"/>\n"); //$NON-NLS-1$ //$NON-NLS-2$
-		buf.append("<link REL=\"stylesheet\" media=\"screen\" HREF=\"").append(cssDir).append("\"/>"); //$NON-NLS-1$ //$NON-NLS-2$
-		buf.append("</head><body><div id=\"content\"><h1>"); //$NON-NLS-1$
-		buf.append(headline);
-		buf.append("</h1><p>"); //$NON-NLS-1$
-		buf.append(""); //$NON-NLS-1$
-		buf.append(bodytext.replaceAll("\\n", "<br/>")); //$NON-NLS-1$ //$NON-NLS-2$
-		buf.append("</p></div></body></html>"); //$NON-NLS-1$
-	}
-
-	private String toHtml(RisikoMassnahmenUmsetzung ums) {
-		StringBuilder buf = new StringBuilder();
-		RisikoMassnahmeHome.getInstance().initRisikoMassnahmeUmsetzung(ums);
-		writeHtml(buf, ums.getNumber() + " " + ums.getName(), ums.getDescription(), "iso-8859-1"); //$NON-NLS-1$ //$NON-NLS-2$
-		return buf.toString();
-	}
-
 	public void setFocus() {
 		browser.setFocus();
-	}
-
-	/**
-	 * Sets the contents to be displayed in the browser window.
-	 * 
-	 * @param is
-	 *            The HTML page to be displayed as an input stream
-	 */
-	public void setUrl(InputStream is, String encoding) {
-		try {
-			if ( !(encoding.equalsIgnoreCase("iso-8859-1") || encoding.equalsIgnoreCase("utf-8")) ) { //$NON-NLS-1$ //$NON-NLS-2$
-				encoding = "utf-8"; //$NON-NLS-1$
-			}
-			
-			InputStreamReader read = new InputStreamReader(is, encoding); //$NON-NLS-1$
-			BufferedReader buffRead = new BufferedReader(read);
-			StringBuilder b = new StringBuilder();
-			String line;
-			boolean skip = false;
-			boolean skipComplete = false;
-			String cssDir = CnAWorkspace.getInstance().getWorkdir()
-					+ File.separator + "html" + File.separator + "screen.css"; //$NON-NLS-1$ //$NON-NLS-2$
-
-			while ((line = buffRead.readLine()) != null) {
-				if (!skipComplete) {
-					if (line.matches(".*div.*id=\"menuoben\".*") //$NON-NLS-1$
-							|| line.matches(".*div.*class=\"standort\".*")) //$NON-NLS-1$
-						skip = true;
-					else if (line.matches(".*div.*id=\"content\".*")) { //$NON-NLS-1$
-						skip = false;
-						skipComplete = true;
-					}
-				}
-
-				// Logger.getLogger(this.getClass()).debug("PRE: " + line);
-
-				// we strip away images et al to keep just the information we
-				// need:
-				line = line.replace("../../media/style/css/screen.css", cssDir); //$NON-NLS-1$
-				line = line.replace("../../../screen.css", cssDir); //$NON-NLS-1$
-				line = line.replace("../../screen.css", cssDir); //$NON-NLS-1$
-				line = line.replace("../screen.css", cssDir); //$NON-NLS-1$
-				line = line.replaceAll("<a.*?>", ""); //$NON-NLS-1$ //$NON-NLS-2$
-				line = line.replaceAll("</a.*?>", ""); //$NON-NLS-1$ //$NON-NLS-2$
-				line = line.replaceAll("<img.*?>", ""); //$NON-NLS-1$ //$NON-NLS-2$
-				line = line.replace((char) 160, ' '); // replace non-breaking
-														// spaces
-
-				// Logger.getLogger(this.getClass()).debug("POST: " + line);
-
-				if (!skip) {
-					// Logger.getLogger(BrowserView.class).debug(line);
-					b.append(line);
-				}
-			}
-			browser.stop();
-			browser.setText(b.toString());
-		} catch (Exception e) {
-			Logger.getLogger(BrowserView.class).error(e);
-		}
 	}
 	
 	/**
@@ -342,8 +161,9 @@ public class BrowserView extends ViewPart implements ILinkedWithEditorView {
 	 *            The HTML page to be displayed as an input stream
 	 */
 	public void setText(String text) {
-		browser.stop();
-		browser.setText(text);
+	    if(text!=null) {
+	        serialiseListener.setText(text);
+	    }
 	}
 
 	@Override
