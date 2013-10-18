5d3613dceea764b9d9be799feeb7ca2425749928
Daniel Murygin <dm@sernet.de>
2013-10-18 13:31:06 +0000

    -Base classes and configuration added to run unit test on services (bug 915)
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@3793 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

---
+++ sernet.gs.server/testSrc/sernet/verinice/service/test/DaoTest.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Daniel Murygin.
+ *
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public License 
+ * as published by the Free Software Foundation, either version 3 
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Daniel Murygin <dm[at]sernet[dot]de> - initial API and implementation
+ ******************************************************************************/
+package sernet.verinice.service.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+
+import sernet.verinice.interfaces.IBaseDao;
+import sernet.verinice.model.common.CnATreeElement;
+
+/**
+ *
+ *
+ * @author Daniel Murygin <dm[at]sernet[dot]de>
+ */
+public class DaoTest extends BaseTest {
+    
+    @Resource(name="cnaTreeElementDao")
+    private IBaseDao<CnATreeElement, Integer> elementDao;
+    
+    @Test
+    public void testDao() throws Exception {
+        CnATreeElement element = elementDao.findByUuid("42340c5c-fa8f-46f3-8e96-6dadddac013e", null);
+        assertNotNull(element);
+    }
+
+}
