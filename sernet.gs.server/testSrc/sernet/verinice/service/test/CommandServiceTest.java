b02025d34bae6db7ce361d9dffd9a92e6c3bc1a9
Daniel Murygin <dm@sernet.de>
2013-10-29 15:42:13 +0000

    -Test for CutCommand added (bug 919)
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@3825 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.server/testSrc/sernet/verinice/service/test/CommandServiceTest.java
+++ sernet.gs.server/testSrc/sernet/verinice/service/test/CommandServiceTest.java
@@ -27,10 +27,8 @@ import static org.junit.Assert.assertTrue;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.Resource;
@@ -50,58 +48,22 @@ import sernet.hui.common.connect.EntityType;
 import sernet.hui.common.connect.HUITypeFactory;
 import sernet.hui.common.connect.PropertyType;
 import sernet.verinice.interfaces.CommandException;
-import sernet.verinice.interfaces.ICommandService;
 import sernet.verinice.iso27k.service.commands.RetrieveCnATreeElement;
 import sernet.verinice.model.bsi.BSIModel;
 import sernet.verinice.model.bsi.IBSIStrukturKategorie;
 import sernet.verinice.model.bsi.ImportBsiGroup;
 import sernet.verinice.model.common.ChangeLogEntry;
 import sernet.verinice.model.common.CnATreeElement;
-import sernet.verinice.model.iso27k.Asset;
-import sernet.verinice.model.iso27k.AssetGroup;
-import sernet.verinice.model.iso27k.Audit;
-import sernet.verinice.model.iso27k.AuditGroup;
-import sernet.verinice.model.iso27k.Control;
-import sernet.verinice.model.iso27k.ControlGroup;
-import sernet.verinice.model.iso27k.Document;
-import sernet.verinice.model.iso27k.DocumentGroup;
-import sernet.verinice.model.iso27k.Evidence;
-import sernet.verinice.model.iso27k.EvidenceGroup;
-import sernet.verinice.model.iso27k.ExceptionGroup;
-import sernet.verinice.model.iso27k.Finding;
-import sernet.verinice.model.iso27k.FindingGroup;
 import sernet.verinice.model.iso27k.Group;
 import sernet.verinice.model.iso27k.IISO27kRoot;
-import sernet.verinice.model.iso27k.ISO27KModel;
 import sernet.verinice.model.iso27k.ImportIsoGroup;
-import sernet.verinice.model.iso27k.Incident;
-import sernet.verinice.model.iso27k.IncidentGroup;
-import sernet.verinice.model.iso27k.IncidentScenario;
-import sernet.verinice.model.iso27k.IncidentScenarioGroup;
-import sernet.verinice.model.iso27k.Interview;
-import sernet.verinice.model.iso27k.InterviewGroup;
 import sernet.verinice.model.iso27k.Organization;
-import sernet.verinice.model.iso27k.PersonGroup;
-import sernet.verinice.model.iso27k.PersonIso;
-import sernet.verinice.model.iso27k.ProcessGroup;
-import sernet.verinice.model.iso27k.Record;
-import sernet.verinice.model.iso27k.RecordGroup;
-import sernet.verinice.model.iso27k.Requirement;
-import sernet.verinice.model.iso27k.RequirementGroup;
-import sernet.verinice.model.iso27k.Response;
-import sernet.verinice.model.iso27k.ResponseGroup;
-import sernet.verinice.model.iso27k.Threat;
-import sernet.verinice.model.iso27k.ThreatGroup;
-import sernet.verinice.model.iso27k.Vulnerability;
-import sernet.verinice.model.iso27k.VulnerabilityGroup;
-import sernet.verinice.service.commands.CreateElement;
 import sernet.verinice.service.commands.LoadElementByTypeId;
 import sernet.verinice.service.commands.LoadElementByUuid;
 import sernet.verinice.service.commands.LoadTreeItem;
 import sernet.verinice.service.commands.RemoveElement;
 import sernet.verinice.service.commands.SyncCommand;
 import sernet.verinice.service.commands.SyncParameter;
-import sernet.verinice.service.iso27k.LoadModel;
 
 /**
  * Tests executing several commands.
@@ -110,41 +72,15 @@ import sernet.verinice.service.iso27k.LoadModel;
  */
 @TransactionConfiguration(transactionManager="txManager", defaultRollback=false)
 @Transactional
-public class CommandServiceTest extends UuidLoader {  
+public class CommandServiceTest extends CommandServiceTestBase {  
     
     private static final Logger LOG = Logger.getLogger(CommandServiceTest.class);
     
-    public static final Map<String, Class> GROUP_TYPE_MAP;
-
     private static final int NUMBER_PER_GROUP = 20;  
     private static final int NUMBER_OF_IMPORTED_ELEMENTS = 29;   
     private static final String SOURCE_ID = "CommandServiceTest";
     private static final String VALUE_PREFIX = "****";
-    
-    static {
-        GROUP_TYPE_MAP = new HashMap<String, Class>();
-        GROUP_TYPE_MAP.put(AssetGroup.TYPE_ID, Asset.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(AuditGroup.TYPE_ID, Audit.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(ControlGroup.TYPE_ID, Control.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(DocumentGroup.TYPE_ID, Document.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(EvidenceGroup.TYPE_ID, Evidence.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(ExceptionGroup.TYPE_ID, sernet.verinice.model.iso27k.Exception.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(FindingGroup.TYPE_ID, Finding.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(IncidentGroup.TYPE_ID, Incident.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(IncidentScenarioGroup.TYPE_ID, IncidentScenario.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(InterviewGroup.TYPE_ID, Interview.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(PersonGroup.TYPE_ID, PersonIso.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(ProcessGroup.TYPE_ID, sernet.verinice.model.iso27k.Process.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(RecordGroup.TYPE_ID, Record.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(RequirementGroup.TYPE_ID, Requirement.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(ResponseGroup.TYPE_ID, Response.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(ThreatGroup.TYPE_ID, Threat.class); //$NON-NLS-1$
-        GROUP_TYPE_MAP.put(VulnerabilityGroup.TYPE_ID, Vulnerability.class); //$NON-NLS-1$
-    }
      
-    @Resource(name="commandService")
-    private ICommandService commandService; 
-    
     @Resource(name="huiTypeFactory")
     private HUITypeFactory huiTypeFactory;
     
@@ -219,8 +155,9 @@ public class CommandServiceTest extends UuidLoader {
         uuidList = new LinkedList<String>();
         
         Organization organization = createOrganization();
+        uuidList.add(organization.getUuid());
         checkOrganization(organization);       
-        createElementsInGroups(organization);
+        uuidList.addAll(createElementsInGroups(organization, NUMBER_PER_GROUP));
         
         LOG.debug("Total number of created elements: " + uuidList.size());
         
@@ -340,65 +277,7 @@ public class CommandServiceTest extends UuidLoader {
         }
         
     }
-
-    protected Organization createOrganization() throws CommandException {
-        LoadModel loadModel = new LoadModel();
-        loadModel = commandService.executeCommand(loadModel);
-        ISO27KModel model = loadModel.getModel();
-        
-        assertNotNull("ISO model is null.", model);
-        
-        CreateElement<Organization> saveCommand = new CreateElement<Organization>(model, Organization.class, getClass().getSimpleName());
-        saveCommand.setInheritAuditPermissions(true);
-        saveCommand = commandService.executeCommand(saveCommand);
-        Organization organization = saveCommand.getNewElement();     
-        uuidList.add(organization.getUuid());
-        
-        LOG.debug("Organisation created.");
-        
-        return organization;
-    }
     
-    protected void createElementsInGroups(Organization organization) throws CommandException {
-        Set<CnATreeElement> children = organization.getChildren();
-        for (CnATreeElement child : children) {
-            uuidList.add(child.getUuid());
-            assertTrue("Child of organization is not a group", child instanceof Group);
-            Group<CnATreeElement> group = (Group) child;
-            for (int i = 0; i < NUMBER_PER_GROUP; i++) {
-                CnATreeElement newElement = createNewElement(group,i);
-                uuidList.add(newElement.getUuid());
-                LOG.debug(newElement.getTypeId() + ": " + newElement.getTitle() + " created.");
-            }         
-        }
-    }
-    
-    protected CnATreeElement createNewElement(Group<CnATreeElement> group, int n) throws CommandException {
-        CreateElement<CnATreeElement> command = new CreateElement<CnATreeElement>(
-                group, 
-                GROUP_TYPE_MAP.get(group.getTypeId()), 
-                getClass().getSimpleName() + "_" + n);
-        command.setInheritAuditPermissions(true);
-        command = commandService.executeCommand(command);
-        CnATreeElement newElement = command.getNewElement();
-        checkElement(newElement);
-        
-        return newElement;
-    }
-    
-    protected void checkOrganization(Organization organization) {
-        checkElement(organization);
-        Set<CnATreeElement> children = organization.getChildren();
-        assertNotNull("Children of organization are null.", children);
-        assertEquals("Organization does not contain 14 groups.", 14, children.size());
-    }
-    protected void checkElement(CnATreeElement element) {
-        assertNotNull("Element is null.", element);
-        assertNotNull("Db-id of element is null.", element.getDbId());
-        assertNotNull("Scope-id of element is null.", element.getScopeId());
-    }
-
-
     /**
      * Vna import does not work in Junit-Test. After importing only the organization is 
      * saved in DB. Probably this is a spring-junit-transaction issue.
