3f15a27270bdfb24473bec3e88d2d96934fddd13
Benjamin Weissenfels <bw@sernet.de>
2014-07-28 16:46:25 +0000

    Add some test-cases for SyncCommand.
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@4156 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.server/testSrc/sernet/verinice/service/test/SyncInsertUpdateTest.java
+++ sernet.gs.server/testSrc/sernet/verinice/service/test/SyncInsertUpdateTest.java
@@ -22,25 +22,37 @@ package sernet.verinice.service.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import sernet.gs.service.RetrieveInfo;
+import sernet.gs.service.RuntimeCommandException;
 import sernet.verinice.interfaces.CommandException;
 import sernet.verinice.iso27k.service.Retriever;
 import sernet.verinice.model.bsi.Anwendung;
 import sernet.verinice.model.bsi.AnwendungenKategorie;
 import sernet.verinice.model.bsi.Client;
+import sernet.verinice.model.bsi.ITVerbund;
+import sernet.verinice.model.common.ChangeLogEntry;
 import sernet.verinice.model.common.CnALink;
 import sernet.verinice.model.common.CnATreeElement;
-import sernet.verinice.model.ds.Personengruppen;
-import sernet.verinice.model.iso27k.PersonGroup;
+import sernet.verinice.model.iso27k.ControlGroup;
+import sernet.verinice.model.iso27k.Organization;
+import sernet.verinice.service.commands.LoadElementByUuid;
 import sernet.verinice.service.commands.SyncCommand;
 import sernet.verinice.service.commands.SyncParameter;
 import sernet.verinice.service.commands.SyncParameterException;
+import sernet.verinice.service.commands.UpdateElement;
 import sernet.verinice.service.test.helper.vnaimport.BeforeEachVNAImportHelper;
 
 /**
@@ -49,117 +61,240 @@ import sernet.verinice.service.test.helper.vnaimport.BeforeEachVNAImportHelper;
  */
 public class SyncInsertUpdateTest extends BeforeEachVNAImportHelper {
 
+    private Logger log = Logger.getLogger(SyncInsertUpdateTest.class);
+
     private static final String VNA_FILE = "SyncInsertUpdateTest.vna";
 
     private static final String VNA_FILE_INSERT = "SyncInsertUpdateTestInsert.vna";
-    
+
     private static final String VNA_FILE_UPDATE = "SyncInsertUpdateTestUpdate.vna";
-    
+
     private static final String VNA_FILE_UPDATE_INSERT = "SyncInsertUpdateTestUpdateInsert.vna";
-    
+
     private static final String VNA_FILE_DELETE = "SyncInsertUpdateTestDelete.vna";
-    
+
+    private static final String VNA_FILE_RELATION = "SyncInsertUpdateTestRelation.vna";
+
+    private static final String VNA_FILE_INSERT_CLIENT = "SyncInsertUpdateTestInsertClient.vna";
+
     private static final String SOURCE_ID = "JUNIT SyncInsertUpdate";
-       
-    private static final String ANWENDUNGEN_KATEGORIE_EXT_ID = "ENTITY_676f8622-f7e3-4e9f-9247-2bd473b8e256";
-    
-    private static final String ANWENDUNG_1_EXT_ID = "ENTITY_112";
-    
-    private static final String ANWENDUNG_2_EXT_ID = "ENTITY_330";
-    
-    private static final String CLIENT_EXT_ID = "ENTITY_40214";
+
+    private static final String IT_VERBUND_EXT_ID = "ENTITY_41713";
+
+    private static final String ANWENDUNGEN_KATEGORIE_EXT_ID = "ENTITY_9146d4ce-26c5-4b03-8f34-44ee4d1deddd";
+
+    private static final String ANWENDUNG_1_EXT_ID = "ENTITY_41763";
+
+    private static final String ANWENDUNG_2_EXT_ID = "ENTITY_8650";
+
+    private static final String ANWENDUNG_INSERT_2_EXT_ID = "ENTITY_42156";
+
+    private static final String CLIENT_EXT_ID = "ENTITY_10072";
 
     @Test
     public void insertTest() throws IOException, CommandException, SyncParameterException {
 
         SyncParameter syncParameter = new SyncParameter(true, false, false, false, SyncParameter.EXPORT_FORMAT_VERINICE_ARCHIV);
-        SyncCommand syncCommand = importFile(getAbsoluteFilePath(VNA_FILE_INSERT), syncParameter);                
-        
-      
+        SyncCommand syncCommand = importFile(getAbsoluteFilePath(VNA_FILE_INSERT), syncParameter);
+
         Anwendung anwendung1 = (Anwendung) loadElement(SOURCE_ID, ANWENDUNG_1_EXT_ID);
-        Anwendung anwendung2 = (Anwendung) loadElement(SOURCE_ID, ANWENDUNG_2_EXT_ID);
-        
+        Anwendung anwendung2 = (Anwendung) loadElement(SOURCE_ID, ANWENDUNG_INSERT_2_EXT_ID);
+
         assertEquals("inserted Object anwendung must have the same parent id as the already existing", anwendung1.getParent().getDbId(), anwendung2.getParent().getDbId());
-        
-        assertEquals("updated flag is set to false", 0, syncCommand.getUpdated());
-//        assertEquals("only one object should have been inserted", 1, syncCommand.getInserted());
+
+        assertEquals("updated flag is set to false", 0, syncCommand.getPotentiallyUpdated());
+
     }
-    
+
     @Test
-    public void updateTest() throws IOException, CommandException, SyncParameterException {
-        
-        Anwendung anwendungBeforeImport = (Anwendung) loadElement(SOURCE_ID, ANWENDUNG_1_EXT_ID);
-        AnwendungenKategorie anwendungenKategorieBeforeImport = (AnwendungenKategorie) loadElement(SOURCE_ID, ANWENDUNGEN_KATEGORIE_EXT_ID);
-        
+    public void countInsertedAnwendungTest() throws SyncParameterException, IOException, CommandException {
+
+        SyncParameter syncParameter = new SyncParameter(true, false, false, false, SyncParameter.EXPORT_FORMAT_VERINICE_ARCHIV);
+        SyncCommand syncCommand = importFile(getAbsoluteFilePath(VNA_FILE_INSERT), syncParameter);
+
+        assertEquals("only one object should have been inserted", 6, syncCommand.getInserted());
+    }
+
+    @Test
+    public void countInsertedClientTest() throws SyncParameterException, IOException, CommandException {
+
+        SyncParameter syncParameter = new SyncParameter(true, false, false, false, SyncParameter.EXPORT_FORMAT_VERINICE_ARCHIV);
+        SyncCommand syncCommand = importFile(getAbsoluteFilePath(VNA_FILE_INSERT_CLIENT), syncParameter);
+
+        assertEquals("only one object should have been inserted", 1, syncCommand.getInserted());
+    }
+
+    @Test
+    public void countUpdatedZeroTest() throws SyncParameterException, IOException, CommandException {
+
+        SyncParameter syncParameter = new SyncParameter(true, false, false, false, SyncParameter.EXPORT_FORMAT_VERINICE_ARCHIV);
+        SyncCommand syncCommand = importFile(getAbsoluteFilePath(VNA_FILE_UPDATE), syncParameter);
+
+        assertEquals("updated flag is set to false", 0, syncCommand.getPotentiallyUpdated());
+    }
+
+    @Test
+    public void countPotentiallyUpdatedTest() throws SyncParameterException, IOException, CommandException {
+
+        final int OBJECTS_IN_VNA_ARCHIV = getAmountOfElements(loadElement(SOURCE_ID, IT_VERBUND_EXT_ID));
+
         SyncParameter syncParameter = new SyncParameter(false, true, false, false, SyncParameter.EXPORT_FORMAT_VERINICE_ARCHIV);
         SyncCommand syncCommand = importFile(getAbsoluteFilePath(VNA_FILE_UPDATE), syncParameter);
-        
-        Anwendung anwendungAfterImport = (Anwendung) loadElement(SOURCE_ID, ANWENDUNG_1_EXT_ID);               
-        
-        String anwendungsStatusBefore = anwendungBeforeImport.getEntity().getSimpleValue("anwendung_status");
-        String anwendungsStatusAfter = anwendungAfterImport.getEntity().getSimpleValue("anwendungs_status");        
-        assertFalse(anwendungsStatusBefore.equals(anwendungsStatusAfter));
-        
-        String anwendungsPersonenBezogenBefore = anwendungBeforeImport.getEntity().getSimpleValue("anwendung_persbez");
-        String anwendungsPersonenBezogenAfter = anwendungAfterImport.getEntity().getSimpleValue("anwendung_persbez");        
-        assertFalse(anwendungsPersonenBezogenBefore.equals(anwendungsPersonenBezogenAfter));
-        
-        assertEquals(anwendungBeforeImport.getKuerzel(), anwendungAfterImport.getKuerzel());
-        
-        assertEquals("AnwendungenKategorie must still have only 1 child", 1, anwendungenKategorieBeforeImport.getChildren().size());
-//        assertEquals("updated exactly one object", 1, syncCommand.getUpdated());
-        assertEquals("insert flag is set to false", 0, syncCommand.getInserted());
+
+        assertEquals("only " + " elements can be updated", OBJECTS_IN_VNA_ARCHIV, syncCommand.getPotentiallyUpdated());
     }
-    
+
     @Test
-    public void insertUpdateTest() throws IOException, CommandException, SyncParameterException{
-        
+    public void updateTest() throws IOException, CommandException, SyncParameterException {
+
         Anwendung anwendungBeforeImport = (Anwendung) loadElement(SOURCE_ID, ANWENDUNG_1_EXT_ID);
-        
+        AnwendungenKategorie anwendungenKategorieBeforeImport = (AnwendungenKategorie) loadElement(SOURCE_ID, ANWENDUNGEN_KATEGORIE_EXT_ID);
+
+        SyncParameter syncParameter = new SyncParameter(false, true, false, false, SyncParameter.EXPORT_FORMAT_VERINICE_ARCHIV);
+        importFile(getAbsoluteFilePath(VNA_FILE_UPDATE), syncParameter);
+
+        Anwendung anwendungAfterImport = (Anwendung) loadElement(SOURCE_ID, ANWENDUNG_1_EXT_ID);
+
+        String anwendungsStatusBefore = anwendungBeforeImport.getEntity().getSimpleValue("anwendung_status");
+        String anwendungsStatusAfter = anwendungAfterImport.getEntity().getSimpleValue("anwendungs_status");
+        assertFalse(anwendungsStatusBefore.equals(anwendungsStatusAfter));
+
+        String anwendungsPersonenBezogenBefore = anwendungBeforeImport.getEntity().getSimpleValue("anwendung_persbez");
+        String anwendungsPersonenBezogenAfter = anwendungAfterImport.getEntity().getSimpleValue("anwendung_persbez");
+        assertFalse(anwendungsPersonenBezogenBefore.equals(anwendungsPersonenBezogenAfter));
+
+        assertEquals(anwendungBeforeImport.getKuerzel(), anwendungAfterImport.getKuerzel());
+        assertEquals("AnwendungenKategorie must still have only 1 child", 1, anwendungenKategorieBeforeImport.getChildren().size());
+
+    }
+
+    @Test
+    public void insertUpdateTest() throws IOException, CommandException, SyncParameterException {
+
+        Anwendung anwendungBeforeImport = (Anwendung) loadElement(SOURCE_ID, ANWENDUNG_1_EXT_ID);
+
         SyncParameter syncParameter = new SyncParameter(true, true, false, false, SyncParameter.EXPORT_FORMAT_VERINICE_ARCHIV);
         importFile(getAbsoluteFilePath(VNA_FILE_UPDATE_INSERT), syncParameter);
-        
+
         Anwendung anwendung1 = (Anwendung) loadElement(SOURCE_ID, ANWENDUNG_1_EXT_ID);
         Anwendung anwendung2 = (Anwendung) loadElement(SOURCE_ID, ANWENDUNG_2_EXT_ID);
-        
-        
-        assertEquals("inserted Object anwendung must have the same parent id as the already existing", anwendung1.getParent().getDbId(), anwendung2.getParent().getDbId());        
-        
-        Client client = (Client) loadElement(SOURCE_ID, CLIENT_EXT_ID);
-        assertNotNull(client);
-                
+
+        validateImportExtId(anwendungBeforeImport);
+        validateImportExtId(anwendung1);
+        validateImportExtId(anwendung2);
+
+        assertEquals("inserted Object anwendung must have the same parent id as the already existing", anwendung1.getParent().getDbId(), anwendung2.getParent().getDbId());
+
         String anwendungsStatusBefore = anwendungBeforeImport.getEntity().getSimpleValue("anwendung_status");
-        String anwendungsStatusAfter = anwendung1.getEntity().getSimpleValue("anwendungs_status");        
-        assertFalse(anwendungsStatusBefore.equals(anwendungsStatusAfter));      
+        String anwendungsStatusAfter = anwendung2.getEntity().getSimpleValue("anwendungs_status");
+        assertFalse(anwendungsStatusBefore.equals(anwendungsStatusAfter));
     }
-    
+
     @Test
-    public void relationImported() throws SyncParameterException, IOException, CommandException{
-        
+    public void relationImported() throws SyncParameterException, IOException, CommandException {
+
         SyncParameter syncParameter = new SyncParameter(true, true, false, false, SyncParameter.EXPORT_FORMAT_VERINICE_ARCHIV);
-        importFile(getAbsoluteFilePath(VNA_FILE_UPDATE_INSERT), syncParameter);   
-        
+        importFile(getAbsoluteFilePath(VNA_FILE_RELATION), syncParameter);
+
         Anwendung anwendungWithLink = (Anwendung) loadElement(SOURCE_ID, ANWENDUNG_1_EXT_ID);
+        validateImportExtId(anwendungWithLink);
+
         Client clientWithLink = (Client) loadElement(SOURCE_ID, CLIENT_EXT_ID);
-        
-        anwendungWithLink = (Anwendung) Retriever.retrieveElement(anwendungWithLink, new RetrieveInfo().setLinksDown(true));  
+        validateImportExtId(clientWithLink);
+
+        anwendungWithLink = (Anwendung) Retriever.retrieveElement(anwendungWithLink, new RetrieveInfo().setLinksDown(true));
         Set<CnALink> links = anwendungWithLink.getLinksDown();
         Client client = (Client) links.iterator().next().getDependency();
-        
-        assertEquals(client.getDbId(), clientWithLink.getDbId());        
+
+        assertEquals(client.getDbId(), clientWithLink.getDbId());
     }
-    
-    @Test(expected=AssertionError.class)
+
+    @Test
     public void delete() throws SyncParameterException, IOException, CommandException {
-        
-        PersonGroup personengruppen = (PersonGroup) loadElement(SOURCE_ID, "ENTITY_44561");
-        
-        assertNotNull(personengruppen);
-        
+
+        Anwendung anwendung = null;
+        anwendung = (Anwendung) loadElement(SOURCE_ID, ANWENDUNG_1_EXT_ID);
+
+        validateImportExtId(anwendung);
+        assertNotNull(anwendung);
+
         SyncParameter syncParameter = new SyncParameter(false, false, true, false, SyncParameter.EXPORT_FORMAT_VERINICE_ARCHIV);
         importFile(getAbsoluteFilePath(VNA_FILE_DELETE), syncParameter);
-        
-        personengruppen = (PersonGroup) loadElement(SOURCE_ID, "ENTITY_44561");
+
+        try {
+            anwendung = (Anwendung) loadElement(SOURCE_ID, ANWENDUNG_1_EXT_ID);
+            fail("extected an assertion error, due the element should be deleted");
+        } catch (AssertionError ex) {
+            log.info("element " + (anwendung != null ? anwendung.getUuid() : "") + " is deleted");
+        }
+    }
+
+    @Test
+    public void integratedDelete() throws CommandException, SyncParameterException, IOException {
+
+        ITVerbund itVerbund = (ITVerbund) loadElement(SOURCE_ID, IT_VERBUND_EXT_ID);
+        Anwendung anwendung = (Anwendung) loadElement(SOURCE_ID, ANWENDUNG_1_EXT_ID);
+        deleteSourceId(itVerbund);
+
+        SyncParameter syncParameter = new SyncParameter(false, false, true, false, SyncParameter.EXPORT_FORMAT_VERINICE_ARCHIV);
+        importFile(getAbsoluteFilePath(VNA_FILE_DELETE), syncParameter);
+
+        LoadElementByUuid<Anwendung> loadElementByUuid = new LoadElementByUuid<Anwendung>(anwendung.getUuid());
+        commandService.executeCommand(loadElementByUuid);
+
+        assertTrue("Element " + anwendung.getUuid() + " may not deleted", loadElementByUuid.getElement() != null);
+    }
+
+    @Test
+    public void deleteEverythingWithSameSourceIdAndUnequalExtId() throws CommandException, SyncParameterException, IOException {
+        List<String> victims = new ArrayList<String>(0);
+        Organization organization = createOrganization();
+        victims.add(organization.getUuid());
+
+        victims.addAll(createInOrganisation(organization, GROUP_TYPE_MAP.get(ControlGroup.TYPE_ID), 2));
+        setSourceId(organization);
+
+        SyncParameter syncParameter = new SyncParameter(false, false, true);
+        importFile(getAbsoluteFilePath(VNA_FILE_DELETE), syncParameter);
+
+        for (String uuid : victims) {
+            LoadElementByUuid<CnATreeElement> loadElementByUuid = new LoadElementByUuid<CnATreeElement>(uuid);
+            commandService.executeCommand(loadElementByUuid);
+
+            assertTrue("element should have been deleted from database", loadElementByUuid.getElement() == null);
+        }
+
+    }
+
+    private void setSourceId(CnATreeElement organization) {
+        doBreadthFirstSearch(organization, new CallBack<CnATreeElement>() {
+            @Override
+            public void execute(CnATreeElement v) {
+                v.setSourceId(SOURCE_ID);
+                UpdateElement<CnATreeElement> updateElement = new UpdateElement<CnATreeElement>(v, true, ChangeLogEntry.STATION_ID);
+                try {
+                    SyncInsertUpdateTest.this.commandService.executeCommand(updateElement);
+                } catch (CommandException e) {
+                    throw new RuntimeCommandException("update element " + v.getUuid() + " failed");
+                }
+            }
+        });
+    }
+
+    private void deleteSourceId(ITVerbund itVerbund) {
+        doBreadthFirstSearch(itVerbund, new CallBack<CnATreeElement>() {
+            @Override
+            public void execute(CnATreeElement element) {
+                element.setSourceId(null);
+                try {
+                    log.info("delete source id from element " + element.getUuid());
+                    UpdateElement<CnATreeElement> updateElement = new UpdateElement<CnATreeElement>(element, true, ChangeLogEntry.STATION_ID);
+                    SyncInsertUpdateTest.this.commandService.executeCommand(updateElement);
+                } catch (CommandException e) {
+                    throw new RuntimeCommandException("update element " + element.getUuid() + " failed");
+                }
+            }
+        });
     }
 
     private String getAbsoluteFilePath(String path) {
@@ -176,4 +311,46 @@ public class SyncInsertUpdateTest extends BeforeEachVNAImportHelper {
         return new SyncParameter(true, true, true, false, SyncParameter.EXPORT_FORMAT_VERINICE_ARCHIV);
     }
 
+    private void validateImportExtId(CnATreeElement element) throws CommandException {
+        assertEquals(element.getScopeId(), loadElement(SOURCE_ID, IT_VERBUND_EXT_ID).getScopeId());
+    }
+
+    private int getAmountOfElements(CnATreeElement root) throws CommandException {
+
+        root = (ITVerbund) Retriever.retrieveElement(root, new RetrieveInfo().setChildren(true));
+
+        int i = 0;
+        Queue<CnATreeElement> queue = new LinkedList<CnATreeElement>();
+        queue.add(root);
+        CnATreeElement current;
+
+        while (!queue.isEmpty()) {
+            current = queue.poll();
+            current = (CnATreeElement) Retriever.retrieveElement(current, new RetrieveInfo().setChildren(true));
+            queue.addAll(current.getChildren());
+            i++;
+        }
+
+        return i;
+    }
+
+    private interface CallBack<V> {
+        public void execute(V v);
+    }
+
+    private void doBreadthFirstSearch(CnATreeElement root, CallBack<CnATreeElement> callback) {
+
+        root = Retriever.retrieveElement(root, new RetrieveInfo().setChildren(true));
+
+        Queue<CnATreeElement> queue = new LinkedList<CnATreeElement>();
+        queue.add(root);
+        CnATreeElement current;
+
+        while (!queue.isEmpty()) {
+            current = queue.poll();
+            current = (CnATreeElement) Retriever.retrieveElement(current, new RetrieveInfo().setChildren(true));
+            queue.addAll(current.getChildren());
+            callback.execute(current);
+        }
+    }
 }
