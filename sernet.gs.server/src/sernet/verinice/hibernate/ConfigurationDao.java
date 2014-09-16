1ebe6fe909b8e6bde46e6537c82c4c820d2cf7a8
Benjamin Weissenfels <bw@sernet.de>
2014-09-16 15:28:02 +0000

    Add a ConfigurationDao. This is needed, because we want to save new groups into an extra table, not only into the properties of the Configuration and so we have to hook into the save mechanism of the configuration object. The reason is the generic AccountDialog, which gives us no chance to catch the groups earlier. (VN-921)
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@4256 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

---
+++ sernet.gs.server/src/sernet/verinice/hibernate/ConfigurationDao.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Benjamin Weißenfels.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *     Benjamin Weißenfels <bw[at]sernet[dot]de> - initial API and implementation
+ ******************************************************************************/
+package sernet.verinice.hibernate;
+
+import static sernet.gs.ui.rcp.main.service.ServiceFactory.lookupAccountService;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
+
+import sernet.verinice.interfaces.IAccountService;
+import sernet.verinice.model.common.accountgroup.AccountGroup;
+import sernet.verinice.model.common.configuration.Configuration;
+
+/**
+ * @author Benjamin Weißenfels <bw[at]sernet[dot]de>
+ *
+ */
+public class ConfigurationDao extends TreeElementDao<Configuration, Serializable> {
+
+    private IAccountService accountService;
+
+    public ConfigurationDao() {
+        super(Configuration.class);
+    }
+
+    @Override
+    public Configuration merge(Configuration entity) {
+        saveAccountGroups(entity.getRoles());
+        return (Configuration) super.merge(entity);
+    }
+
+    @Override
+    public void saveOrUpdate(Configuration entity) {
+        saveAccountGroups(entity.getRoles());
+        super.saveOrUpdate(entity);
+    }
+
+    private void saveAccountGroups(Set<String> accountGroupNames) {
+
+        List<AccountGroup> accountGroups = getAccountService().listGroups();
+
+        for (AccountGroup aGroup : accountGroups) {
+            if (accountGroupNames.contains(aGroup.getName())) {
+                accountGroupNames.remove(aGroup.getName());
+            }
+        }
+
+        accountService.saveAccountGroups(accountGroupNames);
+    }
+
+    public IAccountService getAccountService() {
+        if (accountService == null) {
+            accountService = lookupAccountService();
+        }
+        return accountService;
+    }
+}
