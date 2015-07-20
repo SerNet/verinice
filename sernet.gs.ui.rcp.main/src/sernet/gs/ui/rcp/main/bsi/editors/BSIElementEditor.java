1e2b48eb55a3f33d435daf0d04e062b33ec2c667
Moritz Reiter <mr@sernet.de>
2015-07-20 08:33:48 +0000

    - Fix for a condition where a newly set custom icon is not showing (VN-1084)
    git-svn-id: svn+ssh://svn.verinice.org/data/subversion/svnroot/TRUNK@4952 4c0145ca-515c-4d1a-a6bc-f9bd594c3410

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/editors/BSIElementEditor.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/editors/BSIElementEditor.java
@@ -221,7 +221,7 @@ public class BSIElementEditor extends EditorPart {
         }
         try {
             // save element, refresh etc:
-            CnAElementHome.getInstance().updateEntity(cnAElement);
+            cnAElement = CnAElementHome.getInstance().updateEntity(cnAElement);           
             isModelModified = false;
             this.setPartName(cnAElement.getTitle());
             this.setTitleToolTip(cnAElement.getTitle());
