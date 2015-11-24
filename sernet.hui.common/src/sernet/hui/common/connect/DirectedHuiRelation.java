dd29c6f7edf38970c4fa2fddb958ed0996c5f18e
Moritz Reiter <mr@sernet.de>
2015-11-24 16:34:12 +0100

    Fix Data Provider for Relation Combo in Link Maker
    VN-1280
    
    Added a class for directed HUI relations and utility class for creating
    Links via the CreateLink command.
    
    Also modified and heavily refactored the LinkMaker class.

---
+++ sernet.hui.common/src/sernet/hui/common/connect/DirectedHuiRelation.java
@@ -0,0 +1,85 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Moritz Reiter.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *     Moritz Reiter <mr@sernet.de> - initial API and implementation
+ ******************************************************************************/
+
+package sernet.hui.common.connect;
+
+/**
+ * This is a wrapper class for a HuiRelation that adds a direction to the
+ * relation. It can be 'forward', meaning from 'source' to 'target', or
+ * 'backward', meaning from 'target' to 'source'.
+ * 
+ * It is important to understand, that this information is relative to the
+ * context. A HuiRelation is always a property of a HUI entity X. This implies,
+ * that then X is the source of the relation, and Y is the target. Looking at
+ * the relation from the perspective of X, means that the relation is forward,
+ * looking at it from the perspective of Y means that it is backward.
+ * 
+ * @author Moritz Reiter <mr@sernet.de>
+ */
+public final class DirectedHuiRelation {
+
+    private final HuiRelation huiRelation;
+    private final boolean isForward;
+
+    private DirectedHuiRelation(HuiRelation huiRelation, boolean isForward) {
+
+        this.huiRelation = huiRelation;
+        this.isForward = isForward;
+    }
+
+    /**
+     * A static factory method that returns an instance of a directed
+     * HuiRelation.
+     *
+     * @param huiRelation
+     *            the undirected HuiRelation
+     * @param isForward
+     *            true, if the HuiRelation is "forward" according to the context
+     *            of invocation
+     * @return the HuiRelation from the parameter, but with an added direction
+     */
+    public static DirectedHuiRelation getDirectedHuiRelation(HuiRelation huiRelation,
+            boolean isForward) {
+
+        return new DirectedHuiRelation(huiRelation, isForward);
+    }
+
+    /**
+     * @return the label, depending on the direction of the relation
+     */
+    public String getLabel() {
+
+        if (this.isForward()) {
+            return this.getHuiRelation().getName();
+        } else {
+            return this.getHuiRelation().getReversename();
+        }
+    }
+
+    public HuiRelation getHuiRelation() {
+
+        return this.huiRelation;
+    }
+
+    public boolean isForward() {
+
+        return this.isForward;
+    }
+}
