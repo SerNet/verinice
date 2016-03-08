0699fe3430ed4feecbd1e5b9c17c94023e9fe93a
Moritz Reiter <mr@sernet.de>
2016-03-08 10:44:08 +0100

    Use IsaDecoratorService in IsaDecoratorForAudit
    vn-1146
    
    Use the new IsaDecoratorService instead of the old
    ControlMaturityService in IsaDecoratorForAudit.

--- sernet.verinice.samt.rcp/src/sernet/verinice/samt/rcp/IsaDecoratorForAudit.java
+++ sernet.verinice.samt.rcp/src/sernet/verinice/samt/rcp/IsaDecoratorForAudit.java
@@ -1,3 +1,4 @@
+
 package sernet.verinice.samt.rcp;
 
 import org.apache.log4j.Logger;
@@ -5,58 +6,48 @@ import org.eclipse.jface.viewers.IDecoration;
 import org.eclipse.jface.viewers.ILightweightLabelDecorator;
 import org.eclipse.jface.viewers.LabelProvider;
 
-import sernet.gs.ui.rcp.main.service.ServiceFactory;
-import sernet.verinice.interfaces.CommandException;
-import sernet.verinice.iso27k.service.ControlMaturityService;
-import sernet.verinice.iso27k.service.ControlMaturityService.DecoratorColor;
+import sernet.verinice.iso27k.service.IsaDecoratorService;
+import sernet.verinice.iso27k.service.IsaDecoratorService.DecoratorColor;
 import sernet.verinice.iso27k.service.Retriever;
+import sernet.verinice.model.common.CnATreeElement;
 import sernet.verinice.model.iso27k.Audit;
-import sernet.verinice.model.iso27k.ControlGroup;
-import sernet.verinice.samt.service.TotalSecurityFigureISA2Command;
+
+import java.math.BigDecimal;
 
 @SuppressWarnings("restriction")
 public class IsaDecoratorForAudit extends LabelProvider implements ILightweightLabelDecorator {
 
-    private static final Logger LOG = Logger.getLogger(IsaDecoratorForControlGroup.class);
+    private static final Logger logger = Logger.getLogger(IsaDecoratorForAudit.class);
 
     @Override
     public void decorate(Object element, IDecoration decoration) {
-        boolean prefEnabled = Activator.getDefault().getPreferenceStore().getBoolean(SamtPreferencePage.ISA_RESULTS);
-        if (!prefEnabled || !(element instanceof Audit)) {
+
+        boolean preferenceEnabled = Activator.getDefault().getPreferenceStore()
+                .getBoolean(SamtPreferencePage.ISA_RESULTS);
+
+        if (!preferenceEnabled || !(element instanceof Audit)) {
             return;
         }
 
-        Audit audit = (Audit) Retriever.checkRetrieveChildren((Audit) element);
-        
-        ControlGroup controlGroup = (ControlGroup) audit.getGroup(ControlGroup.TYPE_ID);
-        controlGroup = (ControlGroup) Retriever.checkRetrieveChildren(controlGroup);
-             
-        boolean hasIControlChild = IsaDecoratorUtil.retrieveChildrenAndCheckForIControl(controlGroup);
-        if (hasIControlChild) {
-            DecoratorColor color = (new ControlMaturityService()).getDecoratorColor(controlGroup);
-            IsaDecoratorUtil.addOverlay(color, decoration);
-            
-            Double securityFigure = getSecurityFigure(audit);
-            addSuffix(securityFigure, decoration);
+        // This Activator has to be addressed with the fully qualified name because otherwise it
+        // would collide with the Activator used earlier
+        sernet.gs.ui.rcp.main.Activator.inheritVeriniceContextState();
 
-            LOG.debug("Security figure: " + securityFigure + ", audit uuid: " + audit.getUuid());
-        }
-    }
-    
-    private Double getSecurityFigure(Audit audit) {
-        TotalSecurityFigureISA2Command command = null;
-        try {
-            command = new TotalSecurityFigureISA2Command(audit.getDbId());
-            command = ServiceFactory.lookupCommandService().executeCommand(command);
-        } catch (CommandException e) {
-            LOG.error("Error computing security figure for Audit.", e);
-        }
-        return command.getTotalSecurityFigure();
+        Audit audit = (Audit) Retriever.checkRetrieveChildren((CnATreeElement) element);
+
+        DecoratorColor color = IsaDecoratorService.decoratorColor(audit);
+        IsaDecoratorService.addOverlay(color, decoration);
+
+        BigDecimal score = IsaDecoratorService.resultScore(audit);
+        addSuffix(score, decoration);
+
+        logger.debug("Score: " + score + ", audit uuid: " + audit.getUuid());
     }
 
-    private void addSuffix(double securityFigure, IDecoration decoration) {
+    private void addSuffix(BigDecimal score, IDecoration decoration) {
+
         StringBuilder sb = new StringBuilder();
-        sb.append(" [").append(String.format("%.2f", securityFigure)).append("]");
+        sb.append(" [").append(String.format("%.2f", score)).append("]");
         decoration.addSuffix(sb.toString());
     }
 }
