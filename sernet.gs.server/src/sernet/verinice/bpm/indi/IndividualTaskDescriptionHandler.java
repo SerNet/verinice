bd46545b86c846b19f3d41c5d03f09b6a040837b
Viktor Schmidt <vschmidt@ckc.de>
2016-08-18 12:22:17 +0200

    add decline description only for task execute
    req_62_1

--- sernet.gs.server/src/sernet/verinice/bpm/indi/IndividualTaskDescriptionHandler.java
+++ sernet.gs.server/src/sernet/verinice/bpm/indi/IndividualTaskDescriptionHandler.java
@@ -47,7 +47,7 @@ public class IndividualTaskDescriptionHandler implements ITaskDescriptionHandler
             description = (String) value;
         }
         if(getDescriptionKey()!=null) {
-            if(varMap.containsKey(getDeclineDescriptionVar())) {
+            if(varMap.containsKey(getDeclineDescriptionVar()) && taskId == IIndividualProcess.TASK_EXECUTE) {
                 return Messages.getString(getDeclineDescriptionKey(), description); 
             } else {
                 return Messages.getString(getDescriptionKey(), description); 
