f7a5d5415037c9f26f174530b8e20f21d0b9c4db
Daniel Murygin <dm@sernet.de>
2016-11-07 14:39:46 +0100

    Add method getCnaLinkPropertyMessage to LinkTableUtil
    VN-1476, VN-1503

--- sernet.gs.ui.rcp.main/src/sernet/verinice/rcp/linktable/LinkTableUtil.java
+++ sernet.gs.ui.rcp.main/src/sernet/verinice/rcp/linktable/LinkTableUtil.java
@@ -29,6 +29,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.preference.IPreferenceStore;
@@ -37,13 +38,13 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 
-import sernet.gs.service.StringUtil;
 import sernet.gs.ui.rcp.main.Activator;
 import sernet.gs.ui.rcp.main.preferences.PreferenceConstants;
 import sernet.verinice.rcp.linktable.ui.CsvExportDialog;
 import sernet.verinice.rcp.linktable.ui.LinkTableComposite;
 import sernet.verinice.rcp.linktable.ui.combo.LinkTableOperationType;
 import sernet.verinice.service.csv.ICsvExport;
+import sernet.verinice.service.linktable.CnaLinkPropertyConstants;
 import sernet.verinice.service.linktable.ColumnPathParseException;
 import sernet.verinice.service.linktable.ColumnPathParser;
 import sernet.verinice.service.linktable.vlt.VeriniceLinkTable;
@@ -64,6 +65,8 @@ public class LinkTableUtil {
     private static CsvExportDialog csvDialog;
     private static IObjectModelService loader;
 
+    private static final String ALIAS_DELIMITER = " AS ";
+    
     static {    
         if (vltExtensions == null) {
             vltExtensions = new HashMap<>();
@@ -319,11 +322,29 @@ public class LinkTableUtil {
         }
     }
     
-    public static IObjectModelService getLoader() {
-        if(loader==null) {
-            loader = createLoader();
+    public static String getCnaLinkPropertyMessage(String cnaLinkProperty) {
+        switch (cnaLinkProperty) {
+        case CnaLinkPropertyConstants.TYPE_TITLE:
+            return Messages.LinkTableColumn_CnaLink_Property_Title;
+        case CnaLinkPropertyConstants.TYPE_DESCRIPTION:
+            return Messages.LinkTableColumn_CnaLink_Property_Description;
+        case CnaLinkPropertyConstants.TYPE_RISK_VALUE_C:
+            return Messages.LinkTableColumn_CnaLink_Property_C;
+        case CnaLinkPropertyConstants.TYPE_RISK_VALUE_I:
+            return Messages.LinkTableColumn_CnaLink_Property_I;
+        case CnaLinkPropertyConstants.TYPE_RISK_VALUE_A:
+            return Messages.LinkTableColumn_CnaLink_Property_A;
+        case CnaLinkPropertyConstants.TYPE_RISK_VALUE_C_WITH_CONTROLS:
+            return Messages.LinkTableColumn_CnaLink_Property_C_With_Controls;
+        case CnaLinkPropertyConstants.TYPE_RISK_VALUE_I_WITH_CONTROLS:
+            return Messages.LinkTableColumn_CnaLink_Property_I_With_Controls;
+        case CnaLinkPropertyConstants.TYPE_RISK_VALUE_A_WITH_CONTROLS:
+            return Messages.LinkTableColumn_CnaLink_Property_A_With_Controls;
+        case CnaLinkPropertyConstants.TYPE_RISK_TREATMENT:
+            return Messages.LinkTableColumn_CnaLink_Property_Risk_Treatment;
+        default:
+            return Messages.LinkTableColumn_CnaLink_Property_Unknown;
         }
-        return loader;
     }
 
     /**
@@ -365,7 +386,15 @@ public class LinkTableUtil {
         message = message.replaceAll(" ", "_");
 
         return columnPath + ALIAS_DELIMITER + message;
+    }
     
+    public static IObjectModelService getLoader() {
+        if (loader == null) {
+            loader = createLoader();
+        }
+        return loader;
+    }
+
     private static IObjectModelService createLoader() {
         return loader = (IObjectModelService) HUIObjectModelLoader.getInstance();
     }
