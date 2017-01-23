883701c53a229f25b00612f19b2d9ed623337621
Viktor Schmidt <vschmidt@ckc.de>
2017-01-23 16:25:44 +0100

    Fix update childs and parent if mark template safequard
    Req_56

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/actions/MarkTemplateSafeguardActionDelegate.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/actions/MarkTemplateSafeguardActionDelegate.java
@@ -27,47 +27,53 @@ import sernet.gs.ui.rcp.main.ExceptionUtil;
 import sernet.gs.ui.rcp.main.common.model.CnAElementFactory;
 import sernet.gs.ui.rcp.main.common.model.CnAElementHome;
 import sernet.verinice.interfaces.CommandException;
-import sernet.verinice.model.bsi.BausteinUmsetzung;
 import sernet.verinice.model.bsi.MassnahmenUmsetzung;
 import sernet.verinice.model.common.CnATreeElement;
 
 public class MarkTemplateSafeguardActionDelegate extends MarkTemplateActionDelegate {
     private IWorkbenchPart targetPart;
 
+    @Override
     public void setActivePart(IAction action, IWorkbenchPart targetPart) {
         this.targetPart = targetPart;
     }
 
+    @Override
     public void run(IAction action) {
         try {
-            markSelectionsAsTemplateSafequardsAndUpdate();
+            markSelectionsAsTemplateSafeguardsAndUpdate();
             markParentAsTemplateModuleAndUpdate();
         } catch (Exception e) {
             ExceptionUtil.log(e, Messages.MarkTemplateSafeguardActionDelegate_0);
         }
     }
 
-    private void markSelectionsAsTemplateSafequardsAndUpdate() throws CommandException {
+    private void markSelectionsAsTemplateSafeguardsAndUpdate() throws CommandException {
         Object[] selections = ((IStructuredSelection) targetPart.getSite().getSelectionProvider().getSelection()).toArray();
         for (Object selection : selections) {
             if (selection instanceof MassnahmenUmsetzung) {
                 MassnahmenUmsetzung safeguard = (MassnahmenUmsetzung) selection;
                 safeguard.setTemplateTypeValue(CnATreeElement.TemplateType.TEMPLATE.name());
                 CnAElementHome.getInstance().update(safeguard);
+
+                // notify all listeners:
+                CnAElementFactory.getModel(safeguard.getParent()).childChanged(safeguard);
+                CnAElementFactory.getModel(safeguard.getParent()).databaseChildChanged(safeguard);
             }
         }
     }
-    
+
     private void markParentAsTemplateModuleAndUpdate() throws CommandException {
         Object firstElement = ((IStructuredSelection) targetPart.getSite().getSelectionProvider().getSelection()).getFirstElement();
         if (firstElement instanceof MassnahmenUmsetzung) {
-            BausteinUmsetzung module = (BausteinUmsetzung) ((MassnahmenUmsetzung) firstElement).getParent();
-            module.setTemplateTypeValue(CnATreeElement.TemplateType.TEMPLATE.name());
-            CnAElementHome.getInstance().update(module);
-
-            // notify all listeners:
-            CnAElementFactory.getModel(module.getParent()).childChanged(module);
-            CnAElementFactory.getModel(module.getParent()).databaseChildChanged(module);
+            CnATreeElement parent = ((MassnahmenUmsetzung) firstElement).getParent();
+            if (!parent.isImplementation()) {
+                parent.setTemplateTypeValue(CnATreeElement.TemplateType.TEMPLATE.name());
+                CnAElementHome.getInstance().update(parent);
+                // notify all listeners:
+                CnAElementFactory.getModel(parent.getParent()).childChanged(parent);
+                CnAElementFactory.getModel(parent.getParent()).databaseChildChanged(parent);
+            }
         }
     }
 }
