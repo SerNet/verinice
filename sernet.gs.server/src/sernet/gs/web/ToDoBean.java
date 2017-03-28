7b1c41e8dda935bfb876b35ca846ffa96cfaa3f2
Moritz Reiter <mr@sernet.de>
2017-03-28 15:06:01 +0200

    Convert to Unix line breaks

--- sernet.gs.server/src/sernet/gs/web/ToDoBean.java
+++ sernet.gs.server/src/sernet/gs/web/ToDoBean.java
@@ -1,35 +1,21 @@
 /*******************************************************************************
  * Copyright (c) 2009 Daniel Murygin <dm[at]sernet[dot]de>.
- * This program is free software: you can redistribute it and/or 
- * modify it under the terms of the GNU Lesser General Public License 
- * as published by the Free Software Foundation, either version 3 
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- *     This program is distributed in the hope that it will be useful,    
- * but WITHOUT ANY WARRANTY; without even the implied warranty 
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *     This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
- *     You should have received a copy of the GNU Lesser General Public 
- * License along with this program. 
+ *     You should have received a copy of the GNU Lesser General Public
+ * License along with this program.
  * If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Contributors:
  *     Daniel <dm[at]sernet[dot]de> - initial API and implementation
  ******************************************************************************/
 package sernet.gs.web;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.Set;
-import java.util.TimeZone;
-
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-import javax.faces.convert.Converter;
 
 import org.apache.log4j.Logger;
 
@@ -51,10 +37,24 @@ import sernet.verinice.model.common.configuration.Configuration;
 import sernet.verinice.service.commands.LoadCurrentUserConfiguration;
 import sernet.verinice.service.commands.SaveElement;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+import java.util.TimeZone;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
 /**
  * JSF managed bean for view ToDoList, template: todo/todo.xhtml
  * Asset-navigation data and methods are located in {@link AssetNavigationBean}.
- * 
+ *
  * @author Daniel Murygin <dm[at]sernet[dot]de>
  */
 @ManagedBean(name = "toDo")
