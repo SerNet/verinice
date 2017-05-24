4e66a51525d91810ec42c0119b3503a5f441c41c
Benjamin Weißenfels <bw@sernet.de>
2017-05-24 11:13:27 +0200

    Use jvm local instead of faces context.
    Seems like faces context does not prefer the locale from jvm, but the
    browser settings. Needs more investigation, but works right now.

--- sernet.gs.server/src/sernet/verinice/web/MessageBean.java
+++ sernet.gs.server/src/sernet/verinice/web/MessageBean.java
@@ -1,13 +1,13 @@
 package sernet.verinice.web;
 
-import sernet.gs.web.Util;
-
 import java.util.Locale;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+
+import sernet.gs.web.Util;
 
 /**
  * ManagedBean to show error and info messages.
@@ -18,32 +18,22 @@ import javax.faces.context.FacesContext;
 @SessionScoped
 public class MessageBean {
 
-    private String info;
-    
     private String error;
-    
+
     private Locale locale = null;
 
     @PostConstruct
-    public void init(){
+    public void init() {
+        locale = Locale.getDefault();
         FacesContext context = FacesContext.getCurrentInstance();
-        if (locale == null) {
-            locale = context.getViewRoot().getLocale();
-        } else {
-            context.getViewRoot().setLocale(locale);
-        }
+        context.getViewRoot().setLocale(locale);
     }
 
-    public void showInfo() {
-        Util.addInfo("massagePanel", getInfo()); //$NON-NLS-1$
-        setInfo(null);
-    }
-    
     /**
      * Returns an checked icon if the language match the active setting.
      */
-    public String getIcon(String language){
-        if(locale.getLanguage().equalsIgnoreCase(language)){
+    public String getIcon(String language) {
+        if (locale.getLanguage().equalsIgnoreCase(language)) {
             return "fa fa-check-circle-o";
         } else {
             return "fa fa-circle-o";
@@ -51,16 +41,8 @@ public class MessageBean {
     }
 
     public void showError() {
-        Util.addError("massagePanel", getError()); //$NON-NLS-1$
+        Util.addError("messagePanel", getError()); //$NON-NLS-1$
         setError(null);
-    }
-    
-    public String getInfo() {
-        return info;
-    }
-
-    public void setInfo(String message) {
-        this.info = message;
     }
 
     public String getError() {
@@ -70,21 +52,22 @@ public class MessageBean {
     public void setError(String error) {
         this.error = error;
     }
-    
+
     public void repeat() {
         Util.repeatMessage();
     }
-    
+
     public void english() {
         setLocale(Locale.ENGLISH);
     }
-    
+
     public void german() {
         setLocale(Locale.GERMAN);
     }
 
     public void setLocale(Locale locale) {
         this.locale = locale;
+        Locale.setDefault(locale);
         FacesContext context = FacesContext.getCurrentInstance();
         context.getViewRoot().setLocale(locale);
     }
