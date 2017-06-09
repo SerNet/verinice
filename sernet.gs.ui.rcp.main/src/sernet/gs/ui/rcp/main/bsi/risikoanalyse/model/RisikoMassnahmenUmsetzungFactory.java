03e5773e450a9a11b97ff18febf5b4864fa8f4b5
Sebastian Hagedorn <sh@sernet.de>
2017-06-09 14:52:59 +0200

    save description of RisikoMassnahme in RisikomassnahmenUmsetzung
    VN-1753
    
    saving the description of RisikoMassnahme in RisikoMassnahmenUmsetzung
    leads to the license-management decryption mechanism to be able to work
    with risikoMassnahmen as well and, as a consequence of this, display the
    description of the safeguard in the objectbrowser (as part of the
    ITBP-Riskanalysis-Wizard) again.

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/risikoanalyse/model/RisikoMassnahmenUmsetzungFactory.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/risikoanalyse/model/RisikoMassnahmenUmsetzungFactory.java
@@ -50,6 +50,7 @@ public class RisikoMassnahmenUmsetzungFactory {
 		umsetzung.setStand(draftMn.getStand());
 		umsetzung.setStufe(draftMn.getSiegelstufe());
 		umsetzung.setLebenszyklus(draftMn.getLZAsString(BSIKatalogInvisibleRoot.getInstance().getLanguage()));
+		umsetzung.setDescription(draftMn.getDescription());
 		
 		return umsetzung;
 	}
