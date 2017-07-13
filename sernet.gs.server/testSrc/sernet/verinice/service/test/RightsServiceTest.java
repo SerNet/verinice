7364e6f243370a0dccace5763066449627a10c78
Urs Zeidler <uzeidler@sernet.de>
2017-07-13 11:45:33 +0200

    Fix the junit test for the Rightservice.
    With the introduction of the module templates we also introduced two new
    rights 'marktemplate' and 'templates'. These rights need to be defined
    in the verinice-auth-default-test.xml for the tests to work properly.

--- sernet.gs.server/testSrc/sernet/verinice/service/test/RightsServiceTest.java
+++ sernet.gs.server/testSrc/sernet/verinice/service/test/RightsServiceTest.java
@@ -57,7 +57,9 @@ public class RightsServiceTest extends ContextConfiguration {
         ActionRightIDs.SHOWALLFILES,
         ActionRightIDs.TASKCHANGEASSIGNEE,
         ActionRightIDs.TASKCHANGEDUEDATE,
-        ActionRightIDs.TASKWITHRELEASEPROCESS
+        ActionRightIDs.TASKWITHRELEASEPROCESS,
+        ActionRightIDs.MARKTEMPLATE,
+        ActionRightIDs.TEMPLATES
     };
     
     public static final String[] newProfileActionIds = {
@@ -106,7 +108,7 @@ public class RightsServiceTest extends ContextConfiguration {
             }
         }
         rightsService.updateConfiguration(clone(conf));
-        rightsServerHandler.discardData();
+//        rightsServerHandler.discardData();
         assertFalse( "Action: " + NEW_ACTION_ID + " is enabled after removal.", rightsServerHandler.isEnabled(USER_NAME, NEW_ACTION_ID));       
     }
     
