a22edb919041c3bd9cf8d095f3f19d0907c6e1c1
Daniel Murygin <daniel.murygin@gmail.com>
2017-09-18 18:15:25 +0200

    Add new user profile action for adding new base protection groups
    VN-1815
    
    Add action id "addbpgroup" for adding new base protection groups.
    Add a new RCP handler/command for adding new base protection groups.

---
+++ sernet.gs.ui.rcp.main/src/sernet/verinice/iso27k/rcp/action/AddIsoGroupHandler.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Daniel Murygin <dm[at]sernet[dot]de>.
+ *
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public License 
+ * as published by the Free Software Foundation, either version 3 
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Daniel Murygin <dm[at]sernet[dot]de> - initial API and implementation
+ ******************************************************************************/
+package sernet.verinice.iso27k.rcp.action;
+
+import sernet.verinice.interfaces.ActionRightIDs;
+
+/**
+ * THis handler creates new groups for ISO2700 
+ * elements. 
+ * 
+ * @author Daniel Murygin <dm[at]sernet[dot]de>
+ */
+public class AddIsoGroupHandler extends sernet.verinice.rcp.AddGroupHandler {
+	
+
+    /* (non-Javadoc)
+     * @see sernet.verinice.interfaces.RightEnabledUserInteraction#getRightID()
+     */
+    @Override
+    public String getRightID() {
+        return ActionRightIDs.ADDISMGROUP;
+    }
+
+    /* (non-Javadoc)
+     * @see sernet.verinice.interfaces.RightEnabledUserInteraction#setRightID(java.lang.String)
+     */
+    @Override
+    public void setRightID(String rightID) {
+        // DO NOTHING
+    }
+}
