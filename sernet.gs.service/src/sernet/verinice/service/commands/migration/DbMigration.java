491c313967c1d9cdc65048a8c55c52bcb37b86e6
Alexander Ben Nasrallah <an@sernet.de>
2017-10-16 15:10:29 +0200

    Code refactoring
    VN-1829
    
    Moved shared code to already existing abstract super class.

--- sernet.gs.service/src/sernet/verinice/service/commands/migration/DbMigration.java
+++ sernet.gs.service/src/sernet/verinice/service/commands/migration/DbMigration.java
@@ -17,29 +17,66 @@
  ******************************************************************************/
 package sernet.verinice.service.commands.migration;
 
+import java.sql.SQLException;
+
 import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.engine.SessionFactoryImplementor;
+import org.springframework.orm.hibernate3.HibernateCallback;
 
 import sernet.gs.service.RuntimeCommandException;
 import sernet.verinice.interfaces.CommandException;
 import sernet.verinice.interfaces.GenericCommand;
+import sernet.verinice.model.bsi.Attachment;
 import sernet.verinice.model.bsi.BSIModel;
 import sernet.verinice.service.commands.LoadBSIModel;
 import sernet.verinice.service.commands.SaveElement;
 
+@SuppressWarnings("serial")
 public abstract class DbMigration extends GenericCommand {
-	public abstract double getVersion();
-	
-	protected void updateVersion() {
-		Logger.getLogger(this.getClass()).debug("Setting DB version to " + getVersion());
-		try {
-			LoadBSIModel command2 = new LoadBSIModel();
-			command2 = getCommandService().executeCommand(command2);
-			BSIModel model = command2.getModel();
-			model.setDbVersion(getVersion());
-			SaveElement<BSIModel> command4 = new SaveElement<BSIModel>(model);
-			getCommandService().executeCommand(command4);
-		} catch (CommandException e) {
-			throw new RuntimeCommandException(e);
-		}
-	}
+    public abstract double getVersion();
+
+    protected static final String HIBERNATE_DIALECT_POSTGRSQL = "org.hibernate.dialect.PostgreSQLDialect";
+    protected static final String HIBERNATE_DIALECT_ORACLE = "sernet.verinice.hibernate.Oracle10gNclobDialect";
+    protected static final String HIBERNATE_DIALECT_DERBY = "sernet.verinice.hibernate.ByteArrayDerbyDialect";
+
+    protected void updateVersion() {
+        Logger.getLogger(this.getClass()).debug("Setting DB version to " + getVersion());
+        try {
+            LoadBSIModel command2 = new LoadBSIModel();
+            command2 = getCommandService().executeCommand(command2);
+            BSIModel model = command2.getModel();
+            model.setDbVersion(getVersion());
+            SaveElement<BSIModel> command4 = new SaveElement<BSIModel>(model);
+            getCommandService().executeCommand(command4);
+        } catch (CommandException e) {
+            throw new RuntimeCommandException(e);
+        }
+    }
+
+    protected boolean isOracle() {
+        return HIBERNATE_DIALECT_ORACLE.equals(getHibernateDialect());
+    }
+
+    protected boolean isPostgres() {
+        return HIBERNATE_DIALECT_POSTGRSQL.equals(getHibernateDialect());
+    }
+
+    protected boolean isDerby() {
+        return HIBERNATE_DIALECT_DERBY.equals(getHibernateDialect());
+    }
+
+    /**
+     * reads in hibernate session configured database dialog
+     */
+    protected String getHibernateDialect() {
+        return (String) getDaoFactory().getDAO(Attachment.class).executeCallback(new HibernateCallback() {
+
+            @Override
+            public Object doInHibernate(Session session) throws HibernateException, SQLException {
+                return ((SessionFactoryImplementor) session.getSessionFactory()).getDialect().toString();
+            }
+        });
+    }
 }
