61dde79de101b7d650bd9a969bf394b579dcf699
Daniel Murygin <dm@sernet.de>
2017-11-09 10:39:32 +0100

    Solve problems with drag and drop of requirements
    VN-1794
    
    Create GroupNotFoundInScopeException.

--- sernet.gs.service/src/sernet/verinice/service/commands/bp/ModelThreatsCommand.java
+++ sernet.gs.service/src/sernet/verinice/service/commands/bp/ModelThreatsCommand.java
@@ -41,6 +41,7 @@ import sernet.verinice.interfaces.CommandException;
 import sernet.verinice.interfaces.IBaseDao;
 import sernet.verinice.model.bp.elements.BpThreat;
 import sernet.verinice.model.bp.groups.BpThreatGroup;
+import sernet.verinice.model.bp.groups.SafeguardGroup;
 import sernet.verinice.model.common.ChangeLogEntry;
 import sernet.verinice.model.common.CnATreeElement;
 import sernet.verinice.service.bp.exceptions.GroupNotFoundInScopeException;
