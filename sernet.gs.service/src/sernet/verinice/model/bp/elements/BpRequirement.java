b53cdfad71ebb5177eafd54d4497f173e8481279
Urs Zeidler <uz@sernet.de>
2017-11-16 16:00:32 +0100

    Some java doc and style
    VN-1893
    
    Adjust some style and add javadoc.

--- sernet.gs.service/src/sernet/verinice/model/bp/elements/BpRequirement.java
+++ sernet.gs.service/src/sernet/verinice/model/bp/elements/BpRequirement.java
@@ -75,10 +75,10 @@ public class BpRequirement extends CnATreeElement implements IBpElement {
 
         @Override
         public void determineValue(CascadingTransaction ta) throws TransactionAbortedException {
-            if(!isDeductiveImplementationEnabled(sbTarget)){
+            if (!isDeductiveImplementationEnabled(sbTarget)) {
                 return;
             }
-            
+
             for (CnALink cnALink : sbTarget.getLinksUp()) {
                 CnATreeElement dependant = cnALink.getDependant();
                 if (Safeguard.TYPE_ID.equals(dependant.getTypeId())) {
@@ -86,14 +86,14 @@ public class BpRequirement extends CnATreeElement implements IBpElement {
                     if (optionValue != null) {
                         optionValue = optionValue.replaceFirst(Safeguard.TYPE_ID,
                                 BpRequirement.TYPE_ID);
-                        sbTarget.setPropertyValue(getImplementationStatusId(sbTarget),
-                                optionValue);
+                        sbTarget.setPropertyValue(getImplementationStatusId(sbTarget), optionValue);
                     }
                 }
             }
         }
 
     };
+
     private final ISchutzbedarfProvider schutzbedarfProvider = new AssetValueAdapter(this);
     
     @Override
