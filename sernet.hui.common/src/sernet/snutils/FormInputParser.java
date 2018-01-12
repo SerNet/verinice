514ea248f30defb5de6e67a2a6385485fd81a5bf
Jochen Kemnade <jk@sernet.de>
2018-01-12 16:00:58 +0100

    adapt formatting to style guidelines - no functional change

--- sernet.hui.common/src/sernet/snutils/FormInputParser.java
+++ sernet.hui.common/src/sernet/snutils/FormInputParser.java
@@ -24,113 +24,104 @@ import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 /**
- * Class to parse different user text inputs like dates, curencies or
- * simple number formats.
+ * Class to parse different user text inputs like dates, curencies or simple
+ * number formats.
  * 
  * @author prack
  * @version $Id: FormInputParser.java,v 1.1 2005/12/27 16:41:18 aprack Exp $
  */
 public abstract class FormInputParser {
-    
-    public static final SimpleDateFormat DATE_FORMAT_DEFAULT = new SimpleDateFormat("EEE, dd.MM.yyyy"); //$NON-NLS-1$
-    
-	private static final boolean GROUPING = true;
-	private static NumberFormat numFmt;
-	private static NumberFormat priceFmt; 
-	
-	static {
-		numFmt = NumberFormat.getNumberInstance(Locale.GERMANY);
-		numFmt.setGroupingUsed(GROUPING);
-		priceFmt = NumberFormat.getCurrencyInstance(Locale.GERMANY);
-		priceFmt.setGroupingUsed(GROUPING);
-		DATE_FORMAT_DEFAULT.setLenient(true);
-	}
-	
-	public static float stringToCurrency(String fieldName, String s) throws DBException {
-		try {
-			priceFmt.setGroupingUsed(GROUPING);
-			return priceFmt.parse(s).floatValue();
-		}
-		catch (ParseException e) {
-			numFmt.setGroupingUsed(GROUPING);
-			try {
-				return numFmt.parse(s).floatValue();
-			}
-			catch (ParseException e1) {
-				throw new DBException("Falsches Format f�r " + fieldName + "."); //$NON-NLS-1$ //$NON-NLS-2$
-			}
-		}
-	}
-	
-	public static String currencyToString(float value) {
-		priceFmt.setGroupingUsed(GROUPING);
-		return priceFmt.format(value);
-	}
-	
-	public static float stringToFloat(String fieldName, String s) throws DBException {
-		try {
-			numFmt.setGroupingUsed(GROUPING);
-			return numFmt.parse(s).floatValue();
-		}
-		catch (ParseException e) {
-			throw new DBException("Falsches Format f�r " + fieldName + "."); //$NON-NLS-1$ //$NON-NLS-2$
-		}
-	}
-	
-	public static short stringToShort(String fieldName, String s) throws DBException {
-		try {
-			numFmt.setGroupingUsed(GROUPING);
-			return numFmt.parse(s).shortValue();
-		}
-		catch (ParseException e) {
-			throw new DBException("Falsches Format f�r " + fieldName + "."); //$NON-NLS-1$ //$NON-NLS-2$
-		}
-	}
-	
-	public static String dateToString(Date date) throws AssertException {
-		try {
-			if (date == null){
-				return ""; //$NON-NLS-1$
-			}
-			return DATE_FORMAT_DEFAULT.format(date);
-		}
-		catch (IllegalArgumentException e) {
-			throw new AssertException("Falsches / fehlendes Datum: " + date.toString()); //$NON-NLS-1$
-		}
-	}
-	
-	public static Date stringToDate(String string) throws AssertException {
-		try {
-			return new Date(DATE_FORMAT_DEFAULT.parse(string).getTime());
-		}
-		catch (IllegalArgumentException e) {
-			throw new AssertException("Falsches / fehlendes Datum: " + string); //$NON-NLS-1$
-		}
-		catch (ParseException e) {
-			SimpleDateFormat dateFormat2 = new SimpleDateFormat("dd.MM.yyyy"); //$NON-NLS-1$
-			dateFormat2.setLenient(true);
-			try {
-				return new Date(dateFormat2.parse(string).getTime());
-			}
-			catch (ParseException e1) {
-				SimpleDateFormat dateFormat3 = new SimpleDateFormat("yyyy-MM-dd"); //$NON-NLS-1$
-				dateFormat3.setLenient(true);
-				try {
-					return new Date(dateFormat3.parse(string).getTime());
-				}
-				catch (ParseException e2) {
-					throw new AssertException("Falsches / fehlendes Datum: " + string); //$NON-NLS-1$
-				}
-			}
-		}
-	}
 
-	/**
-	 * @param f
-	 * @return
-	 */
-	public static String floatToString(float f) {
-		return numFmt.format(f);
-	}
+    public static final SimpleDateFormat DATE_FORMAT_DEFAULT = new SimpleDateFormat("EEE, dd.MM.yyyy"); //$NON-NLS-1$
+
+    private static final boolean GROUPING = true;
+    private static NumberFormat numFmt;
+    private static NumberFormat priceFmt;
+
+    static {
+        numFmt = NumberFormat.getNumberInstance(Locale.GERMANY);
+        numFmt.setGroupingUsed(GROUPING);
+        priceFmt = NumberFormat.getCurrencyInstance(Locale.GERMANY);
+        priceFmt.setGroupingUsed(GROUPING);
+        DATE_FORMAT_DEFAULT.setLenient(true);
+    }
+
+    public static float stringToCurrency(String fieldName, String s) throws DBException {
+        try {
+            priceFmt.setGroupingUsed(GROUPING);
+            return priceFmt.parse(s).floatValue();
+        } catch (ParseException e) {
+            numFmt.setGroupingUsed(GROUPING);
+            try {
+                return numFmt.parse(s).floatValue();
+            } catch (ParseException e1) {
+                throw new DBException("Falsches Format f�r " + fieldName + "."); //$NON-NLS-1$ //$NON-NLS-2$
+            }
+        }
+    }
+
+    public static String currencyToString(float value) {
+        priceFmt.setGroupingUsed(GROUPING);
+        return priceFmt.format(value);
+    }
+
+    public static float stringToFloat(String fieldName, String s) throws DBException {
+        try {
+            numFmt.setGroupingUsed(GROUPING);
+            return numFmt.parse(s).floatValue();
+        } catch (ParseException e) {
+            throw new DBException("Falsches Format f�r " + fieldName + "."); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+    }
+
+    public static short stringToShort(String fieldName, String s) throws DBException {
+        try {
+            numFmt.setGroupingUsed(GROUPING);
+            return numFmt.parse(s).shortValue();
+        } catch (ParseException e) {
+            throw new DBException("Falsches Format f�r " + fieldName + "."); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+    }
+
+    public static String dateToString(Date date) throws AssertException {
+        try {
+            if (date == null) {
+                return ""; //$NON-NLS-1$
+            }
+            return DATE_FORMAT_DEFAULT.format(date);
+        } catch (IllegalArgumentException e) {
+            throw new AssertException("Falsches / fehlendes Datum: " + date.toString()); //$NON-NLS-1$
+        }
+    }
+
+    public static Date stringToDate(String string) throws AssertException {
+        try {
+            return new Date(DATE_FORMAT_DEFAULT.parse(string).getTime());
+        } catch (IllegalArgumentException e) {
+            throw new AssertException("Falsches / fehlendes Datum: " + string); //$NON-NLS-1$
+        } catch (ParseException e) {
+            SimpleDateFormat dateFormat2 = new SimpleDateFormat("dd.MM.yyyy"); //$NON-NLS-1$
+            dateFormat2.setLenient(true);
+            try {
+                return new Date(dateFormat2.parse(string).getTime());
+            } catch (ParseException e1) {
+                SimpleDateFormat dateFormat3 = new SimpleDateFormat("yyyy-MM-dd"); //$NON-NLS-1$
+                dateFormat3.setLenient(true);
+                try {
+                    return new Date(dateFormat3.parse(string).getTime());
+                } catch (ParseException e2) {
+                    throw new AssertException("Falsches / fehlendes Datum: " + string); //$NON-NLS-1$
+                }
+            }
+        }
+    }
+
+    /**
+     * @param f
+     * @return
+     */
+    public static String floatToString(float f) {
+        return numFmt.format(f);
+    }
 
 }
