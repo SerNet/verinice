db0dbf6c5dee5acf6bce3d5ca552f161faf4ec36
Daniel Murygin <daniel.murygin@gmail.com>
2018-01-25 10:56:04 +0100

    Do a refactoring of RightsEnabledAction
    VN-1977
    
    Format code.
    Improve comments.

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/actions/RightsEnabledAction.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/actions/RightsEnabledAction.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
- * Copyright (c) 2011 Sebastian Hagedorn <sh[at]sernet[dot]de>.
- * This program is free software: you can redistribute it and/or 
- * modify it under the terms of the GNU Lesser General Public License 
- * as published by the Free Software Foundation, either version 3 
+ * Copyright (c) 2011 Sebastian Hagedorn
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- *     This program is distributed in the hope that it will be useful,    
- * but WITHOUT ANY WARRANTY; without even the implied warranty 
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
- *     You should have received a copy of the GNU Lesser General Public 
- * License along with this program. 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.
  * If not, see <http://www.gnu.org/licenses/>.
  * 
  * Contributors:
- *     Sebastian Hagedorn <sh[at]sernet[dot]de> - initial API and implementation
+ * Sebastian Hagedorn - initial API and implementation
  ******************************************************************************/
 package sernet.gs.ui.rcp.main.actions;
 
@@ -28,112 +28,111 @@ import sernet.verinice.interfaces.IInternalServerStartListener;
 import sernet.verinice.interfaces.InternalServerEvent;
 import sernet.verinice.interfaces.RightEnabledUserInteraction;
 
+/**
+ * JFace action that can be configured with rights profile management.
+ * This action is enabled and executable if the rights ID returned by
+ * getRightID() is assigned to the user account.
+ * 
+ * In contrast to JFace actions, the method run () must not be overwritten in
+ * derived classes, but doRun ().
+ *
+ * @author Sebastian Hagedorn
+ * @author Daniel Murygin <dm{a}sernet{dot}de>
+ */
 public abstract class RightsEnabledAction extends Action implements RightEnabledUserInteraction {
-    
+
     private String rightID = null;
-    
-    /* TODO: implement logic for right management on orgs */
-    private boolean orgRelatedCheck = false;
-    
-    public RightsEnabledAction(String rightID){
+
+    public RightsEnabledAction(String rightID) {
         this.setRightID(rightID);
         setEnabledViaRightID();
     }
-    
-    public RightsEnabledAction(String rightID, String text, int style){
+
+    public RightsEnabledAction(String rightID, String text, int style) {
         super(text, style);
         this.setRightID(rightID);
         setEnabledViaRightID();
     }
-    
-    public RightsEnabledAction(){
-    }
-    
 
-    /* (non-Javadoc)
+    public RightsEnabledAction() {
+    }
+
+    /**
+     * Checks if rightID is assigned to the user account.
+     * If it is assigned doRun() is executed if not
+     * an error message is shown.
+     * 
      * @see org.eclipse.jface.action.Action#run()
      */
     @Override
     public void run() {
-        if(checkRights()) {
-            doRun();         
+        if (checkRights()) {
+            doRun();
         } else {
-            MessageDialog.openError(Display.getCurrent().getActiveShell(), Messages.RightsEnabledAction_0, Messages.RightsEnabledAction_1);
+            MessageDialog.openError(Display.getCurrent().getActiveShell(),
+                    Messages.RightsEnabledAction_0, Messages.RightsEnabledAction_1);
         }
-            
+
     }
-    
+
     /**
-     * @param action
+     * Overwrite to implement your action.
      */
     public abstract void doRun();
-   
 
     @Override
-    public boolean checkRights(){
-        /**
-         * no right management should be used
-         */
-        if(getRightID() == null){
-            return true; 
+    public boolean checkRights() {
+        if (getRightID() == null) {
+            // no right management should be used
+            return true;
         }
-        /**
-         * id  set but empty, right not granted, action disabled
-         */
-        else if(getRightID().equals("")){
+        else if (getRightID().equals("")) {
+            // id set but empty, right not granted, action disabled
             return false;
-        /**
-        * right management enabled, check rights and return true if right enabled / false if not
-        */
         } else {
+            // enabled / false if not
             Activator.inheritVeriniceContextState();
-            RightsServiceClient service = (RightsServiceClient)VeriniceContext.get(VeriniceContext.RIGHTS_SERVICE);
+            RightsServiceClient service = (RightsServiceClient) VeriniceContext
+                    .get(VeriniceContext.RIGHTS_SERVICE);
             return service.isEnabled(getRightID());
         }
     }
-    
+
     @Override
     public String getRightID() {
         return rightID;
     }
 
     /**
-     * Overwrite/call this, to enable right-management for the action implementing this class
+     * Overwrite/call this, to enable right-management for the action
+     * this class implements
+     * 
      * @param rightID
      */
     @Override
     public void setRightID(String rightID) {
         this.rightID = rightID;
     }
-    
-    /**
-     * is the action related to an organization or is it globally allowed / not allowed
-     * @return
-     */
-    public boolean isOrgRelated() {
-        return orgRelatedCheck;
-    }
 
-    public void setOrgRelated(boolean orgRelated) {
-        this.orgRelatedCheck = orgRelated;
-    }
-    
     private void setEnabledViaRightID() {
-        if(Activator.getDefault().isStandalone()  && !Activator.getDefault().getInternalServer().isRunning()){
-            IInternalServerStartListener listener = new IInternalServerStartListener(){
-                @Override
-                public void statusChanged(InternalServerEvent e) {
-                    if(e.isStarted()){
-                        setEnabled(checkRights());
-                    }
-                }
-
-            };
-            Activator.getDefault().getInternalServer().addInternalServerStatusListener(listener);
+        if (Activator.getDefault().isStandalone()
+                && !Activator.getDefault().getInternalServer().isRunning()) {
+            addInternalServerStartListener();
         } else {
             setEnabled(checkRights());
         }
     }
-    
-    
+
+    private void addInternalServerStartListener() {
+        IInternalServerStartListener listener = new IInternalServerStartListener() {
+            @Override
+            public void statusChanged(InternalServerEvent e) {
+                if (e.isStarted()) {
+                    setEnabled(checkRights());
+                }
+            }
+        };
+        Activator.getDefault().getInternalServer().addInternalServerStatusListener(listener);
+    }
+
 }
