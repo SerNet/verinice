b8656523c77bf91ced490db65bd6f7b71e6dfead
Jochen Kemnade <jk@sernet.de>
2018-02-02 14:28:25 +0100

    refactor and clean up some code in the ImportIndividualMassnahmen command

--- sernet.gs.service/src/sernet/verinice/service/commands/task/ImportIndividualMassnahmen.java
+++ sernet.gs.service/src/sernet/verinice/service/commands/task/ImportIndividualMassnahmen.java
@@ -22,6 +22,7 @@ package sernet.verinice.service.commands.task;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
@@ -29,6 +30,7 @@ import org.apache.log4j.Logger;
 import sernet.gs.model.Baustein;
 import sernet.gs.model.Massnahme;
 import sernet.gs.reveng.ModZobjBstMass;
+import sernet.gs.reveng.ModZobjBstMassId;
 import sernet.gs.reveng.importData.BausteineMassnahmenResult;
 import sernet.gs.reveng.importData.MassnahmeInformationTransfer;
 import sernet.gs.service.Retriever;
@@ -41,7 +43,7 @@ import sernet.verinice.service.parser.GSScraperUtil;
 
 /**
  * Command imports the controls tagged with >I< from the GSTOOL db, which do not
- * exists as children of the parent baustein in the itgs catalogue
+ * exist as children of the parent baustein in the itgs catalogue
  * 
  * @author Sebastian Hagedorn <sh[at]sernet[dot]de>
  *
@@ -76,9 +78,13 @@ public class ImportIndividualMassnahmen extends GenericCommand {
 
     @Override
     public void execute() {
-        for (BausteinUmsetzung bausteinUmsetzung : individualMassnahmenMap.keySet()) {
-            if (individualMassnahmenMap.get(bausteinUmsetzung).size() > 0) {
-                createIndividualMassnahmenForBausteinUmsetzung(bausteinUmsetzung);
+        for (Entry<BausteinUmsetzung, List<BausteineMassnahmenResult>> entry : individualMassnahmenMap
+                .entrySet()) {
+            BausteinUmsetzung bausteinUmsetzung = entry.getKey();
+            List<BausteineMassnahmenResult> individualMassnahmenForBaustein = entry.getValue();
+            if (!individualMassnahmenForBaustein.isEmpty()) {
+                createIndividualMassnahmenForBausteinUmsetzung(bausteinUmsetzung,
+                        individualMassnahmenForBaustein);
             }
         }
     }
@@ -94,7 +100,6 @@ public class ImportIndividualMassnahmen extends GenericCommand {
                 bausteineMassnahmenResult, bausteinUmsetzung);
         if (individualMassnahmenUmsetzung == null) {
             Massnahme m = findCatalogMassnahmeByURL(bausteineMassnahmenResult.massnahme.getLink());
-            MassnahmenFactory massnahmenFactory = new MassnahmenFactory();
             if (m == null) {
                 m = getIndividualMassnahmeFromGstoolDb(TransferData
                         .createBausteineMassnahmenResultIdentifier(bausteineMassnahmenResult), m);
@@ -103,6 +108,8 @@ public class ImportIndividualMassnahmen extends GenericCommand {
                 bausteinUmsetzung = (BausteinUmsetzung) Retriever
                         .checkRetrieveElementAndChildren(bausteinUmsetzung);
                 m.setId(getIndividualId(m.getId()));
+
+                MassnahmenFactory massnahmenFactory = new MassnahmenFactory();
 
                 individualMassnahmenUmsetzung = massnahmenFactory.createMassnahmenUmsetzung(
                         bausteinUmsetzung, m, GSScraperUtil.getInstance().getModel().getLanguage());
@@ -137,16 +144,14 @@ public class ImportIndividualMassnahmen extends GenericCommand {
         return sb.toString();
     }
 
-    private void createIndividualMassnahmenForBausteinUmsetzung(
-            BausteinUmsetzung bausteinUmsetzung) {
+    private void createIndividualMassnahmenForBausteinUmsetzung(BausteinUmsetzung bausteinUmsetzung,
+            List<BausteineMassnahmenResult> individualMassnahmenForBaustein) {
         if (LOG.isDebugEnabled()) {
-            LOG.debug("creating " + individualMassnahmenMap.get(bausteinUmsetzung).size()
+            LOG.debug("creating " + individualMassnahmenForBaustein.size()
                     + " individual massnahmen for bausteinUmsetzung:\t"
                     + bausteinUmsetzung.getTitle() + "<" + bausteinUmsetzung.getUuid() + ">");
         }
-        List<BausteineMassnahmenResult> bausteineMassnahmenResultList = individualMassnahmenMap
-                .get(bausteinUmsetzung);
-        for (BausteineMassnahmenResult bausteineMassnahmenResult : bausteineMassnahmenResultList) {
+        for (BausteineMassnahmenResult bausteineMassnahmenResult : individualMassnahmenForBaustein) {
             createIndividualMassnahmenForBausteinMassnahmenResult(bausteinUmsetzung,
                     bausteineMassnahmenResult);
         }
@@ -167,25 +172,28 @@ public class ImportIndividualMassnahmen extends GenericCommand {
 
     private MassnahmenUmsetzung getMassnahmeFromAlleMassnahmenMap(
             BausteineMassnahmenResult bausteineMassnahmenResult) {
-        ModZobjBstMass modZobjBstMass = bausteineMassnahmenResult.obm;
-        for (ModZobjBstMass key : alleMassnahmenMap.keySet()) {
-            if (key.getId().getZobId().equals(modZobjBstMass.getId().getZobId())) {
-                if (key.getId().getBauId().equals(modZobjBstMass.getId().getBauId())) {
-                    if (key.getId().getMasId().equals(modZobjBstMass.getId().getMasId())) {
-                        return alleMassnahmenMap.get(key);
-                    }
-                }
+        ModZobjBstMassId soughtId = bausteineMassnahmenResult.obm.getId();
+        for (Entry<ModZobjBstMass, MassnahmenUmsetzung> entry : alleMassnahmenMap.entrySet()) {
+            ModZobjBstMassId currentEntryId = entry.getKey().getId();
+            if (currentEntryId.getZobId().equals(soughtId.getZobId())
+                    && currentEntryId.getBauId().equals(soughtId.getBauId())
+                    && currentEntryId.getMasId().equals(soughtId.getMasId())) {
+                return entry.getValue();
             }
         }
         return null;
     }
 
     private Massnahme findCatalogMassnahmeByURL(String url) {
-        if (url.contains("\\")) {
-            url = url.substring(url.lastIndexOf("\\") + 1);
+
+        int lastIndexOfBackslash = url.lastIndexOf('\\');
+        if (lastIndexOfBackslash != -1) {
+            url = url.substring(lastIndexOfBackslash + 1);
         }
-        if (url.contains(".")) {
-            url = url.substring(0, url.lastIndexOf("."));
+        int lastIndexOfPeriod = url.lastIndexOf('.');
+
+        if (lastIndexOfPeriod != -1) {
+            url = url.substring(0, lastIndexOfPeriod);
         }
         for (Baustein b : allCatalogueBausteine) {
             for (Massnahme m : b.getMassnahmen()) {
