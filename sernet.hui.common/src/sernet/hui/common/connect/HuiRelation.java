bc7ac78556b49efc2157dae0b2c3ecb5ac7dac76
Jochen Kemnade <jk@sernet.de>
2018-05-04 08:39:10 +0200

    add toString, hashCode, and equals methods to relation types

--- sernet.hui.common/src/sernet/hui/common/connect/HuiRelation.java
+++ sernet.hui.common/src/sernet/hui/common/connect/HuiRelation.java
@@ -93,4 +93,35 @@ public class HuiRelation implements Serializable {
         this.from = sourceTypeId;
     }
 
+    @Override
+    public String toString() {
+        return "HuiRelation [id=" + id + ", to=" + to + ", name=" + name + ", reversename="
+                + reversename + ", from=" + from + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        HuiRelation other = (HuiRelation) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
 }
