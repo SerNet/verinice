225f6089394088e7a09030e5e5d31d54d1c4e5d8
Jochen Kemnade <jk@sernet.de>
2018-07-04 10:14:42 +0200

    code format - no functional change

--- sernet.gs.server/src/sernet/verinice/search/JsonBuilder.java
+++ sernet.gs.server/src/sernet/verinice/search/JsonBuilder.java
@@ -49,22 +49,22 @@ import sernet.verinice.model.iso27k.Organization;
  * @author Daniel Murygin <dm[at]sernet[dot]de>
  */
 public class JsonBuilder implements IJsonBuilder {
-    
-    private static final Logger LOG = Logger.getLogger(JsonBuilder.class);    
+
+    private static final Logger LOG = Logger.getLogger(JsonBuilder.class);
 
     private final ConcurrentSimpleDateFormatter simpleDateFormatter = new ConcurrentSimpleDateFormatter();
 
     private IElementTitleCache titleCache;
-    
+
     public String getJson(CnATreeElement element) {
-        if(!isIndexableElement(element)){
+        if (!isIndexableElement(element)) {
             return null;
         }
-        String title = null;    
-        if(getTitleCache()!=null && element.getScopeId()!=null) {
+        String title = null;
+        if (getTitleCache() != null && element.getScopeId() != null) {
             title = getScopeTitle(element);
         }
-        if(title==null && element.getScopeId()!=null) {
+        if (title == null && element.getScopeId() != null) {
             title = element.getScopeId().toString();
         }
         return getJson(element, title);
@@ -72,14 +72,16 @@ public class JsonBuilder implements IJsonBuilder {
 
     private String getScopeTitle(@NonNull CnATreeElement element) {
         String title = null;
-        if(element.isScope()) {
+        if (element.isScope()) {
             title = element.getTitle();
         } else {
             title = getTitleCache().get(element.getScopeId());
         }
-        if(title==null) {
-            LOG.warn("Scope title not found in cache for element: " + element.getUuid() + ", type: " + element.getTypeId() + ". Loading all scope titles now...");
-            getTitleCache().load(new String[] {ITVerbund.TYPE_ID_HIBERNATE, Organization.TYPE_ID, ItNetwork.TYPE_ID});
+        if (title == null) {
+            LOG.warn("Scope title not found in cache for element: " + element.getUuid() + ", type: "
+                    + element.getTypeId() + ". Loading all scope titles now...");
+            getTitleCache().load(new String[] { ITVerbund.TYPE_ID_HIBERNATE, Organization.TYPE_ID,
+                    ItNetwork.TYPE_ID });
             title = getTitleCache().get(element.getScopeId());
         }
         return title;
@@ -88,7 +90,7 @@ public class JsonBuilder implements IJsonBuilder {
     public final String getJson(CnATreeElement element, String scopeTitle) {
         try {
             String json = null;
-            if(isIndexableElement(element)){
+            if (isIndexableElement(element)) {
                 json = doGetJson(element, scopeTitle);
             }
             if (LOG.isDebugEnabled()) {
@@ -99,13 +101,13 @@ public class JsonBuilder implements IJsonBuilder {
             throw e;
         } catch (Exception e) {
             throw new RuntimeException(e);
-        }  
+        }
     }
 
     private String doGetJson(CnATreeElement element, String scopeTitle) throws IOException {
         EntityType entityType = element.getEntityType();
         XContentBuilder builder;
-        builder = XContentFactory.jsonBuilder().startObject();      
+        builder = XContentFactory.jsonBuilder().startObject();
         builder.field(ISearchService.ES_FIELD_UUID, element.getUuid());
         builder.field(ISearchService.ES_FIELD_DBID, element.getDbId());
         builder.field(ISearchService.ES_FIELD_TITLE, element.getTitle());
@@ -113,16 +115,18 @@ public class JsonBuilder implements IJsonBuilder {
         builder.field(ISearchService.ES_FIELD_EXT_ID, element.getExtId());
         builder.field(ISearchService.ES_FIELD_SOURCE_ID, element.getSourceId());
         builder.field(ISearchService.ES_FIELD_SCOPE_ID, element.getScopeId());
-        if(scopeTitle!=null) {
+        if (scopeTitle != null) {
             builder.field(ISearchService.ES_FIELD_SCOPE_TITLE, scopeTitle);
         }
         builder.field(ISearchService.ES_FIELD_PARENT_ID, element.getParentId());
         builder.field(ISearchService.ES_FIELD_ICON_PATH, element.getIconPath());
-       
+
         addPermissions(builder, element);
-        
-        if(element.getEntity()!=null && element.getEntityType()!=null && entityType.getAllPropertyTypeIds()!=null) {
-            builder = addProperties(builder, entityType.getAllPropertyTypeIds(), element.getEntity());
+
+        if (element.getEntity() != null && element.getEntityType() != null
+                && entityType.getAllPropertyTypeIds() != null) {
+            builder = addProperties(builder, entityType.getAllPropertyTypeIds(),
+                    element.getEntity());
         }
         return builder.endObject().string();
     }
@@ -130,89 +134,90 @@ public class JsonBuilder implements IJsonBuilder {
     /**
      * @param element
      * @return
-     * @throws IOException 
+     * @throws IOException
      */
-    private static void addPermissions(XContentBuilder builder, CnATreeElement element) throws IOException {
-        if(element.getPermissions()==null || element.getPermissions().isEmpty()) {
+    private static void addPermissions(XContentBuilder builder, CnATreeElement element)
+            throws IOException {
+        if (element.getPermissions() == null || element.getPermissions().isEmpty()) {
             return;
         }
         builder.startArray(ISearchService.ES_FIELD_PERMISSION_ROLES);
-        for (Permission p : element.getPermissions()) {          
+        for (Permission p : element.getPermissions()) {
             int value = 0;
-            if(p.isReadAllowed()) {
+            if (p.isReadAllowed()) {
                 value = 1;
             }
-            if(p.isWriteAllowed()) {
+            if (p.isWriteAllowed()) {
                 value = 2;
             }
-            if(value > 0) {
+            if (value > 0) {
                 builder.startObject();
-                builder.field( ISearchService.ES_FIELD_PERMISSION_NAME, p.getRole());
-                builder.field( ISearchService.ES_FIELD_PERMISSION_VALUE, value);
+                builder.field(ISearchService.ES_FIELD_PERMISSION_NAME, p.getRole());
+                builder.field(ISearchService.ES_FIELD_PERMISSION_VALUE, value);
                 builder.endObject();
             }
         }
         builder.endArray();
     }
 
-    private XContentBuilder addProperties(XContentBuilder builder, String[] propertyTypeIds, Entity e) throws IOException {
+    private XContentBuilder addProperties(XContentBuilder builder, String[] propertyTypeIds,
+            Entity e) throws IOException {
         HUITypeFactory factory = HUITypeFactory.getInstance();
-        for(String propertyTypeId : propertyTypeIds){
+        for (String propertyTypeId : propertyTypeIds) {
             PropertyType propertyType = factory.getPropertyType(e.getEntityType(), propertyTypeId);
             // reference types are ignored (VN-1204)
-            if(!propertyType.isReference()) {
+            if (!propertyType.isReference()) {
                 builder.field(propertyTypeId, mapPropertyString(e, propertyType));
-            }          
+            }
         }
         return builder;
     }
-    
-    
-    private String mapPropertyString(Entity e, PropertyType pType){
+
+    private String mapPropertyString(Entity e, PropertyType pType) {
         String value = e.getSimpleValue(pType.getId());
         String mappedValue = "";
-        if(StringUtils.isEmpty(value)){
+        if (StringUtils.isEmpty(value)) {
             mappedValue = getNullValue();
-        } else if(pType.isDate()){
+        } else if (pType.isDate()) {
             mappedValue = mapDateProperty(e.getValue(pType.getId()));
-        } else if(pType.isSingleSelect() || pType.isMultiselect()){
+        } else if (pType.isSingleSelect() || pType.isMultiselect()) {
             mappedValue = mapMultiSelectProperty(value, pType);
-        } else if(pType.isNumericSelect()){
+        } else if (pType.isNumericSelect()) {
             mappedValue = mapNumericSelectProperty(value, pType);
         } else {
             mappedValue = value;
         }
         return mappedValue;
     }
-    
-    private static String getNullValue(){
-        if(Locale.GERMAN.equals(Locale.getDefault()) || Locale.GERMANY.equals(Locale.getDefault())){
+
+    private static String getNullValue() {
+        if (Locale.GERMAN.equals(Locale.getDefault())
+                || Locale.GERMANY.equals(Locale.getDefault())) {
             return "unbearbeitet";
         } else {
             return "unedited";
         }
     }
-    
-    
-    
-    private static String mapNumericSelectProperty(String value, PropertyType type){
+
+    private static String mapNumericSelectProperty(String value, PropertyType type) {
         return type.getNameForValue(Integer.parseInt(value));
     }
-    
-    private static String mapMultiSelectProperty(String value, PropertyType type){
+
+    private static String mapMultiSelectProperty(String value, PropertyType type) {
         PropertyOption o = type.getOption(value);
-        if(value == null || type == null || o == null){
-            if(LOG.isDebugEnabled()){
-                LOG.debug("No mapping for:\t" + value + "\t on <" + type.getId() + "> found, returning value");
+        if (value == null || type == null || o == null) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("No mapping for:\t" + value + "\t on <" + type.getId()
+                        + "> found, returning value");
             }
             return value;
         }
-        if(LOG.isDebugEnabled()){
+        if (LOG.isDebugEnabled()) {
             LOG.debug("Mapping for:\t" + value + "\t on <" + type.getId() + ">:\t" + o.getName());
         }
         return type.getOption(value).getName();
     }
-    
+
     /**
      * Parses a date which is represented as milliseconds in string form.
      *
@@ -222,24 +227,24 @@ public class JsonBuilder implements IJsonBuilder {
      * @return the short form of the date {@link DateFormat#SHORT}, SHORT is
      *         completely numeric, such as 12.13.52 or 3:30pm
      */
-    private String mapDateProperty(String value){
-        if(StringUtils.isNotEmpty(value)){
-            try{
+    private String mapDateProperty(String value) {
+        if (StringUtils.isNotEmpty(value)) {
+            try {
                 return tryParseDate(value);
-            } catch (Exception e){
+            } catch (Exception e) {
                 LOG.error("Error on mapping date property: [" + value + "]", e);
             }
         }
         return value;
     }
-    
-    private String tryParseDate(String value){
+
+    private String tryParseDate(String value) {
         long timeStamp = Long.parseLong(value);
         return simpleDateFormatter.getFormatedDate(timeStamp);
     }
-    
-    private static boolean isIndexableElement(CnATreeElement element){
-        if(element instanceof ImportIsoGroup || element instanceof ImportBsiGroup){
+
+    private static boolean isIndexableElement(CnATreeElement element) {
+        if (element instanceof ImportIsoGroup || element instanceof ImportBsiGroup) {
             return false;
         } else {
             return true;
