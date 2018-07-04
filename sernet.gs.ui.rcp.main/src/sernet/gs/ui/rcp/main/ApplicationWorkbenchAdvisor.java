ff3fe38181f56aae1b346d55eacd0598cb994036
Jochen Kemnade <jk@sernet.de>
2018-07-04 16:43:05 +0200

    code format - no functional change

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/ApplicationWorkbenchAdvisor.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/ApplicationWorkbenchAdvisor.java
@@ -34,73 +34,73 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.ServiceReference;
 
 /**
- * This workbench advisor creates the window advisor, and specifies
- * the perspective id for the initial window.
+ * This workbench advisor creates the window advisor, and specifies the
+ * perspective id for the initial window.
  */
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
-	
-	private static final String PERSPECTIVE_ID = "sernet.gs.ui.rcp.main.perspective";
 
-    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+    private static final String PERSPECTIVE_ID = "sernet.gs.ui.rcp.main.perspective";
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+            IWorkbenchWindowConfigurer configurer) {
         return new ApplicationWorkbenchWindowAdvisor(configurer);
     }
 
-	public String getInitialWindowPerspectiveId() {
-		return PERSPECTIVE_ID;
-	} 
-	
-	@Override
-	public void initialize(IWorkbenchConfigurer configurer) {
-		configurer.setSaveAndRestore(true);
-		activateProxyService();
-	}
-	
-	/**
-	 * Activate the proxy service by obtaining it.
-	 * Copied from IDEWorkbenchAdvisor.
-	 */
-	private void activateProxyService() {
-		Bundle bundle = Platform.getBundle("org.eclipse.ui.ide"); //$NON-NLS-1$
-		Object proxyService = null;
-		if (bundle != null) {
-			ServiceReference ref = bundle.getBundleContext().getServiceReference(IProxyService.class.getName());
-			if (ref != null){
-				proxyService = bundle.getBundleContext().getService(ref);
-			}
-		}
-		if (proxyService == null) {
-			IDEWorkbenchPlugin.log("Proxy service could not be found."); //$NON-NLS-1$
-		}
-	}
-	
-	public void postStartup(){
-	    removeUnneededPrefPages();
-	}
-	
-	/**
-	 * removes prefPages that were loaded from plugins but not needed
-	 * currently thats:
-	 *     -   org.eclipse.datatools.connectivity.ui.preferences.dataNode
-	 *     -   org.eclipse.birt.report.designer.ui.preferences
-	 *     -   org.eclipse.birt.chart.ui.swt.fieldassist.preferences.FieldAssistPreferencePage
-	 */
-	private void removeUnneededPrefPages(){
-	    PreferenceManager pm = PlatformUI.getWorkbench().getPreferenceManager();
-	    // add id of prefPage to remove here
-	    String[] prefPageIDsToRemove = new String[]{
-	            "org.eclipse.datatools.connectivity.ui.preferences.dataNode",
-	            "org.eclipse.birt.report.designer.ui.preferences",
-	            "org.eclipse.birt.chart.ui.swt.fieldassist.preferences.FieldAssistPreferencePage"
-	    };
-	    Set<String> idSet = new HashSet<String>();
-	    for(String s : prefPageIDsToRemove){
-	        idSet.add(s);
-	    }
-	    for (IPreferenceNode node : pm.getRootSubNodes()){
-	        if(idSet.contains(node.getId())){
-	            // removing prefPages
-	            pm.remove(node);
-	        }
-	    }
-	}
+    public String getInitialWindowPerspectiveId() {
+        return PERSPECTIVE_ID;
+    }
+
+    @Override
+    public void initialize(IWorkbenchConfigurer configurer) {
+        configurer.setSaveAndRestore(true);
+        activateProxyService();
+    }
+
+    /**
+     * Activate the proxy service by obtaining it. Copied from
+     * IDEWorkbenchAdvisor.
+     */
+    private void activateProxyService() {
+        Bundle bundle = Platform.getBundle("org.eclipse.ui.ide"); //$NON-NLS-1$
+        Object proxyService = null;
+        if (bundle != null) {
+            ServiceReference ref = bundle.getBundleContext()
+                    .getServiceReference(IProxyService.class.getName());
+            if (ref != null) {
+                proxyService = bundle.getBundleContext().getService(ref);
+            }
+        }
+        if (proxyService == null) {
+            IDEWorkbenchPlugin.log("Proxy service could not be found."); //$NON-NLS-1$
+        }
+    }
+
+    public void postStartup() {
+        removeUnneededPrefPages();
+    }
+
+    /**
+     * removes prefPages that were loaded from plugins but not needed currently
+     * thats: - org.eclipse.datatools.connectivity.ui.preferences.dataNode -
+     * org.eclipse.birt.report.designer.ui.preferences -
+     * org.eclipse.birt.chart.ui.swt.fieldassist.preferences.FieldAssistPreferencePage
+     */
+    private void removeUnneededPrefPages() {
+        PreferenceManager pm = PlatformUI.getWorkbench().getPreferenceManager();
+        // add id of prefPage to remove here
+        String[] prefPageIDsToRemove = new String[] {
+                "org.eclipse.datatools.connectivity.ui.preferences.dataNode",
+                "org.eclipse.birt.report.designer.ui.preferences",
+                "org.eclipse.birt.chart.ui.swt.fieldassist.preferences.FieldAssistPreferencePage" };
+        Set<String> idSet = new HashSet<String>();
+        for (String s : prefPageIDsToRemove) {
+            idSet.add(s);
+        }
+        for (IPreferenceNode node : pm.getRootSubNodes()) {
+            if (idSet.contains(node.getId())) {
+                // removing prefPages
+                pm.remove(node);
+            }
+        }
+    }
 }
