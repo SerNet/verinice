72617bce0a6589976a85b0aa19595509f9abd00f
Jochen Kemnade <jk@sernet.de>
2018-07-10 09:00:17 +0200

    code format - no functional change

--- sernet.gs.service/src/sernet/verinice/service/commands/task/ImportIndividualMassnahmen.java
+++ sernet.gs.service/src/sernet/verinice/service/commands/task/ImportIndividualMassnahmen.java
@@ -40,15 +40,17 @@ import sernet.verinice.service.gstoolimport.TransferData;
 import sernet.verinice.service.parser.GSScraperUtil;
 
 /**
- * Command imports the controls tagged with >I< from the GSTOOL db, which do not exists as children of the parent baustein in the itgs catalogue
+ * Command imports the controls tagged with >I< from the GSTOOL db, which do not
+ * exists as children of the parent baustein in the itgs catalogue
+ * 
  * @author Sebastian Hagedorn <sh[at]sernet[dot]de>
  *
  */
 @SuppressWarnings("serial")
 public class ImportIndividualMassnahmen extends GenericCommand {
 
-    private static final Logger LOG =  Logger.getLogger(ImportIndividualMassnahmen.class);
-    
+    private static final Logger LOG = Logger.getLogger(ImportIndividualMassnahmen.class);
+
     private static final String INDIVIDUAL_CONTROL_IDENTIFIER = "iM ";
 
     private final Map<BausteinUmsetzung, List<BausteineMassnahmenResult>> individualMassnahmenMap;
@@ -57,21 +59,25 @@ public class ImportIndividualMassnahmen extends GenericCommand {
     private final Map<String, MassnahmeInformationTransfer> massnahmenInfos;
     private final Set<MassnahmenUmsetzung> changedElements;
 
-
-    public ImportIndividualMassnahmen(Map<BausteinUmsetzung, List<BausteineMassnahmenResult>> individualBausteinMassnahmenResultMap,
-            Map<ModZobjBstMass, MassnahmenUmsetzung> alleMassnahmenMap, List<Baustein> allCatalogueBausteine,
-            Map<String, MassnahmeInformationTransfer> massnahmenInfos){
-        this.individualMassnahmenMap = individualBausteinMassnahmenResultMap; // bausteinumsetzung, list<bausteinmassnahmeresult>
-        this.alleMassnahmenMap = alleMassnahmenMap; // modzobjbstmass, massnahmenumsetzung
+    public ImportIndividualMassnahmen(
+            Map<BausteinUmsetzung, List<BausteineMassnahmenResult>> individualBausteinMassnahmenResultMap,
+            Map<ModZobjBstMass, MassnahmenUmsetzung> alleMassnahmenMap,
+            List<Baustein> allCatalogueBausteine,
+            Map<String, MassnahmeInformationTransfer> massnahmenInfos) {
+        this.individualMassnahmenMap = individualBausteinMassnahmenResultMap; // bausteinumsetzung,
+                                                                              // list<bausteinmassnahmeresult>
+        this.alleMassnahmenMap = alleMassnahmenMap; // modzobjbstmass,
+                                                    // massnahmenumsetzung
         this.allCatalogueBausteine = allCatalogueBausteine; // list baustein
-        this.massnahmenInfos = massnahmenInfos; // bausteinmassnahmeresult, massnahmeinformationstransfer
+        this.massnahmenInfos = massnahmenInfos; // bausteinmassnahmeresult,
+                                                // massnahmeinformationstransfer
         this.changedElements = new HashSet<>(); // set<bausteinumsetzung>
     }
 
     @Override
     public void execute() {
-        for(BausteinUmsetzung bausteinUmsetzung : individualMassnahmenMap.keySet()){
-            if(individualMassnahmenMap.get(bausteinUmsetzung).size() > 0){
+        for (BausteinUmsetzung bausteinUmsetzung : individualMassnahmenMap.keySet()) {
+            if (individualMassnahmenMap.get(bausteinUmsetzung).size() > 0) {
                 createIndividualMassnahmenForBausteinUmsetzung(bausteinUmsetzung);
             }
         }
@@ -81,70 +87,91 @@ public class ImportIndividualMassnahmen extends GenericCommand {
      * @param bausteinUmsetzung
      * @param bausteineMassnahmenResult
      */
-    private void createIndividualMassnahmenForBausteinMassnahmenResult(BausteinUmsetzung bausteinUmsetzung, BausteineMassnahmenResult bausteineMassnahmenResult) {
-        MassnahmenUmsetzung individualMassnahmenUmsetzung = getIndividualMassnahmenUmsetzungFromAlreadyParsedElements(bausteineMassnahmenResult, bausteinUmsetzung);
-        if(individualMassnahmenUmsetzung == null){
+    private void createIndividualMassnahmenForBausteinMassnahmenResult(
+            BausteinUmsetzung bausteinUmsetzung,
+            BausteineMassnahmenResult bausteineMassnahmenResult) {
+        MassnahmenUmsetzung individualMassnahmenUmsetzung = getIndividualMassnahmenUmsetzungFromAlreadyParsedElements(
+                bausteineMassnahmenResult, bausteinUmsetzung);
+        if (individualMassnahmenUmsetzung == null) {
             Massnahme m = findCatalogMassnahmeByURL(bausteineMassnahmenResult.massnahme.getLink());
             MassnahmenFactory massnahmenFactory = new MassnahmenFactory();
-            if(m == null){
-                m = getIndividualMassnahmeFromGstoolDb(TransferData.createBausteineMassnahmenResultIdentifier(bausteineMassnahmenResult), m);
+            if (m == null) {
+                m = getIndividualMassnahmeFromGstoolDb(TransferData
+                        .createBausteineMassnahmenResultIdentifier(bausteineMassnahmenResult), m);
             }
-            if(m != null){
-                bausteinUmsetzung = (BausteinUmsetzung)Retriever.checkRetrieveElementAndChildren(bausteinUmsetzung);
+            if (m != null) {
+                bausteinUmsetzung = (BausteinUmsetzung) Retriever
+                        .checkRetrieveElementAndChildren(bausteinUmsetzung);
                 m.setId(getIndividualId(m.getId()));
-                
-                individualMassnahmenUmsetzung = massnahmenFactory.createMassnahmenUmsetzung(bausteinUmsetzung, m, GSScraperUtil.getInstance().getModel().getLanguage());
-                individualMassnahmenUmsetzung = massnahmenFactory.transferUmsetzungWithDate(individualMassnahmenUmsetzung, bausteineMassnahmenResult.umstxt.getName(), bausteineMassnahmenResult.obm.getUmsDatBis());
-                individualMassnahmenUmsetzung = massnahmenFactory.transferRevision(individualMassnahmenUmsetzung, bausteineMassnahmenResult.obm.getRevDat(), bausteineMassnahmenResult.obm.getRevDatNext(), bausteineMassnahmenResult.obm.getRevBeschr());
+
+                individualMassnahmenUmsetzung = massnahmenFactory.createMassnahmenUmsetzung(
+                        bausteinUmsetzung, m, GSScraperUtil.getInstance().getModel().getLanguage());
+                individualMassnahmenUmsetzung = massnahmenFactory.transferUmsetzungWithDate(
+                        individualMassnahmenUmsetzung, bausteineMassnahmenResult.umstxt.getName(),
+                        bausteineMassnahmenResult.obm.getUmsDatBis());
+                individualMassnahmenUmsetzung = massnahmenFactory.transferRevision(
+                        individualMassnahmenUmsetzung, bausteineMassnahmenResult.obm.getRevDat(),
+                        bausteineMassnahmenResult.obm.getRevDatNext(),
+                        bausteineMassnahmenResult.obm.getRevBeschr());
             } else {
-                if(LOG.isDebugEnabled()) {
-                    LOG.debug("Massnahme not found for massnahmenlink:\t" + bausteineMassnahmenResult.massnahme.getLink());
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Massnahme not found for massnahmenlink:\t"
+                            + bausteineMassnahmenResult.massnahme.getLink());
                 }
             }
         }
-        if(individualMassnahmenUmsetzung != null) {
+        if (individualMassnahmenUmsetzung != null) {
             changedElements.add(individualMassnahmenUmsetzung);
         }
     }
-    
+
     private static String getIndividualId(String id) {
         StringBuilder sb = new StringBuilder();
         sb.append(INDIVIDUAL_CONTROL_IDENTIFIER);
-        for(int i = 0; i < id.length(); i++) {
+        for (int i = 0; i < id.length(); i++) {
             char c = id.charAt(i);
-            if( c == '.' || Character.isDigit(c)){
+            if (c == '.' || Character.isDigit(c)) {
                 sb.append(c);
             }
         }
         return sb.toString();
     }
 
-    private void createIndividualMassnahmenForBausteinUmsetzung(BausteinUmsetzung bausteinUmsetzung) {
-        if(LOG.isDebugEnabled()){
-            LOG.debug("creating " + individualMassnahmenMap.get(bausteinUmsetzung).size() + " individual massnahmen for bausteinUmsetzung:\t" + bausteinUmsetzung.getTitle() + "<" + bausteinUmsetzung.getUuid() + ">" );
+    private void createIndividualMassnahmenForBausteinUmsetzung(
+            BausteinUmsetzung bausteinUmsetzung) {
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("creating " + individualMassnahmenMap.get(bausteinUmsetzung).size()
+                    + " individual massnahmen for bausteinUmsetzung:\t"
+                    + bausteinUmsetzung.getTitle() + "<" + bausteinUmsetzung.getUuid() + ">");
         }
-        List<BausteineMassnahmenResult> bausteineMassnahmenResultList = individualMassnahmenMap.get(bausteinUmsetzung);
-        for(BausteineMassnahmenResult bausteineMassnahmenResult : bausteineMassnahmenResultList){
-            createIndividualMassnahmenForBausteinMassnahmenResult(bausteinUmsetzung, bausteineMassnahmenResult);
+        List<BausteineMassnahmenResult> bausteineMassnahmenResultList = individualMassnahmenMap
+                .get(bausteinUmsetzung);
+        for (BausteineMassnahmenResult bausteineMassnahmenResult : bausteineMassnahmenResultList) {
+            createIndividualMassnahmenForBausteinMassnahmenResult(bausteinUmsetzung,
+                    bausteineMassnahmenResult);
         }
     }
 
-    private MassnahmenUmsetzung getIndividualMassnahmenUmsetzungFromAlreadyParsedElements(BausteineMassnahmenResult bausteinMassnahmenResult, BausteinUmsetzung parent){
-        MassnahmenUmsetzung existingSourceMassnahmenUmsetzung = getMassnahmeFromAlleMassnahmenMap(bausteinMassnahmenResult);
+    private MassnahmenUmsetzung getIndividualMassnahmenUmsetzungFromAlreadyParsedElements(
+            BausteineMassnahmenResult bausteinMassnahmenResult, BausteinUmsetzung parent) {
+        MassnahmenUmsetzung existingSourceMassnahmenUmsetzung = getMassnahmeFromAlleMassnahmenMap(
+                bausteinMassnahmenResult);
         MassnahmenUmsetzung individualMassnahmenUmsetzung = null;
-        if(existingSourceMassnahmenUmsetzung != null){
+        if (existingSourceMassnahmenUmsetzung != null) {
             individualMassnahmenUmsetzung = new MassnahmenUmsetzung(parent);
-            individualMassnahmenUmsetzung.getEntity().copyEntity(existingSourceMassnahmenUmsetzung.getEntity());
+            individualMassnahmenUmsetzung.getEntity()
+                    .copyEntity(existingSourceMassnahmenUmsetzung.getEntity());
         }
         return individualMassnahmenUmsetzung;
     }
 
-    private MassnahmenUmsetzung getMassnahmeFromAlleMassnahmenMap(BausteineMassnahmenResult bausteineMassnahmenResult){
+    private MassnahmenUmsetzung getMassnahmeFromAlleMassnahmenMap(
+            BausteineMassnahmenResult bausteineMassnahmenResult) {
         ModZobjBstMass modZobjBstMass = bausteineMassnahmenResult.obm;
-        for(ModZobjBstMass key : alleMassnahmenMap.keySet()){
-            if(key.getId().getZobId().equals(modZobjBstMass.getId().getZobId())){
-                if(key.getId().getBauId().equals(modZobjBstMass.getId().getBauId())){
-                    if(key.getId().getMasId().equals(modZobjBstMass.getId().getMasId())){
+        for (ModZobjBstMass key : alleMassnahmenMap.keySet()) {
+            if (key.getId().getZobId().equals(modZobjBstMass.getId().getZobId())) {
+                if (key.getId().getBauId().equals(modZobjBstMass.getId().getBauId())) {
+                    if (key.getId().getMasId().equals(modZobjBstMass.getId().getMasId())) {
                         return alleMassnahmenMap.get(key);
                     }
                 }
@@ -153,16 +180,16 @@ public class ImportIndividualMassnahmen extends GenericCommand {
         return null;
     }
 
-    private Massnahme findCatalogMassnahmeByURL(String url){
-        if(url.contains("\\")){
-            url = url.substring(url.lastIndexOf("\\")+1);
+    private Massnahme findCatalogMassnahmeByURL(String url) {
+        if (url.contains("\\")) {
+            url = url.substring(url.lastIndexOf("\\") + 1);
         }
-        if(url.contains(".")){
+        if (url.contains(".")) {
             url = url.substring(0, url.lastIndexOf("."));
         }
-        for(Baustein b : allCatalogueBausteine){
-            for(Massnahme m : b.getMassnahmen()){
-                if(url.equals(m.getUrl())){
+        for (Baustein b : allCatalogueBausteine) {
+            for (Massnahme m : b.getMassnahmen()) {
+                if (url.equals(m.getUrl())) {
                     return m;
                 }
             }
@@ -175,22 +202,28 @@ public class ImportIndividualMassnahmen extends GenericCommand {
      * @param m
      * @return
      */
-    private Massnahme getIndividualMassnahmeFromGstoolDb(String bausteineMassnahmenResultIdentifier, Massnahme m) {
-        if(massnahmenInfos.containsKey(bausteineMassnahmenResultIdentifier)){
-            MassnahmeInformationTransfer massnahmeInformationTransfer = massnahmenInfos.get(bausteineMassnahmenResultIdentifier);
-            if(!massnahmeInformationTransfer.getTitel().isEmpty()){
+    private Massnahme getIndividualMassnahmeFromGstoolDb(String bausteineMassnahmenResultIdentifier,
+            Massnahme m) {
+        if (massnahmenInfos.containsKey(bausteineMassnahmenResultIdentifier)) {
+            MassnahmeInformationTransfer massnahmeInformationTransfer = massnahmenInfos
+                    .get(bausteineMassnahmenResultIdentifier);
+            if (!massnahmeInformationTransfer.getTitel().isEmpty()) {
                 m = new Massnahme();
                 m.setId(massnahmeInformationTransfer.getId());
-                m.setTitel((massnahmeInformationTransfer.getTitel() != null) ? massnahmeInformationTransfer.getTitel() : "no name available");
-                m.setLebenszyklus((massnahmeInformationTransfer.getZyklus() != null) ? Integer.valueOf(massnahmeInformationTransfer.getZyklus()) : -1);
+                m.setTitel((massnahmeInformationTransfer.getTitel() != null)
+                        ? massnahmeInformationTransfer.getTitel()
+                        : "no name available");
+                m.setLebenszyklus((massnahmeInformationTransfer.getZyklus() != null)
+                        ? Integer.valueOf(massnahmeInformationTransfer.getZyklus())
+                        : -1);
             }
         }
 
         return m;
     }
 
-    public Set<MassnahmenUmsetzung> getChangedElements(){
-        if(LOG.isDebugEnabled()){
+    public Set<MassnahmenUmsetzung> getChangedElements() {
+        if (LOG.isDebugEnabled()) {
             LOG.debug("Added " + changedElements.size() + " individual controls ");
         }
         return changedElements;
--- sernet.gs.service/src/sernet/verinice/service/commands/task/ImportNotesForZielobjekt.java
+++ sernet.gs.service/src/sernet/verinice/service/commands/task/ImportNotesForZielobjekt.java
@@ -47,38 +47,38 @@ import sernet.verinice.service.gstoolimport.TransferData;
 
 /**
  * @author koderman@sernet.de
- * @version $Rev$ $LastChangedDate$ 
- * $LastChangedBy$
+ * @version $Rev$ $LastChangedDate$ $LastChangedBy$
  *
  */
 public class ImportNotesForZielobjekt extends GenericCommand {
-    
-    private static final Logger LOG = Logger.getLogger(ImportNotesForZielobjekt.class);
-    
-    private static final String QUERY = "from CnATreeElement elmt " +
-    "where elmt.objectType != 'massnahmen-umsetzung'" +
-    "and elmt.objectType != 'baustein-umsetzung' "; 
 
-	private String zielobjektName;
+    private static final Logger LOG = Logger.getLogger(ImportNotesForZielobjekt.class);
+
+    private static final String QUERY = "from CnATreeElement elmt "
+            + "where elmt.objectType != 'massnahmen-umsetzung'"
+            + "and elmt.objectType != 'baustein-umsetzung' ";
+
+    private String zielobjektName;
     private Map<MbBaust, List<NotizenMassnahmeResult>> notizenMap;
 
     /**
      * @param name
      * @param notizen
      */
-        public ImportNotesForZielobjekt(String name, Map<MbBaust, List<NotizenMassnahmeResult>> notizenMap) {
+
+    public ImportNotesForZielobjekt(String name,
+            Map<MbBaust, List<NotizenMassnahmeResult>> notizenMap) {
         this.zielobjektName = name;
         this.notizenMap = notizenMap;
     }
 
-
     public void execute() {
         IBaseDao<BSIModel, Serializable> dao = getDaoFactory().getDAO(BSIModel.class);
-        List<CnATreeElement> allElements = dao.findByQuery(QUERY,  new Object[] {});
-	  
-	    for (CnATreeElement cnATreeElement : allElements) {
+        List<CnATreeElement> allElements = dao.findByQuery(QUERY, new Object[] {});
+
+        for (CnATreeElement cnATreeElement : allElements) {
             if (cnATreeElement.getTitle().equals(zielobjektName)) {
-                
+
                 Set<Entry<MbBaust, List<NotizenMassnahmeResult>>> entrySet = notizenMap.entrySet();
                 for (Entry<MbBaust, List<NotizenMassnahmeResult>> entry : entrySet) {
                     MbBaust baust = entry.getKey();
@@ -86,10 +86,11 @@ public class ImportNotesForZielobjekt extends GenericCommand {
                     BausteinUmsetzung bstUms = findBausteinUmsetzung(cnATreeElement, baust);
                     if (bstUms != null) {
                         try {
-                            List<NotizenMassnahmeResult> remainingNotes = addNotes(bstUms, massnahmenNotizen);
-                            if (remainingNotes != null && remainingNotes.size()>0) {
+                            List<NotizenMassnahmeResult> remainingNotes = addNotes(bstUms,
+                                    massnahmenNotizen);
+                            if (remainingNotes != null && remainingNotes.size() > 0) {
                                 addNotes(cnATreeElement, remainingNotes);
-                            } 
+                            }
                         } catch (CommandException e) {
                             throw new RuntimeCommandException(e);
                         }
@@ -97,107 +98,111 @@ public class ImportNotesForZielobjekt extends GenericCommand {
                 }
             }
         }
-	}
-
+    }
 
     /**
      * Add remaining notes to target object.
      * 
      * @param cnATreeElement
      * @param remainingNotes
-     * @throws CommandException 
+     * @throws CommandException
      */
-    private void addNotes(CnATreeElement cnATreeElement, List<NotizenMassnahmeResult> remainingNotes) throws CommandException {
+    private void addNotes(CnATreeElement cnATreeElement,
+            List<NotizenMassnahmeResult> remainingNotes) throws CommandException {
         for (NotizenMassnahmeResult notiz : remainingNotes) {
             LOG.debug("Adding note for " + cnATreeElement.getTitle());
-            saveNewNote(cnATreeElement.getDbId(), cnATreeElement.getTitle(), cnATreeElement.getTitle(), notiz.notiz.getNotizText());
+            saveNewNote(cnATreeElement.getDbId(), cnATreeElement.getTitle(),
+                    cnATreeElement.getTitle(), notiz.notiz.getNotizText());
             appendDescription(cnATreeElement, notiz.notiz.getNotizText());
         }
     }
 
     @SuppressWarnings("restriction")
     public void appendDescription(CnATreeElement element, String description) {
-           // do not save empty text:
-           if (description == null || description.length()==0 ){
-               return;
-           }
-            Pattern pattern = Pattern.compile("^\\s+$");
-            Matcher matcher = pattern.matcher(description);
-            if (matcher.matches()){
-                return;
-            }
-            String convertedText;
-            try {
-                convertedText = TransferData.convertRtf(description);
-            } catch (Exception e) {
-                convertedText = "!Konvertierungsfehler, Originaltext: " + description;
-                LOG.debug(e);
-            }
-            
-           
-           String typeId = element.getTypeId();
-           String descriptionPropId = CnATypeMapper.getDescriptionPropertyForType(typeId);
+        // do not save empty text:
+        if (description == null || description.length() == 0) {
+            return;
+        }
+        Pattern pattern = Pattern.compile("^\\s+$");
+        Matcher matcher = pattern.matcher(description);
+        if (matcher.matches()) {
+            return;
+        }
+        String convertedText;
+        try {
+            convertedText = TransferData.convertRtf(description);
+        } catch (Exception e) {
+            convertedText = "!Konvertierungsfehler, Originaltext: " + description;
+            LOG.debug(e);
+        }
 
-           if (descriptionPropId == null){
-               return;
-           }
-           PropertyList properties = element.getEntity().getProperties(descriptionPropId);
-           if (properties == null || properties.getProperties().size()==0){
-               return;
-           }
-           Property property = properties.getProperty(0);
-           
-           StringBuilder sb = new StringBuilder();
-           sb.append(property.getPropertyValue());
-           // FIXME externalize strings
-           sb.append("\n\n***Neue Notiz *** ***\n");
-           sb.append(convertedText);
-           property.setPropertyValue(sb.toString());
-   }
+        String typeId = element.getTypeId();
+        String descriptionPropId = CnATypeMapper.getDescriptionPropertyForType(typeId);
+
+        if (descriptionPropId == null) {
+            return;
+        }
+        PropertyList properties = element.getEntity().getProperties(descriptionPropId);
+        if (properties == null || properties.getProperties().size() == 0) {
+            return;
+        }
+        Property property = properties.getProperty(0);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(property.getPropertyValue());
+        // FIXME externalize strings
+        sb.append("\n\n***Neue Notiz *** ***\n");
+        sb.append(convertedText);
+        property.setPropertyValue(sb.toString());
+    }
 
     /**
-     * Add notes to massnahmen of this bausteinumsetznug and to the bstumsetzung itself.
+     * Add notes to massnahmen of this bausteinumsetznug and to the bstumsetzung
+     * itself.
      * 
      * @param bstUms
      * @param massnahmenNotizen
      * @return list of all notes that could not be applied
-     * @throws CommandException 
+     * @throws CommandException
      */
-    private List<NotizenMassnahmeResult> addNotes(BausteinUmsetzung bstUms, List<NotizenMassnahmeResult> massnahmenNotizen) throws CommandException {
+    private List<NotizenMassnahmeResult> addNotes(BausteinUmsetzung bstUms,
+            List<NotizenMassnahmeResult> massnahmenNotizen) throws CommandException {
         List<NotizenMassnahmeResult> copy = new ArrayList<NotizenMassnahmeResult>();
         copy.addAll(massnahmenNotizen);
-        
+
         List<MassnahmenUmsetzung> ums = bstUms.getMassnahmenUmsetzungen();
         for (MassnahmenUmsetzung mnums : ums) {
-            List<NotizenMassnahmeResult> notizVorlagen = TransferData.findMassnahmenVorlageNotiz(mnums, massnahmenNotizen);
+            List<NotizenMassnahmeResult> notizVorlagen = TransferData
+                    .findMassnahmenVorlageNotiz(mnums, massnahmenNotizen);
             for (NotizenMassnahmeResult notizVorlage : notizVorlagen) {
                 copy.remove(notizVorlage);
-                
+
                 LOG.debug("Adding note for " + bstUms.getTitle() + ", " + mnums.getKapitel());
                 Integer dbId = mnums.getDbId();
                 String elmtTitle = mnums.getTitle();
                 String noteTitle = "Notiz " + mnums.getKapitel();
                 String text = notizVorlage.notiz.getNotizText();
-                
+
                 saveNewNote(dbId, elmtTitle, noteTitle, text);
                 appendDescription(mnums, notizVorlage.notiz.getNotizText());
             }
         }
-        
+
         if (copy.size() > 0) {
-            List<NotizenMassnahmeResult> bstNotizVorlagen = TransferData.findBausteinVorlageNotiz(massnahmenNotizen);
+            List<NotizenMassnahmeResult> bstNotizVorlagen = TransferData
+                    .findBausteinVorlageNotiz(massnahmenNotizen);
             for (NotizenMassnahmeResult bstNotizVorlage : bstNotizVorlagen) {
                 copy.remove(bstNotizVorlage);
-                
+
                 LOG.debug("Adding note for " + bstUms.getTitle());
                 Integer dbId = bstUms.getDbId();
                 String elmtTitle = bstUms.getTitle();
                 String noteTitle = "Notiz " + bstUms.getKapitel();
                 String text = bstNotizVorlage.notiz.getNotizText();
-                
+
                 saveNewNote(dbId, elmtTitle, noteTitle, text);
             }
-            
+
             LOG.debug("Notes without target object: ");
             for (NotizenMassnahmeResult note : copy) {
                 Logger.getLogger(this.getClass()).debug(note.notiz.getNotizText());
@@ -206,7 +211,6 @@ public class ImportNotesForZielobjekt extends GenericCommand {
         return copy;
     }
 
-
     /**
      * @param dbId
      * @param elmtTitle
@@ -214,7 +218,8 @@ public class ImportNotesForZielobjekt extends GenericCommand {
      * @param text
      * @throws CommandException
      */
-    private void saveNewNote(Integer dbId, String elmtTitle, String noteTitle, String text) throws CommandException {
+    private void saveNewNote(Integer dbId, String elmtTitle, String noteTitle, String text)
+            throws CommandException {
         String convertedText;
         try {
             convertedText = TransferData.convertRtf(text);
@@ -222,11 +227,11 @@ public class ImportNotesForZielobjekt extends GenericCommand {
             convertedText = "!Konvertierungsfehler, Originaltext: " + text;
             LOG.debug(e);
         }
-        
+
         // do not save empty notes:
         Pattern pattern = Pattern.compile("^\\s+$");
         Matcher matcher = pattern.matcher(convertedText);
-        if (matcher.matches()){
+        if (matcher.matches()) {
             return;
         }
         Note note = new Note();
@@ -237,7 +242,6 @@ public class ImportNotesForZielobjekt extends GenericCommand {
         SaveNote command = new SaveNote(note);
         getCommandService().executeCommand(command);
     }
-
 
     /**
      * @param cnATreeElement
@@ -250,7 +254,7 @@ public class ImportNotesForZielobjekt extends GenericCommand {
             if (child instanceof BausteinUmsetzung) {
                 BausteinUmsetzung bstums = (BausteinUmsetzung) child;
                 String id = TransferData.getId(baust);
-                if (bstums.getKapitel().equals(id)){
+                if (bstums.getKapitel().equals(id)) {
                     return bstums;
                 }
             }
