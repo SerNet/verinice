40ad8dc05635602098ccb579703adc6c1ec79333
Jochen Kemnade <jk@sernet.de>
2018-07-30 09:22:35 +0200

    don't check for existing links when importing as catalog
    VN-1874

--- sernet.gs.service/src/sernet/verinice/service/commands/SyncInsertUpdateCommand.java
+++ sernet.gs.service/src/sernet/verinice/service/commands/SyncInsertUpdateCommand.java
@@ -624,7 +624,7 @@ public class SyncInsertUpdateCommand extends GenericCommand implements IAuthAwar
             }
         }
 
-        if (isNew(link)) {
+        if (parameter.isImportAsCatalog() || isNew(link)) {
             dependant.addLinkDown(link);
             dependency.addLinkUp(link);
             if (log.isDebugEnabled()) {
