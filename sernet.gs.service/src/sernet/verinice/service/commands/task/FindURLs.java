2838a202f28dfd0d78497777965cb4ed906d9aaa
Jochen Kemnade <jk@sernet.de>
2018-08-14 08:38:32 +0200

    code format - no functional change

--- sernet.gs.service/src/sernet/verinice/service/commands/task/FindURLs.java
+++ sernet.gs.service/src/sernet/verinice/service/commands/task/FindURLs.java
@@ -27,7 +27,6 @@ import java.util.List;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
-import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -49,65 +48,68 @@ import sernet.verinice.model.common.CnATreeElement;
  */
 public class FindURLs extends GenericCommand {
 
-	private static final long serialVersionUID = 9207422070204886804L;
+    private static final long serialVersionUID = 9207422070204886804L;
 
-	private static final Logger LOG = Logger.getLogger(FindURLs.class);
+    private static final Logger LOG = Logger.getLogger(FindURLs.class);
 
-	private DocumentLinkRoot root = new DocumentLinkRoot();
+    private DocumentLinkRoot root = new DocumentLinkRoot();
 
-	private HibernateCallback hcb;
+    private HibernateCallback hcb;
 
-	public FindURLs(Set<String> allIDs) {
-		hcb = new FindURLsCallbackWithCnATreeElement(allIDs);
-	}
+    public FindURLs(Set<String> allIDs) {
+        hcb = new FindURLsCallbackWithCnATreeElement(allIDs);
+    }
 
-	@Override
+    @Override
     @SuppressWarnings("unchecked")
-	public void execute() {
-		IBaseDao<BSIModel, Serializable> dao = getDaoFactory().getDAO(BSIModel.class);
-		
-		/* Requests the links and resolves the CnATreeElement instances which use
-		 * them. (This is needed for the {@link DocumentView}).
-		 * 
-		 * The result is a list of Object arrays where: The URL is at index 0 and
-		 * the dbid of an CnATreeElement is at index 1.
-		 */
-		List<Object[]> resultList = dao.findByCallback(hcb);
-		
-		// Creates a list of Integers from the second argument. This is needed in the
-		// FindCnATreeElementsCallback.
-		List<Integer> treeElementIds = new ArrayList<Integer>();
-		for (Object[] result : resultList)
-		{
-			treeElementIds.add((Integer) result[1]);
-		}
-		
-		// Retrieves all the CnATreeElement instances which have a document link
-		// according to the query contained in FindURLsCallbackWithCnATreeElement.
-		List<Object[]> treeElements = dao.findByCallback(new FindCnATreeElementsCallback(treeElementIds));
-		
-		// Fills the DocumentRoot structure by iterating the URLs and preparing the
-		// individual DocumentReference instances.
-		final int size = resultList.size();
-		for (int i = 0; i < size; i++)
-		{
-			String rawURL = (String) resultList.get(i)[0];
-			int entityID = ((Integer)resultList.get(i)[1]).intValue();
+    public void execute() {
+        IBaseDao<BSIModel, Serializable> dao = getDaoFactory().getDAO(BSIModel.class);
 
-			String name = URLUtil.getName(rawURL);
-			String url = URLUtil.getHref(rawURL);
-			
-			DocumentLink link = createLinkIfNecessary(name, url);
-			addReferenceToLink(treeElements, entityID, link); 
-		}
-	}
+        /*
+         * Requests the links and resolves the CnATreeElement instances which
+         * use them. (This is needed for the {@link DocumentView}).
+         * 
+         * The result is a list of Object arrays where: The URL is at index 0
+         * and the dbid of an CnATreeElement is at index 1.
+         */
+        List<Object[]> resultList = dao.findByCallback(hcb);
+
+        // Creates a list of Integers from the second argument. This is needed
+        // in the
+        // FindCnATreeElementsCallback.
+        List<Integer> treeElementIds = new ArrayList<Integer>();
+        for (Object[] result : resultList) {
+            treeElementIds.add((Integer) result[1]);
+        }
+
+        // Retrieves all the CnATreeElement instances which have a document link
+        // according to the query contained in
+        // FindURLsCallbackWithCnATreeElement.
+        List<Object[]> treeElements = dao
+                .findByCallback(new FindCnATreeElementsCallback(treeElementIds));
+
+        // Fills the DocumentRoot structure by iterating the URLs and preparing
+        // the
+        // individual DocumentReference instances.
+        final int size = resultList.size();
+        for (int i = 0; i < size; i++) {
+            String rawURL = (String) resultList.get(i)[0];
+            int entityID = ((Integer) resultList.get(i)[1]).intValue();
+
+            String name = URLUtil.getName(rawURL);
+            String url = URLUtil.getHref(rawURL);
+
+            DocumentLink link = createLinkIfNecessary(name, url);
+            addReferenceToLink(treeElements, entityID, link);
+        }
+    }
 
     private void addReferenceToLink(List<Object[]> treeElements, int entityID, DocumentLink link) {
-        if(link != null && treeElements!=null) {
+        if (link != null && treeElements != null) {
             CnATreeElement element = findElement(treeElements, entityID);
-        	if(element != null){
-        	    addReferenceIfAllowed(link, element);
-        	}
+            if (element != null) {
+                addReferenceIfAllowed(link, element);
+            }
         }
     }
 
@@ -118,9 +120,9 @@ public class FindURLs extends GenericCommand {
     }
 
     private CnATreeElement findElement(List<Object[]> treeElements, int entityID) {
-        for(Object[] arr : treeElements){
-            if(((Entity)arr[1]).getDbId().equals(entityID)){
-                return (CnATreeElement)arr[0];
+        for (Object[] arr : treeElements) {
+            if (((Entity) arr[1]).getDbId().equals(entityID)) {
+                return (CnATreeElement) arr[0];
             }
         }
         return null;
@@ -128,88 +130,87 @@ public class FindURLs extends GenericCommand {
 
     private DocumentLink createLinkIfNecessary(String name, String url) {
         DocumentLink link = root.getDocumentLink(name, url);
-        if (link == null && (!name.isEmpty() || !url.isEmpty())) { // at least name or url have to be not empty
+        if (link == null && (!name.isEmpty() || !url.isEmpty())) { // at least
+                                                                   // name or
+                                                                   // url have
+                                                                   // to be not
+                                                                   // empty
             link = new DocumentLink(name, url);
             root.addChild(link);
         }
         return link;
     }
-	
 
+    public DocumentLinkRoot getUrls() {
+        filterNullReferences();
+        return root;
+    }
 
-	public DocumentLinkRoot getUrls() {
-	    filterNullReferences();
-		return root;
-	}
+    private void filterNullReferences() {
+        DocumentLinkRoot filtered = new DocumentLinkRoot();
+        for (DocumentLink link : root.getChildren()) {
+            if (link.getChildren().size() > 0) {
+                filtered.addChild(link);
+            }
+        }
+        root = filtered;
+    }
 
-	private void filterNullReferences(){
-	    DocumentLinkRoot filtered = new DocumentLinkRoot();
-	    for(DocumentLink link : root.getChildren()){
-	        if(link.getChildren().size() > 0){
-	            filtered.addChild(link);
-	        }
-	    }
-	    root = filtered;
-	}
-	
-	@SuppressWarnings("serial")
-	private class FindURLsCallbackWithCnATreeElement implements
-			HibernateCallback, Serializable {
-		
-		private Set<String> types;
-		
-		FindURLsCallbackWithCnATreeElement(Set<String> types) {
-			this.types = types;
-		}
+    @SuppressWarnings("serial")
+    private class FindURLsCallbackWithCnATreeElement implements HibernateCallback, Serializable {
 
-		public Object doInHibernate(Session session) throws HibernateException,
-				SQLException {
-			
-			/**
-			 * Retrieves the property elements which are URLs along with
-			 * the CnATreeElement id that uses it.
-			 */
-			StringBuilder sb = new StringBuilder();
-			sb.append("SELECT p.propertyValue,pl.entityId FROM PropertyList as pl INNER JOIN pl.properties as p ");
-			sb.append("WHERE p.propertyType IN (:types) ");
-			sb.append("AND p.propertyValue IS NOT NULL ");
+        private Set<String> types;
+
+        FindURLsCallbackWithCnATreeElement(Set<String> types) {
+            this.types = types;
+        }
+
+        public Object doInHibernate(Session session) throws HibernateException, SQLException {
+
+            /**
+             * Retrieves the property elements which are URLs along with the
+             * CnATreeElement id that uses it.
+             */
+            StringBuilder sb = new StringBuilder();
+            sb.append(
+                    "SELECT p.propertyValue,pl.entityId FROM PropertyList as pl INNER JOIN pl.properties as p ");
+            sb.append("WHERE p.propertyType IN (:types) ");
+            sb.append("AND p.propertyValue IS NOT NULL ");
             sb.append("AND p.propertyValue NOT LIKE ''");
-			final String hql = sb.toString();
-			Query hqlQuery = session.createQuery(hql);
-			hqlQuery.setParameterList("types", types, sernet.gs.reveng.type.Types.STRING_TYPE);
-			if (LOG.isDebugEnabled()) {
-				LOG.debug("QueryString:\t"+ hqlQuery.getQueryString());
-			}
-			return hqlQuery.list();
-		}
+            final String hql = sb.toString();
+            Query hqlQuery = session.createQuery(hql);
+            hqlQuery.setParameterList("types", types, sernet.gs.reveng.type.Types.STRING_TYPE);
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("QueryString:\t" + hqlQuery.getQueryString());
+            }
+            return hqlQuery.list();
+        }
 
-	}
-	
-	@SuppressWarnings("serial")
-	private class FindCnATreeElementsCallback implements HibernateCallback, Serializable
-	{
-		private List<Integer> treeElementIds;
-		
-		public FindCnATreeElementsCallback(List<Integer> treeElementIds) {
-			this.treeElementIds = treeElementIds;
-		}
+    }
 
-		public Object doInHibernate(Session session) throws HibernateException,
-				SQLException {
-			
-			/*
-			 * Retrieves all the CnATreeElements whose ids are mentioned in the
-			 * treeElementIds list (changed to hql to consider rightmanagement)
-			 */
-			String hql = "from CnATreeElement elmt" + 
-					" inner join elmt.entity as entity" +
-			        " where entity.dbId in (:ids)";
-			if(treeElementIds != null && treeElementIds.size() > 0){
-			    return getDaoFactory().getDAO(CnATreeElement.class).findByQuery(hql, new String[]{"ids"}, new Object[]{treeElementIds});
-			} else {
-			    return Collections.emptyList();
-			}
-		}
-		
-	}
+    @SuppressWarnings("serial")
+    private class FindCnATreeElementsCallback implements HibernateCallback, Serializable {
+        private List<Integer> treeElementIds;
+
+        public FindCnATreeElementsCallback(List<Integer> treeElementIds) {
+            this.treeElementIds = treeElementIds;
+        }
+
+        public Object doInHibernate(Session session) throws HibernateException, SQLException {
+
+            /*
+             * Retrieves all the CnATreeElements whose ids are mentioned in the
+             * treeElementIds list (changed to hql to consider rightmanagement)
+             */
+            String hql = "from CnATreeElement elmt" + " inner join elmt.entity as entity"
+                    + " where entity.dbId in (:ids)";
+            if (treeElementIds != null && treeElementIds.size() > 0) {
+                return getDaoFactory().getDAO(CnATreeElement.class).findByQuery(hql,
+                        new String[] { "ids" }, new Object[] { treeElementIds });
+            } else {
+                return Collections.emptyList();
+            }
+        }
+
+    }
 }
--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/views/DocumentView.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/views/DocumentView.java
@@ -57,217 +57,216 @@ import sernet.verinice.rcp.RightsEnabledView;
 import sernet.verinice.service.commands.task.FindURLs;
 
 public class DocumentView extends RightsEnabledView {
-	
-	public static final String ID = "sernet.gs.ui.rcp.main.documentview"; //$NON-NLS-1$
 
-	private TreeViewer viewer;
+    public static final String ID = "sernet.gs.ui.rcp.main.documentview"; //$NON-NLS-1$
 
-	private Action doubleClickAction;
-	
-	private Action refreshAction;
-	
-	private IModelLoadListener loadListener = new IModelLoadListener() {
-		@Override
+    private TreeViewer viewer;
+
+    private Action doubleClickAction;
+
+    private Action refreshAction;
+
+    private IModelLoadListener loadListener = new IModelLoadListener() {
+        @Override
         public void closed(BSIModel model) {
-			setInputAsync();
-		}
-		
-		@Override
+            setInputAsync();
+        }
+
+        @Override
         public void loaded(final BSIModel model) {
-			setInputAsync();
-		}
+            setInputAsync();
+        }
 
         @Override
         public void loaded(ISO27KModel model) {
-            // work is done in loaded(BSIModel model)       
+            // work is done in loaded(BSIModel model)
         }
 
         @Override
         public void loaded(BpModel model) {
-         // work is done in loaded(BSIModel model)   
-            
+            // work is done in loaded(BSIModel model)
+
         }
 
         @Override
         public void loaded(CatalogModel model) {
             // nothing to do
         }
-	};
+    };
 
-	public DocumentView() {
-	}
-	
-	@Override
-    public String getRightID(){
-	    return ActionRightIDs.DOCUMENTVIEW;
-	}
-	
-	/* (non-Javadoc)
+    public DocumentView() {
+    }
+
+    @Override
+    public String getRightID() {
+        return ActionRightIDs.DOCUMENTVIEW;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
      * @see sernet.verinice.rcp.RightsEnabledView#getViewId()
      */
     @Override
     public String getViewId() {
         return ID;
     }
-	
-	protected void setInput() {
-		Set<String> allIDs = new HashSet<String>();
-		try {
-			List<PropertyType> types;
-			types = HUITypeFactory.getInstance().getURLPropertyTypes();
-			for (PropertyType type : types) {
-				allIDs.add(type.getId());
-			}
 
-			FindURLs command = new FindURLs(allIDs);
-			command = ServiceFactory.lookupCommandService().executeCommand(command);
-			viewer.setInput(command.getUrls());
-		} catch (Exception e) {
-			return;
-		}
-		
-	}
+    protected void setInput() {
+        Set<String> allIDs = new HashSet<String>();
+        try {
+            List<PropertyType> types;
+            types = HUITypeFactory.getInstance().getURLPropertyTypes();
+            for (PropertyType type : types) {
+                allIDs.add(type.getId());
+            }
 
-	@Override
-	public void createPartControl(Composite parent) {
-	    super.createPartControl(parent);
-	    
-	    final int colum1Width = 200;
+            FindURLs command = new FindURLs(allIDs);
+            command = ServiceFactory.lookupCommandService().executeCommand(command);
+            viewer.setInput(command.getUrls());
+        } catch (Exception e) {
+            return;
+        }
+
+    }
+
+    @Override
+    public void createPartControl(Composite parent) {
+        super.createPartControl(parent);
+
+        final int colum1Width = 200;
         final int colum2Width = 100;
         final int colum3Width = colum2Width;
-	    
-		viewer = new TreeViewer(parent,SWT.FULL_SELECTION | SWT.MULTI);
-		viewer.getTree().setLinesVisible(true);
-		viewer.getTree().setHeaderVisible(true);
 
-		TreeViewerColumn column = new TreeViewerColumn(viewer,SWT.NONE);
-		column.setLabelProvider(new ColumnLabelProvider() {
-			@Override
-			public String getText(Object element) {
-				if (element instanceof DocumentLink){
-					return ((DocumentLink)element).getName();
-				}
-				return ""; //$NON-NLS-1$
-			}
-		});
-		column.getColumn().setText(Messages.DocumentView_2);
+        viewer = new TreeViewer(parent, SWT.FULL_SELECTION | SWT.MULTI);
+        viewer.getTree().setLinesVisible(true);
+        viewer.getTree().setHeaderVisible(true);
 
-		TreeViewerColumn column2 = new TreeViewerColumn(viewer,SWT.NONE);
-		column2.setLabelProvider(new ColumnLabelProvider() {
-			@Override
-			public String getText(Object element) {
-				if (element instanceof DocumentLink){
-					return ((DocumentLink)element).getHref();
-				}
-				return ""; //$NON-NLS-1$
-			}
-		});
-		column2.getColumn().setText(Messages.DocumentView_4);
-
-		TreeViewerColumn column3 = new TreeViewerColumn(viewer,SWT.NONE);
-		column3.setLabelProvider(new ColumnLabelProvider() {
-			@Override
-			public String getText(Object element) {
-				if (element instanceof DocumentReference) {
-					DocumentReference docref = (DocumentReference) element;
-					return docref.getCnaTreeElement().getTitle();
-				}
-				return ""; //$NON-NLS-1$
-			}
-		});
-		column3.getColumn().setText(Messages.DocumentView_6);
-		
-		viewer.setContentProvider(new DocumentContentProvider(viewer));
-		viewer.setSorter(new ViewerSorter() {
-			@Override
-			public int compare(Viewer viewer, Object e1, Object e2) {
-				if (e1 instanceof DocumentLink && e2 instanceof DocumentLink) {
-					DocumentLink link1 = (DocumentLink) e1;
-					DocumentLink link2 = (DocumentLink) e2;
-					return link1.getName().compareTo(link2.getName());
-				}
-				if (e1 instanceof DocumentReference && e2 instanceof DocumentReference) {
-					DocumentReference ref1 = (DocumentReference) e1;
-					DocumentReference ref2 = (DocumentReference) e2;
-					return ref1.getCnaTreeElement().getTitle().compareTo(ref2.getCnaTreeElement().getTitle());
-				}
-				return 0;
-			}
-		});
-		
-		setInput();
-		
-		column.getColumn().setWidth(colum1Width);
-		column2.getColumn().setWidth(colum2Width);
-		column3.getColumn().setWidth(colum3Width);
-		
-		makeActions();
-		hookActions();
-		getSite().setSelectionProvider(viewer);
-		CnAElementFactory.getInstance().addLoadListener(loadListener);
-        fillLocalToolBar();
-		addPartLister();
-	}
-	
-
-	@Override
-	public void setFocus() {
-		
-	}
-	
-	private void makeActions() {
-		doubleClickAction = new Action() {
-			@Override
-            public void run() {
-				Object sel = ((IStructuredSelection)viewer.getSelection())
-					.getFirstElement();
-				if (sel instanceof DocumentReference) {
-					DocumentReference ref = (DocumentReference) sel;
-					EditorFactory.getInstance().openEditor(ref.getCnaTreeElement());
-				}
-				else if (sel instanceof DocumentLink) {
-					DocumentLink link = (DocumentLink) sel;
-					Program.launch(link.getHref());
-				}
-			}
-		};
-		
-        refreshAction = new Action(Messages.DocumentView_7, SWT.NONE){
+        TreeViewerColumn column = new TreeViewerColumn(viewer, SWT.NONE);
+        column.setLabelProvider(new ColumnLabelProvider() {
             @Override
-            public void run(){
+            public String getText(Object element) {
+                if (element instanceof DocumentLink) {
+                    return ((DocumentLink) element).getName();
+                }
+                return ""; //$NON-NLS-1$
+            }
+        });
+        column.getColumn().setText(Messages.DocumentView_2);
+
+        TreeViewerColumn column2 = new TreeViewerColumn(viewer, SWT.NONE);
+        column2.setLabelProvider(new ColumnLabelProvider() {
+            @Override
+            public String getText(Object element) {
+                if (element instanceof DocumentLink) {
+                    return ((DocumentLink) element).getHref();
+                }
+                return ""; //$NON-NLS-1$
+            }
+        });
+        column2.getColumn().setText(Messages.DocumentView_4);
+
+        TreeViewerColumn column3 = new TreeViewerColumn(viewer, SWT.NONE);
+        column3.setLabelProvider(new ColumnLabelProvider() {
+            @Override
+            public String getText(Object element) {
+                if (element instanceof DocumentReference) {
+                    DocumentReference docref = (DocumentReference) element;
+                    return docref.getCnaTreeElement().getTitle();
+                }
+                return ""; //$NON-NLS-1$
+            }
+        });
+        column3.getColumn().setText(Messages.DocumentView_6);
+
+        viewer.setContentProvider(new DocumentContentProvider(viewer));
+        viewer.setSorter(new ViewerSorter() {
+            @Override
+            public int compare(Viewer viewer, Object e1, Object e2) {
+                if (e1 instanceof DocumentLink && e2 instanceof DocumentLink) {
+                    DocumentLink link1 = (DocumentLink) e1;
+                    DocumentLink link2 = (DocumentLink) e2;
+                    return link1.getName().compareTo(link2.getName());
+                }
+                if (e1 instanceof DocumentReference && e2 instanceof DocumentReference) {
+                    DocumentReference ref1 = (DocumentReference) e1;
+                    DocumentReference ref2 = (DocumentReference) e2;
+                    return ref1.getCnaTreeElement().getTitle()
+                            .compareTo(ref2.getCnaTreeElement().getTitle());
+                }
+                return 0;
+            }
+        });
+
+        setInput();
+
+        column.getColumn().setWidth(colum1Width);
+        column2.getColumn().setWidth(colum2Width);
+        column3.getColumn().setWidth(colum3Width);
+
+        makeActions();
+        hookActions();
+        getSite().setSelectionProvider(viewer);
+        CnAElementFactory.getInstance().addLoadListener(loadListener);
+        fillLocalToolBar();
+        addPartLister();
+    }
+
+    @Override
+    public void setFocus() {
+
+    }
+
+    private void makeActions() {
+        doubleClickAction = new Action() {
+            @Override
+            public void run() {
+                Object sel = ((IStructuredSelection) viewer.getSelection()).getFirstElement();
+                if (sel instanceof DocumentReference) {
+                    DocumentReference ref = (DocumentReference) sel;
+                    EditorFactory.getInstance().openEditor(ref.getCnaTreeElement());
+                } else if (sel instanceof DocumentLink) {
+                    DocumentLink link = (DocumentLink) sel;
+                    Program.launch(link.getHref());
+                }
+            }
+        };
+
+        refreshAction = new Action(Messages.DocumentView_7, SWT.NONE) {
+            @Override
+            public void run() {
                 setInput();
             }
         };
-        
-        refreshAction.setImageDescriptor(ImageCache.getInstance().getImageDescriptor(ImageCache.RELOAD));
-	}
-	
-	private void hookActions() {
-		viewer.addDoubleClickListener(new IDoubleClickListener() {
-			@Override
+
+        refreshAction
+                .setImageDescriptor(ImageCache.getInstance().getImageDescriptor(ImageCache.RELOAD));
+    }
+
+    private void hookActions() {
+        viewer.addDoubleClickListener(new IDoubleClickListener() {
+            @Override
             public void doubleClick(DoubleClickEvent event) {
-				doubleClickAction.run();
-			}
-		});
-}
+                doubleClickAction.run();
+            }
+        });
+    }
 
     private void setInputAsync() {
         Display.getDefault().asyncExec(new Runnable() {
-        	@Override
+            @Override
             public void run() {
-        		if (viewer.getContentProvider() != null) {
-        			setInput();
-        		}
-        	}
+                if (viewer.getContentProvider() != null) {
+                    setInput();
+                }
+            }
 
-        	
         });
     }
-    
-    
-    private void addPartLister(){
+
+    private void addPartLister() {
         this.getSite().getWorkbenchWindow().getPartService().addPartListener(new IPartListener() {
-            
+
             /**
              * this ensures a refresh on reopening the view
              */
@@ -275,27 +274,30 @@ public class DocumentView extends RightsEnabledView {
             public void partActivated(IWorkbenchPart arg0) {
                 setInput();
             }
-            
+
             // other events does not matter here
             @Override
-            public void partBroughtToTop(IWorkbenchPart arg0) {}
+            public void partBroughtToTop(IWorkbenchPart arg0) {
+            }
 
             @Override
-            public void partClosed(IWorkbenchPart arg0) {}
+            public void partClosed(IWorkbenchPart arg0) {
+            }
 
             @Override
-            public void partDeactivated(IWorkbenchPart arg0) {}
+            public void partDeactivated(IWorkbenchPart arg0) {
+            }
 
             @Override
-            public void partOpened(IWorkbenchPart arg0) {}
+            public void partOpened(IWorkbenchPart arg0) {
+            }
         });
     }
-    
+
     private void fillLocalToolBar() {
         IActionBars bars = getViewSite().getActionBars();
         IToolBarManager manager = bars.getToolBarManager();
         manager.add(this.refreshAction);
     }
-
 
 }
