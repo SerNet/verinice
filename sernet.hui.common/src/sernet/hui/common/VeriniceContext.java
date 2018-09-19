082b6f0f9707e8c60e4d319b6530113eb4f46699
Alexander Ben Nasrallah <an@sernet.de>
2018-09-19 15:12:15 +0200

    add risk analysis to spring context
    VN-2027

--- sernet.hui.common/src/sernet/hui/common/VeriniceContext.java
+++ sernet.hui.common/src/sernet/hui/common/VeriniceContext.java
@@ -128,6 +128,8 @@ public final class VeriniceContext {
     
     public static final String RISK_ANALYSIS_SERVICE = "riskAnalysisService";
     
+    public static final String ITBP_RISK_SERVICE = "itbpRiskService";
+
     public static final String LICENSE_SERVICE = "licenseManagementService";
     
     public static final String ENCRYPTION_SERVICE = "encryptionService";
