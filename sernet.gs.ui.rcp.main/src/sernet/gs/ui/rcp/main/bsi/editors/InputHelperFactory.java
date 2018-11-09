2886fb1efa5d02e9c0841d7b0c5e19e6fcead53e
Jochen Kemnade <jk@sernet.de>
2018-11-09 12:45:11 +0100

    code refactoring and simplification

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/editors/InputHelperFactory.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/editors/InputHelperFactory.java
@@ -18,6 +18,7 @@
 package sernet.gs.ui.rcp.main.bsi.editors;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import sernet.gs.ui.rcp.main.Activator;
 import sernet.gs.ui.rcp.main.ExceptionUtil;
@@ -59,109 +60,63 @@ import sernet.verinice.model.iso27k.Response;
 import sernet.verinice.model.iso27k.Threat;
 import sernet.verinice.model.iso27k.Vulnerability;
 
-public class InputHelperFactory {
+public final class InputHelperFactory {
 
     private static IInputHelper schutzbedarfHelper;
     private static IInputHelper tagHelper;
     private static IInputHelper personHelper;
 
     public static void setInputHelpers(EntityType entityType, HitroUIComposite huiComposite2) {
-        //
         if (personHelper == null) {
-            personHelper = new IInputHelper() {
-                public String[] getSuggestions() {
-                    List<Person> personen;
-                    try {
-                        personen = CnAElementHome.getInstance().getPersonen();
-                        String[] titles = new String[personen.size()];
-                        int i = 0;
-                        for (Person person : personen) {
-                            titles[i++] = person.getTitle();
-                        }
-                        return titles.length > 0 ? titles
-                                : new String[] { Messages.InputHelperFactory_0 };
-                    } catch (CommandException e) {
-                        ExceptionUtil.log(e, Messages.InputHelperFactory_1);
+            personHelper = () -> {
+                try {
+                    List<Person> personen = CnAElementHome.getInstance().getPersonen();
+                    if (personen.isEmpty()) {
                         return new String[] { Messages.InputHelperFactory_0 };
                     }
+                    return personen.stream().map(Person::getTitle).toArray(String[]::new);
+
+                } catch (CommandException e) {
+                    ExceptionUtil.log(e, Messages.InputHelperFactory_1);
+                    return new String[] { Messages.InputHelperFactory_0 };
                 }
             };
         }
 
         if (tagHelper == null) {
-            tagHelper = new IInputHelper() {
-                public String[] getSuggestions() {
-                    List<String> tags;
-                    try {
-                        tags = CnAElementHome.getInstance().getTags();
-                        String[] tagArray = tags.toArray(new String[tags.size()]);
-                        for (int i = 0; i < tagArray.length; i++) {
-                            tagArray[i] = tagArray[i] + " "; //$NON-NLS-1$
-                        }
-                        return tagArray.length > 0 ? tagArray : new String[] {};
-                    } catch (CommandException e) {
-                        ExceptionUtil.log(e, Messages.InputHelperFactory_5);
-                        return new String[] {};
-                    }
+            tagHelper = () -> {
+                try {
+                    return CnAElementHome.getInstance().getTags().stream().toArray(String[]::new);
+                } catch (CommandException e) {
+                    ExceptionUtil.log(e, Messages.InputHelperFactory_5);
+                    return new String[] {};
                 }
             };
         }
 
         if (schutzbedarfHelper == null) {
-            schutzbedarfHelper = new IInputHelper() {
-                public String[] getSuggestions() {
-                    return new String[] { Messages.InputHelperFactory_4,
-                            Messages.InputHelperFactory_2, Messages.InputHelperFactory_3 };
-                }
-            };
+            schutzbedarfHelper = () -> new String[] { Messages.InputHelperFactory_4,
+                    Messages.InputHelperFactory_2, Messages.InputHelperFactory_3 };
         }
 
         boolean showHint = Activator.getDefault().getPluginPreferences()
                 .getBoolean(PreferenceConstants.INPUTHINTS);
 
         // Tag Helpers:
-        // BSI elements
-        huiComposite2.setInputHelper(Anwendung.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD,
-                showHint);
-        huiComposite2.setInputHelper(Client.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(Gebaeude.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(NetzKomponente.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD,
-                showHint);
-        huiComposite2.setInputHelper(Person.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(Raum.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(Server.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(SonstIT.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(TelefonKomponente.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD,
-                showHint);
-        huiComposite2.setInputHelper(ITVerbund.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD,
-                showHint);
 
-        // ISO27k elements
-        huiComposite2.setInputHelper(Asset.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(Audit.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(Control.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(Document.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(Evidence.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(Exception.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD,
-                showHint);
-        huiComposite2.setInputHelper(Finding.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(Incident.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(IncidentScenario.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD,
-                showHint);
-        huiComposite2.setInputHelper(Interview.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD,
-                showHint);
-        huiComposite2.setInputHelper(PersonIso.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD,
-                showHint);
-        huiComposite2.setInputHelper(Process.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(Record.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(Requirement.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD,
-                showHint);
-        huiComposite2.setInputHelper(Response.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(Threat.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD, showHint);
-        huiComposite2.setInputHelper(Vulnerability.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD,
-                showHint);
-        huiComposite2.setInputHelper(Organization.PROP_TAG, tagHelper, IInputHelper.TYPE_ADD,
-                showHint);
+        Stream.of(
+                // BSI elements
+                Anwendung.PROP_TAG, Client.PROP_TAG, Gebaeude.PROP_TAG, NetzKomponente.PROP_TAG,
+                Person.PROP_TAG, Raum.PROP_TAG, Server.PROP_TAG, SonstIT.PROP_TAG,
+                TelefonKomponente.PROP_TAG, ITVerbund.PROP_TAG,
+                // ISO27k elements
+                Asset.PROP_TAG, Audit.PROP_TAG, Control.PROP_TAG, Document.PROP_TAG,
+                Evidence.PROP_TAG, Exception.PROP_TAG, Finding.PROP_TAG, Incident.PROP_TAG,
+                IncidentScenario.PROP_TAG, Interview.PROP_TAG, PersonIso.PROP_TAG, Process.PROP_TAG,
+                Record.PROP_TAG, Requirement.PROP_TAG, Response.PROP_TAG, Threat.PROP_TAG,
+                Vulnerability.PROP_TAG, Organization.PROP_TAG)
+                .forEach(propertyName -> huiComposite2.setInputHelper(propertyName, tagHelper,
+                        IInputHelper.TYPE_ADD, showHint));
 
         setSchutzbedarfHelpers(entityType, huiComposite2, showHint);
 
@@ -179,6 +134,10 @@ public class InputHelperFactory {
                 }
             }
         }
+
+    }
+
+    private InputHelperFactory() {
 
     }
 }
