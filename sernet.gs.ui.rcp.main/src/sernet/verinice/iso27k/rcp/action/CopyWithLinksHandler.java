a84274d8531cee78754d5aa22423efebe1d13c3c
Jochen Kemnade <jk@sernet.de>
2019-06-12 11:54:22 +0200

    check for invalid links when copying elements with links
    VN-2486

--- sernet.gs.ui.rcp.main/src/sernet/verinice/iso27k/rcp/action/CopyWithLinksHandler.java
+++ sernet.gs.ui.rcp.main/src/sernet/verinice/iso27k/rcp/action/CopyWithLinksHandler.java
@@ -19,16 +19,29 @@
  ******************************************************************************/
 package sernet.verinice.iso27k.rcp.action;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
 
+import sernet.gs.ui.rcp.main.service.ServiceFactory;
+import sernet.verinice.interfaces.CommandException;
 import sernet.verinice.iso27k.rcp.CnPItems;
+import sernet.verinice.model.common.CnALink;
+import sernet.verinice.model.common.CnATreeElement;
+import sernet.verinice.service.commands.ValidateLinksInSubtrees;
 
 /**
  * @author Daniel Murygin <dm[at]sernet[dot]de>
  *
  */
 public class CopyWithLinksHandler extends CopyHandler {
+
+    private static final int ERROR_MESSAGE_LINK_CUTOFF = 5;
 
     /*
      * @see
@@ -37,9 +50,52 @@ public class CopyWithLinksHandler extends CopyHandler {
      */
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
+        IStructuredSelection selectedElements = HandlerUtil.getCurrentStructuredSelection(event);
+        if (!selectedElements.isEmpty()) {
+            Set<String> selectedElementsUUIDs = new HashSet<>(selectedElements.size());
+            for (Object selectedElement : selectedElements.toList()) {
+                if (selectedElement instanceof CnATreeElement) {
+                    selectedElementsUUIDs.add(((CnATreeElement) selectedElement).getUuid());
+                }
+            }
+
+            ValidateLinksInSubtrees validateLinksInSubtrees = new ValidateLinksInSubtrees(
+                    selectedElementsUUIDs);
+            try {
+                validateLinksInSubtrees = ServiceFactory.lookupCommandService()
+                        .executeCommand(validateLinksInSubtrees);
+
+                Set<CnALink> invalidLinks = validateLinksInSubtrees.getInvalidLinks();
+                if (!invalidLinks.isEmpty()) {
+                    showInvalidLinksMessage(invalidLinks);
+                    throw new ExecutionException(
+                            "Found " + invalidLinks.size() + " invalid link(s): " + invalidLinks);
+
+                }
+            } catch (CommandException e) {
+                throw new ExecutionException("Could not check links", e);
+            }
+
+        }
+
         Object result = super.execute(event);
         CnPItems.setCopyLinks(true);
         return result;
+    }
+
+    private static void showInvalidLinksMessage(Set<CnALink> invalidLinks) {
+        StringBuilder dialogMessageBuilder = new StringBuilder(
+                Messages.getString("InvalidLinksDialogMessage")).append("\n");
+        invalidLinks.stream().limit(ERROR_MESSAGE_LINK_CUTOFF)
+                .forEach(link -> dialogMessageBuilder.append("\n - ")
+                        .append(link.getDependant().getTitle()).append(" → ")
+                        .append(link.getDependency().getTitle()).append(" (")
+                        .append(link.getRelationId()).append(")"));
+        if (invalidLinks.size() > ERROR_MESSAGE_LINK_CUTOFF) {
+            dialogMessageBuilder.append("\n...");
+        }
+        MessageDialog.openError(null, Messages.getString("InvalidLinksDialogTitle"),
+                dialogMessageBuilder.toString());
     }
 
 }
