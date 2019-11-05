a85fd14a755d277b0d96290570230765382c1937
Daniel Murygin <dm@sernet.de>
2019-11-05 09:29:42 +0100

    Remove the call of the ITPB modeling migration
    VN-2634

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/Activator.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/Activator.java
@@ -622,7 +622,6 @@ public class Activator extends AbstractUIPlugin implements IMain {
         try {
             DbVersion command = new DbVersion(VersionConstants.COMPATIBLE_CLIENT_VERSION);
             command = ServiceFactory.lookupCommandService().executeCommand(command);
-            ServiceFactory.lookupModelingMigrationService().migrateModeling();
             done[0] = true;
         } catch (CommandException e) {
             done[0] = true;
