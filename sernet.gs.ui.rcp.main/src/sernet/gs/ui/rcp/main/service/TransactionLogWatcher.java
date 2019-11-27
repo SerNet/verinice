482f7c68e46c673db3f9f316a55b483744fdd008
Alexander Ben Nasrallah <an@sernet.de>
2019-11-27 13:48:43 +0100

    code formatting
    VEST-8

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/service/TransactionLogWatcher.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/service/TransactionLogWatcher.java
@@ -43,104 +43,111 @@ import sernet.verinice.service.commands.task.GetChangesSince;
  */
 // TODO server: implement server-side push of events (beacon service)
 public class TransactionLogWatcher {
-	
-	private final Logger log = Logger.getLogger(TransactionLogWatcher.class);
-	
-	private Date lastChecked = null;
 
-	/** ICommandService instance is injected by Spring. */
-	private ICommandService commandService;
+    private final Logger log = Logger.getLogger(TransactionLogWatcher.class);
 
-	public void checkLog() {
-		Activator.inheritVeriniceContextState();
+    private Date lastChecked = null;
 
-		if (!CnAElementFactory.isModelLoaded() && !CnAElementFactory.isIsoModelLoaded())
-		{
-			return;
-		}
+    /** ICommandService instance is injected by Spring. */
+    private ICommandService commandService;
 
-		// No need to do anything when the internal server is used as this
-		// means that there is only one user.
-		if (Preferences.isStandalone()){
-			return;
-		}
+    public void checkLog() {
+        Activator.inheritVeriniceContextState();
 
-		try {
-			GetChangesSince command = new GetChangesSince(lastChecked, ChangeLogEntry.STATION_ID);
-			command = commandService.executeCommand(command);
+        if (!CnAElementFactory.isModelLoaded() && !CnAElementFactory.isIsoModelLoaded()) {
+            return;
+        }
 
-			lastChecked = command.getLastChecked();
-			List<ChangeLogEntry> entries = command.getEntries();				
-			if(entries==null || entries.isEmpty() ) {
-    			if(log.isDebugEnabled()) {
-    				log.debug("No changes");
-    			}
-			} else {
-			    Collections.sort(entries);
-    			for (ChangeLogEntry changeLogEntry : entries) {
-    				Integer elementId = changeLogEntry.getElementId();
-    				CnATreeElement changedElement = command.getChangedElements().get(elementId);
-    				process(changeLogEntry, changedElement);
-    			}
-			}
+        // No need to do anything when the internal server is used as this
+        // means that there is only one user.
+        if (Preferences.isStandalone()) {
+            return;
+        }
 
-		} catch (CommandException e) {
-			log.error("Fehler bei Abfrage des Transaktionslogfiles.", e);
-		}
-	}
+        try {
+            GetChangesSince command = new GetChangesSince(lastChecked, ChangeLogEntry.STATION_ID);
+            command = commandService.executeCommand(command);
 
-	/**
-	 * @param changeLogEntry
-	 * @param changedElement
-	 */
-	private void process(ChangeLogEntry changeLogEntry, CnATreeElement changedElement) {
-		int changetype = changeLogEntry.getChange();
-		if (log.isInfoEnabled()) {
-			log.info("Processing change event type " + changeLogEntry.getChangeDescription() + " from user " + changeLogEntry.getUsername() + " for element " + changeLogEntry.getElementClass() + " / " + changeLogEntry.getElementId());
-		}
-		
-	
-		switch (changetype) {
-			case ChangeLogEntry.TYPE_UPDATE:
-				CnAElementFactory.getModel(changedElement).databaseChildChanged(changedElement);
-				break;
-	
-			case ChangeLogEntry.TYPE_INSERT:
-			    if(changedElement!=null) {
-			        CnAElementFactory.getModel(changedElement).databaseChildAdded(changedElement);
-			        CnAElementFactory.getModel(changedElement).childAdded(changedElement.getParent(), changedElement);
-			    }
-			    break;
-			case ChangeLogEntry.TYPE_DELETE:
-				if (changedElement == null) {
-					// element no longer retrievable, notify by ID:
-					CnAElementFactory.databaseChildRemoved(changeLogEntry);
-				} else {
-					// element was retrieved before deletion took place, notify
-					// using element itself:
-					CnAElementFactory.getModel(changedElement).databaseChildRemoved(changedElement);
-				}
-	
-				break;
-			case ChangeLogEntry.TYPE_PERMISSION:
-				// Changes to the permissions are potentially disruptive (items may
-				// be invisible now etc). As such reload everything.
-	
+            lastChecked = command.getLastChecked();
+            List<ChangeLogEntry> entries = command.getEntries();
+            if (entries == null || entries.isEmpty()) {
+                if (log.isDebugEnabled()) {
+                    log.debug("No changes");
+                }
+            } else {
+                Collections.sort(entries);
+                for (ChangeLogEntry changeLogEntry : entries) {
+                    Integer elementId = changeLogEntry.getElementId();
+                    CnATreeElement changedElement = command.getChangedElements().get(elementId);
+                    process(changeLogEntry, changedElement);
+                }
+            }
+
+        } catch (CommandException e) {
+            log.error("Fehler bei Abfrage des Transaktionslogfiles.", e);
+        }
+    }
+
+    /**
+     * @param changeLogEntry
+     * @param changedElement
+     */
+    private void process(ChangeLogEntry changeLogEntry, CnATreeElement changedElement) {
+        int changetype = changeLogEntry.getChange();
+        if (log.isInfoEnabled()) {
+            log
+                    .info("Processing change event type " + changeLogEntry.getChangeDescription()
+                            + " from user " + changeLogEntry.getUsername() + " for element "
+                            + changeLogEntry.getElementClass() + " / "
+                            + changeLogEntry.getElementId());
+        }
+
+        switch (changetype) {
+        case ChangeLogEntry.TYPE_UPDATE:
+            CnAElementFactory.getModel(changedElement).databaseChildChanged(changedElement);
+            break;
+
+        case ChangeLogEntry.TYPE_INSERT:
+            if (changedElement != null) {
+                CnAElementFactory.getModel(changedElement).databaseChildAdded(changedElement);
+                CnAElementFactory
+                        .getModel(changedElement)
+                        .childAdded(changedElement.getParent(), changedElement);
+            }
+            break;
+        case ChangeLogEntry.TYPE_DELETE:
+            if (changedElement == null) {
+                // element no longer retrievable, notify by ID:
+                CnAElementFactory.databaseChildRemoved(changeLogEntry);
+            } else {
+                // element was retrieved before deletion took place, notify
+                // using element itself:
+                CnAElementFactory.getModel(changedElement).databaseChildRemoved(changedElement);
+            }
+
+            break;
+        case ChangeLogEntry.TYPE_PERMISSION:
+            // Changes to the permissions are potentially disruptive (items may
+            // be invisible now etc). As such reload everything.
+
             CnAElementFactory.getInstance().reloadAllModelsFromDatabase();
-				break;
-			default:
-			    //ignore (but debug) other change types
-				Logger.getLogger(this.getClass()).debug("Unrecognized change type received from server. Ignored, type ID was: " + changetype);
-				break;
-		}
-	}
+            break;
+        default:
+            // ignore (but debug) other change types
+            Logger
+                    .getLogger(this.getClass())
+                    .debug("Unrecognized change type received from server. Ignored, type ID was: "
+                            + changetype);
+            break;
+        }
+    }
 
-	public void setCommandService(ICommandService commandService) {
-		this.commandService = commandService;
-	}
+    public void setCommandService(ICommandService commandService) {
+        this.commandService = commandService;
+    }
 
-	public ICommandService getCommandService() {
-		return commandService;
-	}
+    public ICommandService getCommandService() {
+        return commandService;
+    }
 
 }
