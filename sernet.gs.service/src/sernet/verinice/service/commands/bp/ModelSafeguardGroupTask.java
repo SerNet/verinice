a7776aa3a7620daba23ede856f98af15de0759db
Jochen Kemnade <jk@sernet.de>
2020-02-24 08:46:07 +0100

    compare the identifier when checking for existing safeguard groups
    VN-2640

--- sernet.gs.service/src/sernet/verinice/service/commands/bp/ModelSafeguardGroupTask.java
+++ sernet.gs.service/src/sernet/verinice/service/commands/bp/ModelSafeguardGroupTask.java
@@ -71,7 +71,7 @@ public class ModelSafeguardGroupTask extends ModelCopyTask {
             return ((Safeguard) element).getIdentifier();
         }
         if (element instanceof SafeguardGroup) {
-            return ((SafeguardGroup) element).getTitle();
+            return ((SafeguardGroup) element).getIdentifier();
         }
         throw new IllegalArgumentException("Cannot handle " + element);
     }
