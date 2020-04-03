4a5ed0f6b3d1fb09d271f9d969910b00e961daa9
Alexander Ben Nasrallah <an@sernet.de>
2020-04-03 09:58:25 +0200

    add utilility method to obtain a localized properties file path
    VN-2738

---
+++ sernet.gs.service/src/sernet/gs/service/PropertiesFileUtil.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Alexander Ben Nasrallah.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+package sernet.gs.service;
+
+import java.io.File;
+import java.util.Locale;
+import java.util.function.Function;
+
+import org.apache.commons.io.FilenameUtils;
+
+/**
+ * Provides utility methods for properties files.
+ */
+public class PropertiesFileUtil {
+
+    /**
+     * <p>
+     * Returns the properties file to a given file depending on the locale.
+     *
+     * <p>
+     * Basically the file extension is replaces by _<language>.properties. "en"
+     * is considered the default language and not added to the file path.
+     */
+    public static File getPropertiesFile(final File file, final Locale locale) {
+        final String localeSuffix = "en".equals(locale.getLanguage()) ? ""
+                : "_" + locale.getLanguage();
+        final Function<File, String> getPath = File::getPath;
+        return getPath.andThen(FilenameUtils::removeExtension).andThen(s -> s.concat(localeSuffix))
+                .andThen(s -> s.concat(".properties")).andThen(File::new).apply(file);
+    }
+
+    private PropertiesFileUtil() {
+    }
+}
---
+++ sernet.gs.service/srcTests/sernet/gs/service/PropertiesFileUtilTest.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Alexander Ben Nasrallah.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+package sernet.gs.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.Locale;
+
+import org.junit.Test;
+
+public class PropertiesFileUtilTest {
+
+    @Test
+    public void testGetPropertiesFileName() {
+        assertEquals(new File("/tmp/test.properties"), PropertiesFileUtil.getPropertiesFile(new File("/tmp/test.rtpdesign"), Locale.ENGLISH));
+        assertEquals(new File("/tmp/test_de.properties"), PropertiesFileUtil.getPropertiesFile(new File("/tmp/test_de.rtpdesign"), Locale.ENGLISH));
+
+        assertEquals(new File("/tmp/test_de.properties"), PropertiesFileUtil.getPropertiesFile(new File("/tmp/test.rtpdesign"), Locale.GERMANY));
+        assertEquals(new File("/tmp/test_de_de.properties"), PropertiesFileUtil.getPropertiesFile(new File("/tmp/test_de.rtpdesign"), Locale.GERMANY));
+
+        assertEquals(new File("/tmp/test__de.properties"), PropertiesFileUtil.getPropertiesFile(new File("/tmp/test_.rtpdesign"), Locale.GERMANY));
+        assertEquals(new File("/tmp/test_de__de.properties"), PropertiesFileUtil.getPropertiesFile(new File("/tmp/test_de_.rtpdesign"), Locale.GERMANY));
+    }
+}
