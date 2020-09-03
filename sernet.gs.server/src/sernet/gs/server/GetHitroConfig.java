1f704b60151b414f3aef40854e4875a276a02f30
Jochen Kemnade <jk@sernet.de>
2020-09-03 08:28:07 +0200

    code format - no functional change

--- sernet.gs.server/src/sernet/gs/server/GetHitroConfig.java
+++ sernet.gs.server/src/sernet/gs/server/GetHitroConfig.java
@@ -36,15 +36,15 @@ import sernet.hui.common.connect.SNCAMessages;
  * Servlet implementation class GetHitroConfig
  */
 public class GetHitroConfig extends HttpServlet {
-	private final Logger log = Logger.getLogger(GetHitroConfig.class);
-	
-	private static final long serialVersionUID = 1L;
-	
-	// copy binary data using 100K buffer:
-	static final int BUFF_SIZE = 100000;
+    private final Logger log = Logger.getLogger(GetHitroConfig.class);
 
-	static final byte[] BUFFER = new byte[BUFF_SIZE];
-       
+    private static final long serialVersionUID = 1L;
+
+    // copy binary data using 100K buffer:
+    static final int BUFF_SIZE = 100000;
+
+    static final byte[] BUFFER = new byte[BUFF_SIZE];
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -52,8 +52,10 @@ public class GetHitroConfig extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-    
-    /* (non-Javadoc)
+
+    /*
+     * (non-Javadoc)
+     * 
      * @see javax.servlet.GenericServlet#init(javax.servlet.ServletConfig)
      */
     @Override
@@ -63,74 +65,77 @@ public class GetHitroConfig extends HttpServlet {
         config.getServletContext();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		OutputStream out = null;
-		InputStream in = null;
-		try {
-			String basePath = getInitParameter("snca.xml.path");
-			if(basePath==null) {
-				String message = "init parameter snca.xml.path is not set in web.xml";
-				response.getWriter().append(message);
-				throw new RuntimeException(message);
-			} else {
-				String fileName = HUITypeFactory.HUI_CONFIGURATION_FILE;
-				String resourceParameter = request.getParameter("resource");
-				if(resourceParameter!=null) {
-					// return a resource bundle
-					// security check
-					if(resourceParameter.indexOf("..")!=-1 
-					   || resourceParameter.indexOf(':')!=-1
-					   || !resourceParameter.startsWith(SNCAMessages.BUNDLE_NAME)
-					   || !resourceParameter.endsWith(SNCAMessages.BUNDLE_EXTENSION)) {
-						String message = "illegal parameter: " + resourceParameter;
-						response.getWriter().append(message);
-						throw new RuntimeException(message);
-					}
-					fileName = resourceParameter;
-				} 
-				String path = basePath + "/" + fileName;
-				if (log.isDebugEnabled()) {
-					log.debug("returning: " + path );
-				}
-				in = getServletContext().getResourceAsStream(path);
-				if(in==null) {
-					String message = "Resource not found: " + path;
-					response.getWriter().append(message);
-					// check if an language only file is searched (i.e. snca-messages_de.properties)
-					// or an language-region file (i.e. snca-messages_de_DE.properties)
-					if(fileName.matches("snca\\-messages_[A-Za-z]{2}_[A-Za-z]{2}\\.properties")) {
-					    if (log.isInfoEnabled()) {
-		                    log.info(message);
-		                }
-					} else {
-					    throw new RuntimeException(message);
-					}
-				} else {
-    				out = response.getOutputStream();
-    				while (true) {
-    					synchronized (BUFFER) {
-    						int amountRead = in.read(BUFFER);
-    						if (amountRead == -1) {
-    							break;
-    						}
-    						out.write(BUFFER, 0, amountRead);
-    					}
-    				}
-				}
-			}
-		} catch (IOException e) {
-			log.error("Error while getting hitro config or resource bundle.", e);
-		} finally {
-			if (in != null) {
-				in.close();
-			}
-			if (out != null) {
-				out.close();
-			}
-		}
-	}
-
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        OutputStream out = null;
+        InputStream in = null;
+        try {
+            String basePath = getInitParameter("snca.xml.path");
+            if (basePath == null) {
+                String message = "init parameter snca.xml.path is not set in web.xml";
+                response.getWriter().append(message);
+                throw new RuntimeException(message);
+            } else {
+                String fileName = HUITypeFactory.HUI_CONFIGURATION_FILE;
+                String resourceParameter = request.getParameter("resource");
+                if (resourceParameter != null) {
+                    // return a resource bundle
+                    // security check
+                    if (resourceParameter.indexOf("..") != -1
+                            || resourceParameter.indexOf(':') != -1
+                            || !resourceParameter.startsWith(SNCAMessages.BUNDLE_NAME)
+                            || !resourceParameter.endsWith(SNCAMessages.BUNDLE_EXTENSION)) {
+                        String message = "illegal parameter: " + resourceParameter;
+                        response.getWriter().append(message);
+                        throw new RuntimeException(message);
+                    }
+                    fileName = resourceParameter;
+                }
+                String path = basePath + "/" + fileName;
+                if (log.isDebugEnabled()) {
+                    log.debug("returning: " + path);
+                }
+                in = getServletContext().getResourceAsStream(path);
+                if (in == null) {
+                    String message = "Resource not found: " + path;
+                    response.getWriter().append(message);
+                    // check if an language only file is searched (i.e.
+                    // snca-messages_de.properties)
+                    // or an language-region file (i.e.
+                    // snca-messages_de_DE.properties)
+                    if (fileName.matches("snca\\-messages_[A-Za-z]{2}_[A-Za-z]{2}\\.properties")) {
+                        if (log.isInfoEnabled()) {
+                            log.info(message);
+                        }
+                    } else {
+                        throw new RuntimeException(message);
+                    }
+                } else {
+                    out = response.getOutputStream();
+                    while (true) {
+                        synchronized (BUFFER) {
+                            int amountRead = in.read(BUFFER);
+                            if (amountRead == -1) {
+                                break;
+                            }
+                            out.write(BUFFER, 0, amountRead);
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            log.error("Error while getting hitro config or resource bundle.", e);
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
 }
