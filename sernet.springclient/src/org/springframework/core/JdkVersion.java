d65eeb85ed20d54e9b5c99ce9cbd5914300f93ff
Urs Zeidler <uz@sernet.de>
2021-01-27 14:54:54 +0100

    update libs, updatesite and tp
    VN-2857
    minor patch, remove class from jar, patch jdk version, add jaxb impl,
    increase version, add qualifier to elastic search, update
    es, update spring.core to sec03, replace jar, fix jaxb dependencies, fix
    java servlet, add quallifier, add note in gradle file

--- sernet.springclient/src/org/springframework/core/JdkVersion.java
+++ sernet.springclient/src/org/springframework/core/JdkVersion.java
@@ -19,135 +19,138 @@ package org.springframework.core;
 import sernet.verinice.interfaces.IVeriniceConstants;
 
 /**
- * Internal helper class used to find the Java/JDK version
- * that Spring is operating on, to allow for automatically
- * adapting to the present platform's capabilities.
- *
- * <p>Note that Spring requires JVM 1.4 or higher, as of Spring 2.5.
- *
+ * This is a hacked class, which will be added to the spring-core-verinice-patch.jar in favor
+ * of the original class, which don't work with 1.5. This class needs compiling 
+ * and to be placed in the spring-core-verinice-patch.jar.
  * @author Rod Johnson
  * @author Juergen Hoeller
  * @author Rick Evans
  */
 public abstract class JdkVersion {
 
-	/**
-	 * Constant identifying the 1.3.x JVM (JDK 1.3).
-	 */
-	public static final int JAVA_13 = 0;
+    /**
+     * Constant identifying the 1.3.x JVM (JDK 1.3).
+     */
+    public static final int JAVA_13 = 0;
 
-	/**
-	 * Constant identifying the 1.4.x JVM (J2SE 1.4).
-	 */
-	public static final int JAVA_14 = 1;
+    /**
+     * Constant identifying the 1.4.x JVM (J2SE 1.4).
+     */
+    public static final int JAVA_14 = 1;
 
-	/**
-	 * Constant identifying the 1.5 JVM (Java 5).
-	 */
-	public static final int JAVA_15 = 2;
+    /**
+     * Constant identifying the 1.5 JVM (Java 5).
+     */
+    public static final int JAVA_15 = 2;
 
-	/**
-	 * Constant identifying the 1.6 JVM (Java 6).
-	 */
-	public static final int JAVA_16 = 3;
+    /**
+     * Constant identifying the 1.6 JVM (Java 6).
+     */
+    public static final int JAVA_16 = 3;
 
-	/**
-	 * Constant identifying the 1.7 JVM (Java 7).
-	 */
-	public static final int JAVA_17 = 4;
-	
-	/**
+    /**
+     * Constant identifying the 1.7 JVM (Java 7).
+     */
+    public static final int JAVA_17 = 4;
+
+    /**
      * Constant identifying the 1.8 JVM (Java 8).
      */
     public static final int JAVA_18 = 5;
 
+    /**
+     * Constant identifying the 11.x JVM (Java 11).
+     */
+    public static final int JAVA_11 = 6;
 
-	private static final String javaVersion;
+    private static final String javaVersion;
 
-	private static final int majorJavaVersion;
+    private static final int majorJavaVersion;
 
-	static {
-		javaVersion = System.getProperty(IVeriniceConstants.JAVA_VERSION);
-		// version String should look like "1.4.2_10"
-		if (javaVersion.indexOf("1.8.") != -1) {
+    static {
+        javaVersion = System.getProperty(IVeriniceConstants.JAVA_VERSION);
+        // version String should look like "1.4.2_10"
+        if (javaVersion.startsWith("11.")) {
+            majorJavaVersion = JAVA_11;
+        } else if (javaVersion.indexOf("1.8.") != -1) {
             majorJavaVersion = JAVA_18;
+        } else if (javaVersion.indexOf("1.7.") != -1) {
+            majorJavaVersion = JAVA_17;
+        } else if (javaVersion.indexOf("1.6.") != -1) {
+            majorJavaVersion = JAVA_16;
+        } else if (javaVersion.indexOf("1.5.") != -1) {
+            majorJavaVersion = JAVA_15;
+        } else {
+            // else leave 1.4 as default (it's either 1.4 or unknown)
+            majorJavaVersion = Integer.MAX_VALUE;
         }
-		else if (javaVersion.indexOf("1.7.") != -1) {
-			majorJavaVersion = JAVA_17;
-		}
-		else if (javaVersion.indexOf("1.6.") != -1) {
-			majorJavaVersion = JAVA_16;
-		}
-		else if (javaVersion.indexOf("1.5.") != -1) {
-			majorJavaVersion = JAVA_15;
-		}
-		else {
-			// else leave 1.4 as default (it's either 1.4 or unknown)
-			majorJavaVersion = JAVA_14;
-		}
-	}
+    }
 
+    /**
+     * Return the full Java version string, as returned by
+     * <code>System.getProperty("java.version")</code>.
+     * 
+     * @return the full Java version string
+     * @see System#getProperty(String)
+     */
+    public static String getJavaVersion() {
+        return javaVersion;
+    }
 
-	/**
-	 * Return the full Java version string, as returned by
-	 * <code>System.getProperty("java.version")</code>.
-	 * @return the full Java version string
-	 * @see System#getProperty(String)
-	 */
-	public static String getJavaVersion() {
-		return javaVersion;
-	}
+    /**
+     * Get the major version code. This means we can do things like
+     * <code>if (getMajorJavaVersion() < JAVA_14)</code>.
+     * 
+     * @return a code comparable to the JAVA_XX codes in this class
+     * @see #JAVA_13
+     * @see #JAVA_14
+     * @see #JAVA_15
+     * @see #JAVA_16
+     * @see #JAVA_17
+     */
+    public static int getMajorJavaVersion() {
+        return majorJavaVersion;
+    }
 
-	/**
-	 * Get the major version code. This means we can do things like
-	 * <code>if (getMajorJavaVersion() < JAVA_14)</code>.
-	 * @return a code comparable to the JAVA_XX codes in this class
-	 * @see #JAVA_13
-	 * @see #JAVA_14
-	 * @see #JAVA_15
-	 * @see #JAVA_16
-	 * @see #JAVA_17
-	 */
-	public static int getMajorJavaVersion() {
-		return majorJavaVersion;
-	}
+    /**
+     * Convenience method to determine if the current JVM is at least Java 1.4.
+     * 
+     * @return <code>true</code> if the current JVM is at least Java 1.4
+     * @see #getMajorJavaVersion()
+     * @see #JAVA_14
+     * @see #JAVA_15
+     * @see #JAVA_16
+     * @see #JAVA_17
+     */
+    public static boolean isAtLeastJava14() {
+        return true;
+    }
 
-	/**
-	 * Convenience method to determine if the current JVM is at least Java 1.4.
-	 * @return <code>true</code> if the current JVM is at least Java 1.4
-	 * @see #getMajorJavaVersion()
-	 * @see #JAVA_14
-	 * @see #JAVA_15
-	 * @see #JAVA_16
-	 * @see #JAVA_17
-	 */
-	public static boolean isAtLeastJava14() {
-		return true;
-	}
+    /**
+     * Convenience method to determine if the current JVM is at least Java 1.5
+     * (Java 5).
+     * 
+     * @return <code>true</code> if the current JVM is at least Java 1.5
+     * @see #getMajorJavaVersion()
+     * @see #JAVA_15
+     * @see #JAVA_16
+     * @see #JAVA_17
+     */
+    public static boolean isAtLeastJava15() {
+        return true;
+    }
 
-	/**
-	 * Convenience method to determine if the current JVM is at least
-	 * Java 1.5 (Java 5).
-	 * @return <code>true</code> if the current JVM is at least Java 1.5
-	 * @see #getMajorJavaVersion()
-	 * @see #JAVA_15
-	 * @see #JAVA_16
-	 * @see #JAVA_17
-	 */
-	public static boolean isAtLeastJava15() {
-		return getMajorJavaVersion() >= JAVA_15;
-	}
-
-	/**
-	 * Convenience method to determine if the current JVM is at least
-	 * Java 1.6 (Java 6).
-	 * @return <code>true</code> if the current JVM is at least Java 1.6
-	 * @see #getMajorJavaVersion()
-	 * @see #JAVA_16
-	 * @see #JAVA_17
-	 */
-	public static boolean isAtLeastJava16() {
-		return getMajorJavaVersion() >= JAVA_16;
-	}
+    /**
+     * Convenience method to determine if the current JVM is at least Java 1.6
+     * (Java 6).
+     * 
+     * @return <code>true</code> if the current JVM is at least Java 1.6
+     * @see #getMajorJavaVersion()
+     * @see #JAVA_16
+     * @see #JAVA_17
+     */
+    public static boolean isAtLeastJava16() {
+        return true;
+    }
 
 }
