e31700897cfa6ebfb03d01d17ed1fcd54157d519
Urs Zeidler <uz@sernet.de>
2022-03-18 15:17:06 +0100

    code format
    VN-3018

--- sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/dialogs/AccessControlEditDialog.java
+++ sernet.gs.ui.rcp.main/src/sernet/gs/ui/rcp/main/bsi/dialogs/AccessControlEditDialog.java
@@ -1,17 +1,17 @@
 /*******************************************************************************
  * Copyright (c) 2009 Robert Schuster <r.schuster@tarent.de>.
- * This program is free software: you can redistribute it and/or 
- * modify it under the terms of the GNU Lesser General Public License 
- * as published by the Free Software Foundation, either version 3 
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- *     This program is distributed in the hope that it will be useful,    
- * but WITHOUT ANY WARRANTY; without even the implied warranty 
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *     This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
- *     You should have received a copy of the GNU Lesser General Public 
- * License along with this program. 
+ *     You should have received a copy of the GNU Lesser General Public
+ * License along with this program.
  * If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Contributors:
  *     Robert Schuster <r.schuster@tarent.de> - initial API and implementation
  *     Daniel Murygin <dm[at]sernet[dot]de> - New Layout with table, add mode
@@ -74,7 +74,7 @@ import sernet.verinice.service.commands.crud.LoadPermissions;
 
 /**
  * Dialog that allows changing the access options for elements.
- * 
+ *
  * @author Robert Schuster <r.schuster@tarent.de>
  * @author Daniel Murygin <dm[at]sernet[dot]de>
  */
