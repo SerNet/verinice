ee9b7ae40b97036a4a803fd88bc6434b782ee4d3
Jochen Kemnade <jk@sernet.de>
2023-11-17 10:16:00 +0100

    improve today rule performance
    verinice#3120

--- sernet.hui.common/src/sernet/hui/common/rules/Today.java
+++ sernet.hui.common/src/sernet/hui/common/rules/Today.java
@@ -18,54 +18,46 @@
 package sernet.hui.common.rules;
 
 import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 
 public class Today implements IFillRule {
 
-	private int changeField = 0;
-	
-	private int timeDifference = 0;
-	
-	private final Map<String, Integer>  calendarFields = new HashMap<String, Integer>(10);
-	
-	public String getValue() {
-		Calendar calendar = GregorianCalendar.getInstance();
-		calendar.add(changeField, timeDifference);
-		return String.valueOf(calendar.getTimeInMillis());
-	}
+    private int changeField = 0;
 
-	public void init(String[] params) {
-		initFieldValues();
-		try {
-			if (params != null && params.length == 2) {
-				changeField = calendarFields.get(params[0]);
-				timeDifference = Integer.parseInt(params[1]);
-			}
-		} catch (Exception e) {
-			Logger.getLogger(this.getClass()).debug(e);
-		}
-		
-	}
+    private int timeDifference = 0;
 
-	private void initFieldValues() {
-		calendarFields.put("YEAR", Calendar.YEAR); //$NON-NLS-1$
-		calendarFields.put("HOUR", Calendar.HOUR); //$NON-NLS-1$
-		calendarFields.put("MINUTE", Calendar.MINUTE); //$NON-NLS-1$
-		calendarFields.put("SECOND", Calendar.SECOND); //$NON-NLS-1$
-		calendarFields.put("MONTH", Calendar.MONTH); //$NON-NLS-1$
-		calendarFields.put("DAY", Calendar.DAY_OF_MONTH); //$NON-NLS-1$
-	}
+    private static Map<String, Integer> calendarFields = Map.of("YEAR", Calendar.YEAR, //$NON-NLS-1$
+            "HOUR", Calendar.HOUR, //$NON-NLS-1$
+            "MINUTE", Calendar.MINUTE, //$NON-NLS-1$
+            "SECOND", Calendar.SECOND, //$NON-NLS-1$
+            "MONTH", Calendar.MONTH, //$NON-NLS-1$
+            "DAY", Calendar.DAY_OF_MONTH); //$NON-NLS-1$
 
-    /* (non-Javadoc)
-     * @see sernet.hui.common.rules.IFillRule#isMultiLanguage()
-     */
+    public String getValue() {
+        if (timeDifference == 0) {
+            return Long.toString(System.currentTimeMillis());
+        }
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(changeField, timeDifference);
+        return String.valueOf(calendar.getTimeInMillis());
+    }
+
+    public void init(String[] params) {
+        try {
+            if (params != null && params.length == 2) {
+                changeField = calendarFields.get(params[0]);
+                timeDifference = Integer.parseInt(params[1]);
+            }
+        } catch (Exception e) {
+            Logger.getLogger(this.getClass()).debug(e);
+        }
+
+    }
+
     public boolean isMultiLanguage() {
         return false;
     }
-
 
 }
